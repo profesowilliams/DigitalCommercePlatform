e0f25b5510b63ed633988caac85db4a87a596037
Vidhi Joshi <vidhi.joshi@techdata.com>
2020-03-11 09:10:01 +0000

    Merged PR 1761: News article component -Added policy and default component
    News article component -Added policy and default component

--- core/src/main/java/com/techdata/digitalCommerce/core/constants/Constants.java
+++ core/src/main/java/com/techdata/digitalCommerce/core/constants/Constants.java
@@ -1,8 +1,8 @@
 package com.techdata.digitalCommerce.core.constants;
 
 public final class Constants {
-	
+
 	public static final String TD_SESSION_COOKIE_NAME = "td_session";
 	public static final String TD_LWP_COOKIE_NAME = "td_lwp";
-
+	
 }
