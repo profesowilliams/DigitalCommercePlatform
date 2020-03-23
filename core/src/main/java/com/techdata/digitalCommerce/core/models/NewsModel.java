c25b646bd78d62150afb6690a0b84ebedb3fb0e1
Vidhi Joshi <vidhi.joshi@techdata.com>
2020-03-23 11:19:53 +0000

    Merged PR 2103: 27207-News items canary-Added translation for content fragment
    Added translation for content fragment
    
    Related work items: #27207

--- core/src/main/java/com/techdata/digitalCommerce/core/models/NewsModel.java
+++ core/src/main/java/com/techdata/digitalCommerce/core/models/NewsModel.java
@@ -5,15 +5,23 @@ import java.io.InputStream;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.annotations.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.day.cq.wcm.api.Page;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.techdata.digitalCommerce.core.constants.Constants;
 
 @Model(adaptables=SlingHttpServletRequest.class)
 public class NewsModel extends APIModel{
+	private Logger LOG = LoggerFactory.getLogger(NewsModel.class);
+	
+	@Inject
+	private Page currentPage;
 	
 	@SuppressWarnings("unchecked")
 	@PostConstruct
@@ -21,12 +29,22 @@ public class NewsModel extends APIModel{
 		
 		td_session_cookie =  getHttpServletRequest().getCookie(Constants.TD_SESSION_COOKIE_NAME);
 		ObjectMapper mapper = new ObjectMapper();
+		Page page = getHttpServletRequest().getResourceResolver().getResource(currentPage.getPath()).adaptTo(Page.class);
+		if(page!=null) {
+		String lang = page.getLanguage(true).getLanguage();
+		String tmp = getApiURL().replace("/en/", "/"+lang+"/");
+		
+		if(urlConnection.conn(tmp, td_session_cookie.getValue(), getMethod())!=null) {
+			setApiURL(tmp);
+		}
+		LOG.info(" lang : " + lang + " path : " + getApiURL() + " tmp : " + tmp);
+		}
 		
 		String articleId = getHttpServletRequest().getParameter(Constants.QUERY_PARAM_ID);
 		if(articleId!=null) {
 			setApiURL(getApiURL().replace("{ID}",articleId));
 		}
-		
+		LOG.info("api url : " + getApiURL());
 		con = urlConnection.conn(getApiURL(), td_session_cookie.getValue(), getMethod());
 		try {
 			
