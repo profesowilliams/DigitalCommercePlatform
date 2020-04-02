477dc7ad611ca235480bef004977823cfd69972b
Sayali Kurud <sayali.kurud2@techdata.com>
2020-04-02 12:40:24 +0000

    Merged PR 2385: 27523: News List
    path change
    
    Related work items: #27523

--- ui.apps/src/main/content/jcr_root/apps/digitalCommerce/components/content/newsList/v2/newsList/clientlibs/js/pagination-component.js
+++ ui.apps/src/main/content/jcr_root/apps/digitalCommerce/components/content/newsList/v2/newsList/clientlibs/js/pagination-component.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function () {
     paginator({
     get_rows: function () {
-        return document.getElementById("list").getElementsByTnagName("li");
+        return document.getElementById("list").getElementsByTagName("li");
     },
     box: document.getElementById("list_index"),
     active_class: "color_page"
