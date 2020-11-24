3a1b48e81691f017615cf0dca81b80cd857483a8
Hertay, Jonas <Jonas.Hertay@techdata.com>
2020-11-24 16:35:10 +0100

    Add UnitTests and IntegrationTests projects

---
+++ services/Quote/DigitalCommercePlatform.UIServices.Quote.Tests/Controllers/QuoteControllerTest.cs
@@ -0,0 +1,48 @@
+using DigitalCommercePlatform.UIServices.Quote.Controllers;
+using DigitalFoundation.Common.Contexts;
+using DigitalFoundation.Common.Security.Identity;
+using DigitalFoundation.Common.Settings;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Xunit;
+
+namespace DigitalCommercePlatform.UIServices.Quote.Tests.Controllers
+{
+    public class QuoteControllerTest
+    {
+        [Fact]
+        public void GetTest()
+        {
+            // Arrange
+            var mockMediator = new Mock<IMediator>();
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            var mockOptions = new Mock<IOptions<AppSettings>>();
+            var mockLoggerFactory = new Mock<ILoggerFactory>();
+            var mockContext = new Mock<IContext>();
+            var mockUserIdentity = new Mock<IUserIdentity>();
+            var mockSiteSettings = new Mock<ISiteSettings>();
+            
+            var _sut = new QuoteController(mockMediator.Object,
+                mockHttpContextAccessor.Object,
+                mockOptions.Object,
+                mockLoggerFactory.Object,
+                mockContext.Object,
+                mockUserIdentity.Object,
+                mockSiteSettings.Object);
+            var nameToTest = "John";
+            // Act
+            var result = _sut.Test(nameToTest);
+            // Assert
+            Assert.Contains(nameToTest, result);
+        }
+
+        [Fact(Skip = "It's not ready yet")]
+        public void GetQuotesTests()
+        {
+            // TODO
+        }
+    }
+}
