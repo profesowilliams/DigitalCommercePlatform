6cfb3ef36df6e9186a8870d7e3a7136884052819
Goran Zdesar <Goran.Zdesar@techdata.com>
2020-11-27 15:17:30 +0100

    [111099] - Create a new project - DigitalCommercePlatform.UIServices.Security

---
+++ services/Security/DigitalCommercePlatform.UIServices.Security/Controllers/SecurityController.cs
@@ -0,0 +1,42 @@
+﻿using DigitalFoundation.Common.Contexts;
+using DigitalFoundation.Common.Http.Controller;
+using DigitalFoundation.Common.Security.Identity;
+using DigitalFoundation.Common.Settings;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalCommercePlatform.UIServices.Security.Controllers
+{
+   // [Authorize(AuthenticationSchemes = "UserIdentityValidationScheme")]
+    [ApiVersion("1.0")]
+    [Route("/v{apiVersion}")]
+    public class SecurityController : BaseUIServiceController
+    {
+        public SecurityController(IMediator mediator,
+            IOptions<AppSettings> options,
+            ILoggerFactory loggerFactory,
+            IUserIdentity userIdentity,
+            IContext context,
+            IHttpContextAccessor httpContextAccessor,
+            ISiteSettings siteSettings)
+            : base(mediator, httpContextAccessor, loggerFactory, context, userIdentity.User, options, siteSettings)
+        {
+
+        }
+
+
+        [HttpGet]
+        [Route("testSecurityAPI")]
+        public string Test([FromQuery] string name)
+        {
+            return "Welcome " + name + " !";
+        }
+    }
+}
---
+++ services/Security/DigitalCommercePlatform.UIServices.Security/Startup.cs
@@ -0,0 +1,26 @@
+using DigitalFoundation.Common.Logging;
+using DigitalFoundation.Common.Services.StartupConfiguration;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace DigitalCommercePlatform.UIServices.Security
+{
+    [ExcludeFromCodeCoverage]
+    public class Startup : BaseAppServiceStartup
+    {
+        public Startup(IConfiguration configuration, IStartupLogger startupLogger) : base(configuration, startupLogger)
+        {
+        }
+
+        protected override string HealthCheckEndpoint => "https://service.dit.df.svc.us.tdworldwide.com/core-security/health/heartbeat";
+                                                          
+
+        public override void AddBaseComponents(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
