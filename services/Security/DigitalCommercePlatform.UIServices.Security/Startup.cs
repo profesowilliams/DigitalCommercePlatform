60b0831f56899a4a1841dc055ed276e2f2cb51e9
Goran Zdesar <Goran.Zdesar@techdata.com>
2020-12-09 21:38:39 +0100

    [111106] - Call core security endpoints from UI project

--- services/Security/DigitalCommercePlatform.UIServices.Security/Startup.cs
+++ services/Security/DigitalCommercePlatform.UIServices.Security/Startup.cs
@@ -1,14 +1,12 @@
 using DigitalCommercePlatform.UIServices.Security.Infrastructure;
 using DigitalFoundation.Common.Logging;
 using DigitalFoundation.Common.Services.StartupConfiguration;
-using FluentValidation;
-using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
 
 namespace DigitalCommercePlatform.UIServices.Security
 {
@@ -21,14 +19,8 @@ namespace DigitalCommercePlatform.UIServices.Security
 
         protected override string HealthCheckEndpoint => "https://service.dit.df.svc.us.tdworldwide.com/core-security/health/heartbeat";
                                                           
-
         public override void AddBaseComponents(IServiceCollection services, IConfiguration configuration)
         {
-            services.AddMediatR(Assembly.GetExecutingAssembly());
-
-            AssemblyScanner.FindValidatorsInAssembly(Assembly.GetExecutingAssembly())
-                           .ForEach(item => services.AddScoped(item.InterfaceType, item.ValidatorType));
-
             services.Configure<CoreSecurityEndpointsOptions>(Configuration.GetSection(CoreSecurityEndpointsOptions.CoreSecurityEndpoints));
 
             services.AddHttpClient("CoreSecurityClient").AddHeaderPropagation();
@@ -47,6 +39,8 @@ namespace DigitalCommercePlatform.UIServices.Security
             app.UseHeaderPropagation();
             base.ConfigureMiddleSection(app,env);
         }
+
+        protected override IEnumerable<string> AllowedNamespaces => new[] { "DigitalCommercePlatform." };
 
     }
 }
