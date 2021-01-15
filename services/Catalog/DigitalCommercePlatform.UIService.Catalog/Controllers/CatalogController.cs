f1721df647a1ca4c3f7eaa4d52154e9d1285e6dc
Tomasz Mroz <T780650U@techdata.com>
2021-01-15 14:48:46 +0000

    Merged PR 12989: Catalog.UI cleanup part 1
    Related work items: #124423

--- services/Catalog/DigitalCommercePlatform.UIService.Catalog/Controllers/CatalogController.cs
+++ services/Catalog/DigitalCommercePlatform.UIService.Catalog/Controllers/CatalogController.cs
@@ -1,10 +1,8 @@
-﻿using DigitalCommercePlatform.UIServices.Catalog.DTO.Request;
-using DigitalFoundation.Common.Contexts;
+﻿using DigitalFoundation.Common.Contexts;
 using DigitalFoundation.Common.Http.Controller;
 using DigitalFoundation.Common.Security.Identity;
 using DigitalFoundation.Common.Settings;
 using MediatR;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
---
+++ services/Catalog/DigitalCommercePlatform.UIServices.Catalog.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace DigitalCommercePlatform.UIServices.Catalog.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
