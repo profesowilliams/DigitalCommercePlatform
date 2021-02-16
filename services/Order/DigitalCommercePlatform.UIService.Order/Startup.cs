1b780dbd73f83674e862a77c027130090ce89f51
Goran Zdesar <Goran.Zdesar@techdata.com>
2021-02-16 12:12:48 +0100

    Updated health check end point

--- services/Order/DigitalCommercePlatform.UIService.Order/Startup.cs
+++ services/Order/DigitalCommercePlatform.UIService.Order/Startup.cs
@@ -18,7 +18,7 @@ namespace DigitalCommercePlatform.UIService.Order
         {
         }
 
-        protected override string HealthCheckEndpoint => "https://service.dit.df.svc.us.tdworldwide.com/app-order/health/heartbeat";
+        protected override string HealthCheckEndpoint => "https://eastus-dit-service.dc.tdebusiness.cloud/app-order/health/heartbeat";
 
         public override void AddBaseComponents(IServiceCollection services, IConfiguration configuration)
         {
