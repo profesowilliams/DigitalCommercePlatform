aaa39bbf3ea128cd2e5545905b7f59eb985cb1f0
Adam Lewandowski <Adam.Lewandowski@techdata.com>
2021-02-19 12:10:57 +0100

    - usings

--- services/Customer/DigitalCommercePlatform.UIService.Customer/Models/Customer/Common/SalesStructureModel.cs
+++ services/Customer/DigitalCommercePlatform.UIService.Customer/Models/Customer/Common/SalesStructureModel.cs
@@ -1,9 +1,5 @@
 ﻿using DigitalCommercePlatform.UIService.Customer.Models.Customer.Internal;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DigitalCommercePlatform.UIService.Customer.Models.Customer.Common
 {
--- services/Customer/DigitalCommercePlatform.UIService.Customer/Models/Customer/Common/SourceRelModel.cs
+++ services/Customer/DigitalCommercePlatform.UIService.Customer/Models/Customer/Common/SourceRelModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics.CodeAnalysis;
 
 namespace DigitalCommercePlatform.UIService.Customer.Models.Customer.Common
 {
