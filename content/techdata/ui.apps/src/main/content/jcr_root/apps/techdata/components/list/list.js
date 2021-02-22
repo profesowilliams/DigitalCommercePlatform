f091d9e5323b35be4b6ebbf212277127f7e52f84
shyamala <Shyamala.Venugopal@techdata.com>
2021-02-22 12:46:00 -0600

    #127973 Fixed the item icon image changes .

---
+++ content/techdata/ui.apps/src/main/content/jcr_root/apps/techdata/components/list/list.js
@@ -0,0 +1,18 @@
+use(function () {
+    var Constants = {
+        JCR_CONTENT_IMAGE: "/jcr:content/image",
+        FILE: 'file',
+        EXTERNAL_IMAGE_PATH: 'file/jcr:content/dam:thumbnails/dam:thumbnail_48.png',
+        FILE_REFERENCE: 'fileReference',
+		FILE_REFERENCE_IMAGE_PATH: '/jcr:content/renditions/cq5dam.thumbnail.48.48.png'
+    };
+
+    var imageResource = resolver.getResource(this.item.path + Constants.JCR_CONTENT_IMAGE);
+    imagePath = imageResource ? imageResource.getChild(Constants.FILE) ? 
+        imageResource.getChild(Constants.EXTERNAL_IMAGE_PATH).path : 
+    	imageResource.properties.get(Constants.FILE_REFERENCE) + Constants.FILE_REFERENCE_IMAGE_PATH : 
+    	"";
+    return {
+        imagePath: imagePath
+    };
+});
