de4bf400f219fe3e6ba349987722d8c6bd0bc136
Goran Zdesar <Goran.Zdesar@techdata.com>
2021-03-02 09:01:02 +0100

    [132068] - [UI Service] orders filtering

--- services/Order/DigitalCommercePlatform.UIServices.Order/Controllers/OrderController.cs
+++ services/Order/DigitalCommercePlatform.UIServices.Order/Controllers/OrderController.cs
@@ -29,8 +29,13 @@ namespace DigitalCommercePlatform.UIServices.Order.Controllers
         [Route("orders")]
         public async Task<ActionResult<IEnumerable<OrderDto>>> GetOrdersAsync([FromQuery] GetOrdersRequest getOrdersRequest)
         {
-            var getOrdersQuery = new GetOrdersQuery(getOrdersRequest.Id, getOrdersRequest.Reseller, getOrdersRequest.Vendor, getOrdersRequest.CreatedFrom, getOrdersRequest.CreatedTo, 
-                                        getOrdersRequest.OrderBy, getOrdersRequest.PageNumber, getOrdersRequest.PageSize);
+            var filtering = new FilteringDto(getOrdersRequest.Id, getOrdersRequest.Reseller, getOrdersRequest.Vendor, 
+                getOrdersRequest.CreatedFrom, getOrdersRequest.CreatedTo);
+
+            var paging = new PagingDto(getOrdersRequest.OrderBy, getOrdersRequest.PageNumber, getOrdersRequest.PageSize);
+            
+
+            var getOrdersQuery = new GetOrdersQuery(filtering,paging);
 
             var ordersResponse = await Mediator.Send(getOrdersQuery);
             return Ok(ordersResponse);
