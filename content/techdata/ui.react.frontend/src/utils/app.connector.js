e96c7b3e4495e3b08f359c92f682e352c02ecbd3
gaurav sharma <t401325K@techdata.com>
2021-03-13 23:17:21 +0530

    React Build Review and SignIn Component structure

--- content/techdata/ui.react.frontend/src/utils/app.connector.js
+++ content/techdata/ui.react.frontend/src/utils/app.connector.js
@@ -1,38 +1,44 @@
-import React, { Suspense } from 'react';
+import React, {Suspense} from 'react';
 import ReactContentRenderer from 'react-dom';
-import { nanoid } from 'nanoid';
+import {nanoid} from 'nanoid';
+import store from '../store/store';
+import {Provider} from 'react-redux';
 
 class AppConnector {
-    constructor(componentProps, element) {
-        console.log("inside App Connector constructor");
-        var _self = this;
-        this.components = [];
-        this.componentProps = componentProps;
-        console.log(this.componentProps)
+	constructor(componentProps, element) {
+		console.log('inside App Connector constructor');
+		var _self = this;
+		this.components = [];
+		this.componentProps = componentProps;
+		console.log(this.componentProps);
 
-        import (`../global/techdata/components/${this.componentProps.component}/${this.componentProps.component}`)
-        .then(component => {
-                _self.loadComponent(component.default, element);
-            })
-            .catch(error => {
-                console.error(`${_self.componentProps.component} Component not found`);
-            });
-    }
+		import(`../global/techdata/components/${this.componentProps.component}/${this.componentProps.component}`)
+			.then((component) => {
+				_self.loadComponent(component.default, element);
+			})
+			.catch((error) => {
+				console.error(`${_self.componentProps.component} Component not found`);
+			});
+	}
 
-    loadComponent(componentRef, element) {
-        console.log("load components")
-        console.log(componentRef)
-        console.log(" component props")
-        console.log(this.componentProp)
-        let Component = componentRef; // Captilize is mandatory for the variable 'Component'
-        let randomKey = nanoid()
-        ReactContentRenderer.render(<Component key={randomKey} componentProp = {this.componentProps.config} />, element);
+	loadComponent(componentRef, element) {
+		console.log('load components');
+		console.log(componentRef);
+		console.log(' component props');
+		console.log(this.componentProp);
+		let Component = componentRef; // Captilize is mandatory for the variable 'Component'
+		let randomKey = nanoid();
+		ReactContentRenderer.render(
+			<Provider store={store}>
+				<Component key={randomKey} componentProp={this.componentProps.config} />
+			</Provider>,
+			element
+		);
 	}
 }
 
-window.initConfiguration = async() => {
-
-    console.log("inside initConfig");
+window.initConfiguration = async () => {
+	console.log('inside initConfig');
 	/* Get the Configuration from backend on initial load */
 	// await initConfig();
 
@@ -41,6 +47,6 @@ window.initConfiguration = async() => {
 
 	// /* Get the Anonymous token on initial load */
 	// return await initAuth();
-}
+};
 
 window.AppConnector = AppConnector;
