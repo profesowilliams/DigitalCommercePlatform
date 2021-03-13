251f811222a46d44e1811c5c9356df7de4e41cdd
Brumoon Ahamat <Brumoon.Ahamat@techdata.com>
2021-03-12 20:35:43 -0600

    prep for removing the dcp-app module

--- content/techdata/ui.dcp-frontend-app/src/main/webpack/site/main.ts
+++ content/techdata/ui.dcp-frontend-app/src/main/webpack/site/main.ts
@@ -1,7 +1,3 @@
-// Stylesheets
-import "./main.scss";
-import "../components/core/content/**/*.ts";
-import "../components/core/content/**/*.js";
 // React App Initializer
 import "./app.initializer";
 
