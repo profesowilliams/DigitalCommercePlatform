c9cb800b1d4b7544c50e00bf664f9b738f974b30
Goran Zdesar <Goran.Zdesar@techdata.com>
2021-03-17 10:36:49 +0100

    [149150] - Implement new middle tier client in Account , Config and Content projects

---
+++ services/Account/DigitalCommercePlatform.UIServices.Account/Infrastructure/UIContextExtension.cs
@@ -0,0 +1,23 @@
+﻿using DigitalFoundation.Common.Contexts;
+
+namespace DigitalCommercePlatform.UIServices.Account.Infrastructure
+{
+    public static class UIContextExtension
+    {
+        public static void SetContextFromRequest(this IUIContext context, RequestHeaders headers)
+        {
+            context.SetAccessToken(context?.User?.AccessToken);
+            context.SetConsumer(headers?.Consumer);
+            context.SetLanguage(headers?.Language);
+            context.SetTraceId(headers?.TraceId);
+        }
+
+        public static void SetContext(this IUIContext context, string consumer,string language,string traceId, string accessToken = null)
+        {
+            context.SetConsumer(consumer);
+            context.SetLanguage(language);
+            context.SetTraceId(traceId);
+            context.SetAccessToken(accessToken);
+        }
+    }
+}
