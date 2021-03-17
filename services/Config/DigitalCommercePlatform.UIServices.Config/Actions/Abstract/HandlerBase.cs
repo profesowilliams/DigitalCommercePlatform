09765a418e0d9c1013df22ae729996b8cbaa9e41
Goran Zdesar <Goran.Zdesar@techdata.com>
2021-03-17 11:12:51 +0100

    [149150] - Implement new middle tier client in Account , Config and Content projects

--- services/Config/DigitalCommercePlatform.UIServices.Config/Actions/Abstract/HandlerBase.cs
+++ services/Config/DigitalCommercePlatform.UIServices.Config/Actions/Abstract/HandlerBase.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,36 +7,15 @@ namespace DigitalCommercePlatform.UIServices.Config.Actions.Abstract
     public abstract class HandlerBase<T> where T : class
     {
         protected readonly ILogger _logger;
-        protected readonly IHttpClientFactory _httpClientFactory;
 
-        public HandlerBase(ILoggerFactory loggerFactory, IHttpClientFactory httpClientFactory)
+        public HandlerBase(ILoggerFactory loggerFactory)
         {
             _logger = loggerFactory.CreateLogger<T>();
-            _httpClientFactory = httpClientFactory;
         }
 
         protected async Task<U> GetAsync<U>(string url, string token, CancellationToken cancellationToken)
         {
-            // this is temp code once DF team exposes the clitent we need to replace this code.
-            HttpClient httpClient = _httpClientFactory.CreateClient();
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            httpClient.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
-            httpClient.DefaultRequestHeaders.Add("Accept-Language", "en-us");
-            httpClient.DefaultRequestHeaders.Add("Site", "NA");
-            httpClient.DefaultRequestHeaders.Add("Consumer", "NA");
-
-            var httpRequest = new HttpRequestMessage()
-            {
-                RequestUri = new Uri(url),
-                Method = HttpMethod.Get,
-            };
-            var serializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true, };
-
-            HttpResponseMessage response = await httpClient.SendAsync(httpRequest, cancellationToken);
-            response.EnsureSuccessStatusCode();
-            string responseBody = await response.Content.ReadAsStringAsync();
-            var data = JsonSerializer.Deserialize<U>(responseBody, serializerOptions);
-            return data;
+            return await Task.FromResult(default(U));
         }
     }
 }
