f187fec95729c71fea7c0cbe1f66931de0d27d10
Brumoon Ahamat <Brumoon.Ahamat@techdata.com>
2021-03-22 11:27:24 -0500

    further updates to get sign-in working on AEM.

--- content/techdata/ui.frontend/webpack.dev.js
+++ content/techdata/ui.frontend/webpack.dev.js
@@ -268,6 +268,11 @@ module.exports = (env) => {
 					target: 'http://localhost:4502',
 				},
 			],
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+				"Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+			},
 			writeToDisk,
 			liveReload: !writeToDisk,
 		},
--- content/techdata/ui.react.frontend/src/index.js
+++ content/techdata/ui.react.frontend/src/index.js
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom';
 
 import "regenerator-runtime/runtime";
 import './utils/app.connector';
-import './index.scss';
+import './utils/axios';
+import './utils/randomCodeGenrator';
 
+import './store/action/actionType';
+import './store/action/authAction';
+
+import './store/store';
+import './store/authReducer';
+import './store/index';
+
+import './index.scss';
 import Chart from "./global/techdata/components/Chart/Chart";
+import SignIn from "./global/techdata/components/SignIn/SignIn";
+
+
 
 
 
