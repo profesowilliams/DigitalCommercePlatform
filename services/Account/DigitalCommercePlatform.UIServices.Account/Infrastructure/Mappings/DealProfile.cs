8bec8ea95dab7f33d952b56ff3002a96a49c0bf9
US\NMADHAV <nilesh.madhavi@techdata.com>
2021-03-24 09:33:45 -0400

    Removed unnecessary 'using'

--- services/Account/DigitalCommercePlatform.UIServices.Account/Infrastructure/Mappings/DealProfile.cs
+++ services/Account/DigitalCommercePlatform.UIServices.Account/Infrastructure/Mappings/DealProfile.cs
@@ -1,15 +1,11 @@
 ﻿using AutoMapper;
 using DigitalCommercePlatform.UIServices.Account.Actions.DealsSummary;
 using DigitalCommercePlatform.UIServices.Account.Models.Deals;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DigitalCommercePlatform.UIServices.Account.Infrastructure.Mappings
 {
-    
+
     [ExcludeFromCodeCoverage]
     public class DealProfile : Profile
     {
