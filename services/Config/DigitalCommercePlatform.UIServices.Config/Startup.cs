8fdc2c800ca71cac64d0ca403afd81365d7dcacb
Jonas Hertay <Jonas.Hertay@techdata.com>
2021-04-06 08:49:27 +0000

    Merged PR 16412: [153453] Get CORS policy configuration from AppSettings in MongoDB
    [153453] Get CORS policy configuration from AppSettings in MongoDB
    
    Related work items: #153453

--- services/Config/DigitalCommercePlatform.UIServices.Config/Startup.cs
+++ services/Config/DigitalCommercePlatform.UIServices.Config/Startup.cs
@@ -1,6 +1,7 @@
 using DigitalCommercePlatform.UIServices.Config.Services;
 using DigitalFoundation.Common.Logging;
 using DigitalFoundation.Common.Services.StartupConfiguration;
+using DigitalFoundation.Common.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -27,22 +28,22 @@ namespace DigitalCommercePlatform.UIServices.Config
 
         public override void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                            .WithHeaders("Accept-Language", "Authorization", "Consumer", "Content-Length", "Content-Type", "SessionId", "Site", "TraceId", "X-Requested-With")
-                            .WithMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
-                    });
-            });
+            services.AddCors();
             base.ConfigureServices(services);
         }
 
         public override void Configure(IApplicationBuilder app, IWebHostEnvironment env, IOptions<RequestLocalizationOptions> localizationOptions)
         {
-            app.UseCors();
+            var options = app.ApplicationServices.GetService<IOptions<AppSettings>>();
+            var corsAllowedOrigin = options?.Value.GetSetting("UI.Security.CorsAllowedOrigin");
+            var corsAllowedHeaders = options?.Value.GetSetting("UI.Security.CorsAllowedHeaders");
+            var corsAllowedMethods = options?.Value.GetSetting("UI.Security.CorsAllowedMethods");
+            app.UseCors(configurePolicy =>
+            {
+                configurePolicy.WithOrigins(corsAllowedOrigin?.Split(','))
+                            .WithHeaders(corsAllowedHeaders?.Split(','))
+                            .WithMethods(corsAllowedMethods?.Split(','));
+            });
             base.Configure(app, env, localizationOptions);
         }
 
