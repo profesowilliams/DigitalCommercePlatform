3b9d3e6db414c487fa8a891a2cb3acb1893f3519
Harshavardhan Reddy <T118187C@techdata.com>
2021-04-14 21:32:48 +0000

    Merged PR 16827:  AEM changes
    added extra fields for AEM component
    
    Related work items: #156770

--- content/techdata/ui.apps/src/main/content/jcr_root/apps/techdata/components/myrenewals/myrenewals.js
+++ content/techdata/ui.apps/src/main/content/jcr_root/apps/techdata/components/myrenewals/myrenewals.js
@@ -4,6 +4,18 @@ use(function () {
     if (properties && properties["label"]) {
         jsonObject["label"] = properties["label"];
     }
+    if (properties && properties["paramOneLabel"]) {
+        jsonObject["paramOneLabel"] = properties["paramOneLabel"];
+    }
+    if (properties && properties["paramTwoLabel"]) {
+        jsonObject["paramTwoLabel"] = properties["paramTwoLabel"];
+    }
+    if (properties && properties["paramThreeLabel"]) {
+        jsonObject["paramThreeLabel"] = properties["paramThreeLabel"];
+    }
+    if (properties && properties["todayLabel"]) {
+        jsonObject["todayLabel"] = properties["todayLabel"];
+    }
     if (properties && properties["uiServiceEndPoint"]) {
         jsonObject["uiServiceEndPoint"] = properties["uiServiceEndPoint"];
     }
