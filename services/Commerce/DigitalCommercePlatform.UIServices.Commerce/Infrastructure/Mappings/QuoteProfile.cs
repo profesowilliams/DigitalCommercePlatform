40a1f1031dfb4890fd1bb97492e2b1b25fcd0eee
Mounika Kanakamedala <T396608F@techdata.com>
2021-04-22 10:04:02 +0000

    Merged PR 17317: Added Code for the Count in Grid Quote
    Added Code for the Count in Grid Quote
    
    Related work items: #158731

--- services/Commerce/DigitalCommercePlatform.UIServices.Commerce/Infrastructure/Mappings/QuoteProfile.cs
+++ services/Commerce/DigitalCommercePlatform.UIServices.Commerce/Infrastructure/Mappings/QuoteProfile.cs
@@ -35,8 +35,9 @@ namespace DigitalCommercePlatform.UIServices.Commerce.Infrastructure.Mappings
              .ForMember(dest => dest.CanCheckOut, opt => opt.MapFrom(src => string.Join( "","True")))
              ;
             CreateMap<FindResponse<IEnumerable<QuoteModel>>, GetQuotesForGrid.Response>()
-                 .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Data));
-            
+                 .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Data))
+                 .ForMember(dest => dest.Count, s => s.MapFrom(src => src.Count.ToString()));
+
         }
 
     }
