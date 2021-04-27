844aa4f644963cb87e6c415228476aa591953fa2
Venugopal, Shyamala (c) <Shyamala.Venugopal@techdata.com>
2021-04-27 19:08:05 +0000

    Merged PR 17576: Dialog , Htl Changes  and Used JS useAPI for the createquote component.
    Dialog , Htl Changes  and Used JS useAPI for the createquote component.
    
    Related work items: #158710

---
+++ content/techdata/ui.apps/src/main/content/jcr_root/apps/techdata/components/createquote/createquote.js
@@ -0,0 +1,45 @@
+"use strict";
+use(function () {
+
+    var jsonObject = new Packages.org.json.JSONObject();
+    var listValues = [];
+    var resourceResolver = resource.getResourceResolver();
+    var node = resourceResolver.getResource(currentNode.getPath() + "/optionsList");
+
+    if (node !== null) {
+        var childrenList = node.getChildren();
+
+        for (var [key, res] in Iterator(childrenList)) {
+            var label = res.properties["label"];
+            var optionKey = res.properties["key"];
+            var title = res.properties["title"];
+            var itemData = {};
+            itemData.label = label;
+            itemData.key = optionKey;
+            itemData.title = title;
+            listValues.push(itemData);
+        }
+    }
+    if (properties.get("label") != null) {
+        jsonObject.put("label", properties.get("label"));
+    }
+
+    if (properties.get("endpoint") != null) {
+        jsonObject.put("endpoint", properties.get("endpoint"));
+    }
+
+    if (properties.get("buttonTitle") != null) {
+        jsonObject.put("buttonTitle", properties.get("buttonTitle"));
+    }
+
+    if (properties.get("quotePreviewUrl") != null) {
+        jsonObject.put("quotePreviewUrl", properties.get("quotePreviewUrl"));
+    }
+
+    if (listValues != null) {
+        jsonObject.put("optionsList", listValues);
+    }
+    return {
+        configJson: jsonObject.toString()
+    };
+});
