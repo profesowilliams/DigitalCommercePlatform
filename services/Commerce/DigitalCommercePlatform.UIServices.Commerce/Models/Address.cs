9f2d676702815148f714050e1a27a219328aec36
Mounika Kanakamedala <T396608F@techdata.com>
2021-05-07 15:09:46 +0000

    Merged PR 18233: Added the Name and Company Name
    Added the Name and Company Name
    
    Related work items: #165167

--- services/Commerce/DigitalCommercePlatform.UIServices.Commerce/Models/Address.cs
+++ services/Commerce/DigitalCommercePlatform.UIServices.Commerce/Models/Address.cs
@@ -3,6 +3,7 @@
     public class Address
     {
         public string Id { get; set; }
+        public string CompanyName { get; set; }
         public string Name { get; set; }
         public string Line1 { get; set; }
         public string Line2 { get; set; }
