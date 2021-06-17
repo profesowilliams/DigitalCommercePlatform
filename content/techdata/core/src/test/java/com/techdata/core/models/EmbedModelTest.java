ca76ef6d38f923a491db6654ab810494df91a5d3
Bala Kondepudi <T552441J@techdata.com>
2021-06-17 20:37:29 +0000

    Merged PR 20496: 167056: Add JS script support to embed component
    Details of change:
    - Overlayed AEM OOTB embed
    - Added support for Javascript
    - Added Java Model to expose script attribute
    - Added Junit test
    
    Related work items: #167056

---
+++ content/techdata/core/src/test/java/com/techdata/core/models/EmbedModelTest.java
@@ -0,0 +1,67 @@
+package com.techdata.core.models;
+
+import com.day.cq.wcm.api.designer.Style;
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.reflect.Field;
+
+import static com.techdata.core.models.EmbedModel.PN_DESIGN_SCRIPT_DISABLED;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith({AemContextExtension.class, MockitoExtension.class})
+class EmbedModelTest {
+
+    private final AemContext ctx = new AemContext();
+
+    private EmbedModel embedModel;
+
+    @Mock
+    private Style currentStyle;
+
+    @BeforeEach
+    void setUp() {
+        embedModel = new EmbedModel();
+        Field field = null;
+        Field field2 = null;
+        try {
+            field = embedModel.getClass().getDeclaredField("currentStyle");
+            field.setAccessible(true);
+            field.set(embedModel, currentStyle);
+
+            field2 = embedModel.getClass().getDeclaredField("type");
+            field2.setAccessible(true);
+            field2.set(embedModel, "script");
+
+            field2 = embedModel.getClass().getDeclaredField("script");
+            field2.setAccessible(true);
+            field2.set(embedModel, EXPECTED_JS_SCRIPT);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        ctx.addModelsForClasses(EmbedModel.class);
+
+    }
+
+    @Test
+    void validateForJScript() {
+        when(currentStyle.get(PN_DESIGN_SCRIPT_DISABLED, false)).thenReturn(false);
+        embedModel.initModel();
+        assertEquals(EXPECTED_JS_SCRIPT, embedModel.getScript());
+    }
+
+    private static final String EXPECTED_JS_SCRIPT = "<script type=\"text/javascript\">if (!window.AdButler){(function(){var s = document.createElement(\"script\"); s.async = true; s.type = \"text/javascript\";s.src = 'https://servedbyadbutler.com/app.js';var n = document.getElementsByTagName(\"script\")[0]; n.parentNode.insertBefore(s, n);}());}</script>\n" +
+            "<script type=\"text/javascript\">\n" +
+            "var AdButler = AdButler || {}; AdButler.ads = AdButler.ads || [];\n" +
+            "var abkw = window.abkw || '';\n" +
+            "var plc214424 = window.plc214424 || 0;\n" +
+            "document.write('<'+'div id=\"placement_214424_'+plc214424+'\"></'+'div>');\n" +
+            "AdButler.ads.push({handler: function(opt){ AdButler.register(168525, 214424, [226,200], 'placement_214424_'+opt.place, opt); }, opt: { place: plc214424++, keywords: abkw, domain: 'servedbyadbutler.com', click:'CLICK_MACRO_PLACEHOLDER' }});\n" +
+            "</script>";
+}
