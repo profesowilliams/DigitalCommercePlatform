ee5460bdf9ed337b5f0b1adff497a34ecc3f3337
Mounika Kanakamedala <T396608F@techdata.com>
2021-06-22 18:25:59 +0000

    Merged PR 20776: XS✔ ◾ Adding unit test cases to the existing Services
    Adding unit test cases to the existing Services
    
    Related work items: #178750

--- services/Browse/DigitalCommercePlatform.UIServices.Browse.Tests/Services/BrowseServiceTests.cs
+++ services/Browse/DigitalCommercePlatform.UIServices.Browse.Tests/Services/BrowseServiceTests.cs
@@ -1,17 +1,21 @@
 using AutoMapper;
+using DigitalCommercePlatform.UIServices.Browse.Actions.GetCartDetails;
 using DigitalCommercePlatform.UIServices.Browse.Actions.GetCatalogDetails;
+using DigitalCommercePlatform.UIServices.Browse.Actions.GetHeaderDetails;
 using DigitalCommercePlatform.UIServices.Browse.Actions.GetProductDetails;
 using DigitalCommercePlatform.UIServices.Browse.Actions.GetProductSummary;
 using DigitalCommercePlatform.UIServices.Browse.Models.Catalogue;
 using DigitalCommercePlatform.UIServices.Browse.Services;
 using DigitalFoundation.Common.Client;
 using DigitalFoundation.Common.Contexts;
+using DigitalFoundation.Common.Models;
 using DigitalFoundation.Common.Settings;
 using DigitalFoundation.Common.TestUtilities;
 using FluentAssertions;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using Moq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -39,6 +43,7 @@ namespace DigitalCommercePlatform.UIServices.Browse.Tests.Services
             _appSettings.Setup(s => s.GetSetting("Feature.DF.ProuctCatalog")).Returns("false");
             _appSettings.Setup(s => s.GetSetting("External.Product.Catalog.Url")).Returns("https://mtapnew.stage.svc.us.cstenet.com/ProductService/api/VendorProduct/getProductCatalog");
             _uiContext = new Mock<IUIContext>();
+            _uiContext.SetupGet(x => x.User).Returns(new User { ActiveCustomer = new Customer { CustomerNumber = "123", System = "2" }, Customers = new List<string>() });
             _mapper = new Mock<IMapper>();
             _memoryCache = new MemoryCache(new MemoryCacheOptions());
             _cachingService = new CachingService(_memoryCache, _cachelogger.Object);
@@ -46,6 +51,33 @@ namespace DigitalCommercePlatform.UIServices.Browse.Tests.Services
                                
                 
               
+        }
+        [Theory]
+        [AutoDomainData]
+        public async Task GetHeader(GetHeaderHandler.Request request)
+        {
+            // Act
+            var result = await _browseService.GetHeader(request);
+            // Assert
+            Assert.NotNull(result);
+        }
+        [Theory]
+        [AutoDomainData]
+        public async Task GetCustomerDetails()
+        {
+            // Act
+            var result = await _browseService.GetCustomerDetails();
+            // Assert
+            Assert.NotNull(result);
+        }
+        [Theory]
+        [AutoDomainData]
+        public async Task GetCartDetails(GetCartHandler.Request request)
+        {
+            // Act
+            var result = await _browseService.GetCartDetails(request);
+            // Assert
+            Assert.NotNull(result);
         }
 
         [Theory]
