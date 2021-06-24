c9edcf608f8777bf38b38fb15d6b70dc6c6ab184
Thomas Sebastian <T205242F@techdata.com>
2021-06-24 21:17:21 +0000

    Merged PR 20879: Porting old styles from components folder to global - first of many PR's
      - [X] jump to top
     - [X] Button componen
      - [X] Card component, named as cards.scss
      - [X] accordion
      - [X] Article list
      - [X] Bar chart
      - [X] Carousel
      - [X] Cart
      - [X] Container component
      - [X] Create quote aka quote in new scss
      - [X] experience fragment
      - [X] font.scss added to helper
      - [X] Form options
    
    Related work items: #179455

--- content/techdata/ui.frontend/src/main/webpack/global/main.js
+++ content/techdata/ui.frontend/src/main/webpack/global/main.js
@@ -1,3 +1,5 @@
+// TODO: font-awesome libs have a significant impact on bundle size.
+// find a way to optimize this.
 import "../../../../node_modules/@fortawesome/fontawesome-free/js/fontawesome"
 import "../../../../node_modules/@fortawesome/fontawesome-free/css/all.css"
 import "../../../../node_modules/@fortawesome/fontawesome-free/js/all"
