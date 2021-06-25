bd3bc34d5ab2c2b8f7f69bb4d7cdafb22b49b26b
Olivera Pavic <T480216M@techdata.com>
2021-06-25 20:16:36 +0000

    Merged PR 21026: AEM jump button fix
    AEM jump button fix
    
    Related work items: #180201

--- content/techdata/ui.frontend/src/main/webpack/global/td-components/button/buttons.js
+++ content/techdata/ui.frontend/src/main/webpack/global/td-components/button/buttons.js
@@ -1,36 +1,19 @@
 // JUMP TO TOP Button ///
 
 (function () {
-    "use strict";
+  "use strict";
 
-const scrollToTopButton = document.getElementById('js-jumptotop');
+  const scrollToTopButton = document.getElementById("js-jumptotop");
+  
+  // On scrolls down the document, the button appear
+  window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopButton.style.display = "block";
+    } else {
+      scrollToTopButton.style.display = "none";
+    }
+  };
 
-const scrollFunc = () => {
- 
-  let y = window.scrollY;
-
-  if (y > 0) {
-    scrollToTopButton.className = "cmp-jump-to-top show";
-  } else {
-    scrollToTopButton.className = "cmp-jump-to-top hide";
-  }
-};
-
-window.addEventListener("scroll", scrollFunc);
-
-const scrollToTop = () => {
-  const c = document.documentElement.scrollTop || document.body.scrollTop;
-  if (c > 0) {
-    window.requestAnimationFrame(scrollToTop);
-    window.scrollTo(0, c - c / 10);
-  }
-};
-
-// on click: run our ScrolltoTop function above!
-scrollToTopButton.onclick = function(e) {
-  e.preventDefault();
-  scrollToTop();
-}
 })();
 
 // end of JUMP TO TOP Button ///
