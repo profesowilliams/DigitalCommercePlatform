55ec9698f909ce257f9e0240f73968183c3e5b91
Jaswanth Nandikonda <T895600X@techdata.com>
2021-06-28 14:54:35 +0000

    Merged PR 20757: New Component Filmstrip created.
    New Component Filmstrip created.
    
    Upload package for content fragment test content. [172681-Filmstrip-Comp-content-1.0.zip](blob:https://techdatacorp.visualstudio.com/966157c2-5393-45a7-ad95-63f6797fbc91)
    
    Related work items: #172681

---
+++ content/techdata/core/src/main/java/com/techdata/core/models/FilmStripItem.java
@@ -0,0 +1,63 @@
+package com.techdata.core.models;
+
+import java.util.Iterator;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.adobe.cq.dam.cfm.ContentElement;
+import com.adobe.cq.dam.cfm.ContentFragment;
+import com.adobe.cq.wcm.core.components.models.ListItem;
+
+public class FilmStripItem implements ListItem {
+	private String imagePath;
+	private String name;
+	private String position;
+	private String profileLink;
+
+	public String getProfileLink() {
+		return profileLink;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+	
+	public FilmStripItem(String imagePath, String name, String position, String profileLink) {
+        this.imagePath = imagePath;
+        this.name = name;
+        this.position = position;
+        this.profileLink = profileLink;
+    }
+	
+	public static FilmStripItem getProfileListItem(ContentFragment cf, String profilePath){
+
+        String imagePath = StringUtils.EMPTY;
+        String name = StringUtils.EMPTY;
+        String position = StringUtils.EMPTY;
+        String profileLink = StringUtils.EMPTY;
+
+        for (Iterator<ContentElement> it = cf.getElements(); it.hasNext(); ) {
+            ContentElement ce = it.next();
+            String tagElement = ce.getName().toString();
+            if(tagElement.equals("photo")){
+            	imagePath = ce.getContent();
+            }else if(tagElement.equals("name")){
+            	name = ce.getContent();
+            }else if(tagElement.equals("position")){
+            	position = ce.getContent();
+            }else if (profilePath != null){
+            	profileLink = profilePath;
+            }
+        }
+        FilmStripItem v1 = new FilmStripItem(imagePath, name, position, profileLink);
+        return v1;
+    }
+}
