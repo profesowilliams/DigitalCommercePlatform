f5a99a0a87a7562b5b6b5395db739641c716e40a
Harshavardhan Reddy <T118187C@techdata.com>
2021-07-07 16:10:04 +0000

    Merged PR 21580: search functionality js changes
    search functionality js changes
    
    Related work items: #179376

--- content/techdata/ui.apps/src/main/content/jcr_root/apps/techdata/components/search/helper.js
+++ content/techdata/ui.apps/src/main/content/jcr_root/apps/techdata/components/search/helper.js
@@ -1,50 +1,82 @@
 "use strict";
 
 use(function() {
-    console.log("112233");
     var jsonObject = {};
     var keyValues = [];
 
 var listOfElementsData = properties["listOfElements"];
+
+var singleLabelEndpointData = {};
+
+    if(listOfElementsData == "all") {
+            singleLabelEndpointData.label = "All";
+            singleLabelEndpointData.endpoint = this.allSearchEndpoint;
+            keyValues.push(singleLabelEndpointData);
+
+        } else if(listOfElementsData == "products") {
+            singleLabelEndpointData.label = "Products";
+            singleLabelEndpointData.endpoint = this.productSearchEndpoint;
+            keyValues.push(singleLabelEndpointData);
+
+        } else if(listOfElementsData == "content") {
+            singleLabelEndpointData.label = "Content";
+            singleLabelEndpointData.endpoint = this.contentSearchEndpoint;
+            keyValues.push(singleLabelEndpointData);
+
+        } else if(listOfElementsData == "quotes") {
+            singleLabelEndpointData.label = "Quotes";
+            singleLabelEndpointData.endpoint = this.quoteSearchEndpoint;
+            keyValues.push(singleLabelEndpointData);
+
+        } else if(listOfElementsData == "orders") {
+            singleLabelEndpointData.label = "Orders";
+            singleLabelEndpointData.endpoint = this.orderSearchEndpoint;
+            keyValues.push(labelEndpointData);
+
+        } else if(listOfElementsData == "spas") {
+            labelEndpointData.label = "Spa's";
+            labelEndpointData.endpoint = this.spaSearchEndpoint;
+            keyValues.push(singleLabelEndpointData);
+
+        }
 
     for (var key in listOfElementsData) {
         var labelEndpointData = {};
 
   console.log(listOfElementsData[key]);
         if(listOfElementsData[key].toString() == "all") {
-            labelEndpointData.label ="All";
+            labelEndpointData.label = "All";
             labelEndpointData.endpoint =this.allSearchEndpoint;
             keyValues.push(labelEndpointData);
 
         } else if(listOfElementsData[key].toString() == "products") {
-            labelEndpointData.label ="Products";
+            labelEndpointData.label = "Products";
             labelEndpointData.endpoint = this.productSearchEndpoint;
             keyValues.push(labelEndpointData);
 
         } else if(listOfElementsData[key].toString() == "content") {
-            labelEndpointData.label ="Content";
+            labelEndpointData.label = "Content";
             labelEndpointData.endpoint = this.contentSearchEndpoint;
             keyValues.push(labelEndpointData);
 
         } else if(listOfElementsData[key].toString() == "quotes") {
-            labelEndpointData.label ="Quotes";
+            labelEndpointData.label = "Quotes";
             labelEndpointData.endpoint = this.quoteSearchEndpoint;
             keyValues.push(labelEndpointData);
 
         } else if(listOfElementsData[key].toString() == "orders") {
-            labelEndpointData.label ="Orders";
+            labelEndpointData.label = "Orders";
             labelEndpointData.endpoint = this.orderSearchEndpoint;
             keyValues.push(labelEndpointData);
 
         } else if(listOfElementsData[key].toString() == "spas") {
-            labelEndpointData.label ="SPA's";
+            labelEndpointData.label = "SPA's";
             labelEndpointData.endpoint = this.spaSearchEndpoint;
             keyValues.push(labelEndpointData);
 
         }
 }
 jsonObject["dropdownList"] = keyValues;
-console.log(JSON.stringify(jsonObject));
     return {
         configJson: JSON.stringify(jsonObject)
     };
