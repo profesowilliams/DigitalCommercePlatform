3eefceccd129ded584fc3a993c1a95266beaf5c5
Thomas Sebastian <T205242F@techdata.com>
2021-07-09 12:08:44 +0000

    Merged PR 21722: overriding grid styles for megamenu
    overriding grid styles
    
    Related work items: #179327

--- content/techdata/ui.frontend/src/main/webpack/site/main.ts
+++ content/techdata/ui.frontend/src/main/webpack/site/main.ts
@@ -6,7 +6,11 @@ import "./main.scss";
 import "./**/*.js";
 import "./**/*.ts";
 import "../common-utils/js/app.initializer";
+
+// TODO: AEM devs to include clientlib.global.js too instead of fetching dependencies from here
 import "../global/td-components/megamenu/megamenu";
 import "../global/td-components/vendorlistings/vendorlistings";
 import "../global/td-components/region-select/region-select";
-import "../global/td-components/button/buttons";
+import "../global/td-components/button/buttons"
+import {Initializer} from "../global/initializer";
+new Initializer();
