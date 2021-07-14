01d48f32b3f031b2432ec3363de8447a133723df
Sal Capone <Sal.Capone@techdata.com>
2021-07-14 12:41:11 +0000

    Merged PR 22003: feat(148917): allow svg to be rendered and highlighted in teaser
    * Overwrote `image.html` file in teaser component to render SVGs in an explicit manner in the DOM. This is for when SVGs are used as the `fileReference` of the ticket.
    * Created backend model to take `fileReference` and return `<svg>` element tag back to HTML.
    
    Related work items: #185078

---
+++ content/techdata/core/src/main/java/com/techdata/core/models/ExtractSVGModel.java
@@ -0,0 +1,74 @@
+package com.techdata.core.models;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.Optional;
+
+import javax.annotation.PostConstruct;
+
+import com.day.cq.dam.api.Asset;
+import com.day.cq.dam.api.Rendition;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+
+@Model(
+  adaptables = SlingHttpServletRequest.class,
+  defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
+public class ExtractSVGModel {
+
+  private static final String FILE_REFERENCE_PN = "fileReference";
+  private static final String SVG_EXTENSION = ".svg";
+
+  @Self
+  private SlingHttpServletRequest request;
+
+  private Asset asset;
+
+  @PostConstruct
+  protected void init() {
+    this.asset = Optional.ofNullable(this.request.getResource())
+      .map(imageResource -> imageResource.getValueMap().get(FILE_REFERENCE_PN, String.class))
+      .map(fileReference -> this.request.getResourceResolver().getResource(fileReference))
+      .map(resource -> resource.adaptTo(Asset.class))
+      .orElse(null);
+  }
+
+  public boolean isSvg() {
+    if (Objects.nonNull(asset)) {
+      return StringUtils.contains(asset.getPath(), SVG_EXTENSION);
+    }
+    return false;
+  }
+
+  public String getBinary() {
+    if (Objects.nonNull(asset)) {
+      final Rendition rendition = asset.getOriginal();
+      final InputStream stream = rendition.getStream();
+      return readXml(stream);
+    }
+    return StringUtils.EMPTY;
+  }
+
+  private String readXml(final InputStream content) {
+    String readLine;
+    final BufferedReader br = new BufferedReader(new InputStreamReader(content));
+    final StringBuilder strBuilder = new StringBuilder();
+
+    try {
+      while (((readLine = br.readLine()) != null)) {
+        strBuilder.append(readLine);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return strBuilder.toString();
+  }
+}
