6f4c0a8ec82ade8dab4509ca6b39e18041e13e1e
Brumoon Ahamat <T892269G@techdata.com>
2021-07-20 19:28:23 +0000

    Merged PR 22371: updates to get svgs working
    updates to get svgs working
    
    Related work items: #183976

--- content/techdata/ui.apps/src/main/content/jcr_root/apps/techdata/components/list/list.js
+++ content/techdata/ui.apps/src/main/content/jcr_root/apps/techdata/components/list/list.js
@@ -4,14 +4,24 @@ use(function () {
         FILE: 'file',
         EXTERNAL_IMAGE_PATH: 'file/jcr:content/dam:thumbnails/dam:thumbnail_48.png',
         FILE_REFERENCE: 'fileReference',
-		FILE_REFERENCE_IMAGE_PATH: '/jcr:content/renditions/cq5dam.thumbnail.140.100.png'
+        FILE_REFERENCE_IMAGE_PATH: '/jcr:content/renditions/cq5dam.thumbnail.140.100.png',
+        FILE_JCR_CONTENT : '/jcr:content/image/file/jcr:content',
+        THUMBNAIL_FOLDER : '/jcr:content/image/file/jcr:content/dam:thumbnails'
     };
 
     var imageResource = resolver.getResource(this.item.path + Constants.JCR_CONTENT_IMAGE);
-    imagePath = imageResource ? imageResource.getChild(Constants.FILE) ? 
-        imageResource.getChild(Constants.EXTERNAL_IMAGE_PATH).path : 
-    	imageResource.properties.get(Constants.FILE_REFERENCE) + Constants.FILE_REFERENCE_IMAGE_PATH : 
-    	"";
+
+    if (imageResource && imageResource.getChild(Constants.THUMBNAIL_FOLDER))
+    {
+        var fileJcrContent = resolver.getResource(this.item.path + Constants.FILE_JCR_CONTENT);
+        imagePath = imageResource.getChild(Constants.EXTERNAL_IMAGE_PATH).path
+    }else if (imageResource && imageResource.getChild(Constants.FILE)) {
+        imagePath = imageResource.path  + '/' + Constants.FILE;
+    }else {
+        imagePath = imageResource.properties.get(Constants.FILE_REFERENCE) + Constants.FILE_REFERENCE_IMAGE_PATH;
+    }
+
+
     return {
         imagePath: imagePath
     };
