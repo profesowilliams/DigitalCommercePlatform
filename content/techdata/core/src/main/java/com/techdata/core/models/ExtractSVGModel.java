cb51f8e54123ec7da45dcdfe69852444fa76f8aa
Brumoon Ahamat <T892269G@techdata.com>
2021-07-23 12:43:43 +0000

    Merged PR 22604: Revert 'brand-grid-3 svg AEM part'
    Reverts !22599
    
    Related work items: #183976

--- content/techdata/core/src/main/java/com/techdata/core/models/ExtractSVGModel.java
+++ content/techdata/core/src/main/java/com/techdata/core/models/ExtractSVGModel.java
@@ -14,7 +14,6 @@ import com.day.cq.dam.api.Rendition;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.Self;
@@ -22,41 +21,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Model(
-  adaptables = Resource.class,
+  adaptables = SlingHttpServletRequest.class,
   defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
 )
 public class ExtractSVGModel {
 
-  private static final Logger log = LoggerFactory.getLogger(ExtractSVGModel.class);
+  private static final Logger LOG = LoggerFactory.getLogger(ExtractSVGModel.class);
 
 
   private static final String FILE_REFERENCE_PN = "fileReference";
   private static final String SVG_EXTENSION = ".svg";
 
   @Self
-  private Resource resource;
+  private SlingHttpServletRequest request;
 
   private Asset asset;
 
   @PostConstruct
   protected void init() {
-    log.debug("inside init");
-
-    this.asset = Optional.ofNullable(this.resource)
+    this.asset = Optional.ofNullable(this.request.getResource())
       .map(imageResource -> imageResource.getValueMap().get(FILE_REFERENCE_PN, String.class))
-      .map(fileReference -> this.resource.getResourceResolver().getResource(fileReference))
+      .map(fileReference -> this.request.getResourceResolver().getResource(fileReference))
       .map(resource -> resource.adaptTo(Asset.class))
       .orElse(null);
-
-    log.debug("resource path is {}", resource!=null ? resource.getPath() : "is null");
-
-
-    log.debug("is asset null ? {}", this.asset == null);
   }
 
   public boolean isSvg() {
     if (Objects.nonNull(asset)) {
-      log.debug("StringUtils.contains(asset.getPath(), SVG_EXTENSION) {} {}", this.asset.getPath(), StringUtils.contains(asset.getPath(), SVG_EXTENSION));
       return StringUtils.contains(asset.getPath(), SVG_EXTENSION);
     }
     return false;
@@ -81,12 +72,8 @@ public class ExtractSVGModel {
         strBuilder.append(readLine);
       }
     } catch (IOException e) {
-      log.error("Unable to read the SVG's XML from InputStream: ", e);
+      LOG.error("Unable to read the SVG's XML from InputStream: ", e);
     }
     return strBuilder.toString();
-  }
-
-  public String getPath() {
-    return this.asset.getPath();
   }
 }
