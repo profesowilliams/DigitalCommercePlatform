598790247b20a80adead2c4b1e73d33c6a70d8a3
Richard Ayeni <T640367Q@techdata.com>
2021-07-29 07:36:10 +0000

    Merged PR 22877: Added Item Descriptions
    Added Item Descriptions
    
    Related work items: #184971

--- content/techdata/ui.react.frontend/src/global/techdata/components/QuotePreview/ProductLines/ProductLinesItemInformation.jsx
+++ content/techdata/ui.react.frontend/src/global/techdata/components/QuotePreview/ProductLines/ProductLinesItemInformation.jsx
@@ -3,13 +3,23 @@ import React from "react";
 function ProductLinesItemInformation({ line }) {
   return (
     <section>
-       <div className="cmp-product-lines-grid__item-information">
-          <a href={line.urlProductImage} className="cmp-product-lines-grid__item-information__image-link">
-               <img alt="No Image" src={line.urlProductImage} />
-            </a>
-          </div> 
+      <div className="cmp-product-lines-grid__item-information">
+        <a href={line.urlProductImage} className="cmp-product-lines-grid__item-information__image-link">
+          <img alt="No Image" src={line.urlProductImage} />
+        </a>
+        <div className="cmp-product-lines-grid__item-information__box-text">
+          <div className="cmp-product-lines-grid__item-information__box-text__header">
+            {line.description}
+          </div>
+          <div className="cmp-product-lines-grid__item-information__box-text__content">
+            <b>MFR#:</b>{line.mfrNumber} |  <b>TD#:</b>{line.tdNumber}
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
 
 export default ProductLinesItemInformation;
+
+
