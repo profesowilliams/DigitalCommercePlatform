59ee1c9984b1a5066bfdf52cfb8ca28f52bfdcd9
Brumoon Ahamat <T892269G@techdata.com>
2021-08-21 12:22:04 +0000

    Merged PR 24590: clean-up of log statements
    Related work items: #196437

--- content/techdata/core/src/main/java/com/techdata/core/models/VendorComponentList.java
+++ content/techdata/core/src/main/java/com/techdata/core/models/VendorComponentList.java
@@ -13,7 +13,6 @@ import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Optional;
 import org.apache.sling.models.annotations.Via;
 import org.apache.sling.models.annotations.injectorspecific.Self;
-import org.apache.sling.models.annotations.via.ForcedResourceType;
 import org.apache.sling.models.annotations.via.ResourceSuperType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -67,10 +66,7 @@ public class VendorComponentList implements List {
                     {
                         FilmStripListItem vi = FilmStripListItem.getProfileListItem(contentFragment, cfListItem.getPath());
                         listOfVendorItems.add(vi);
-
-                        log.debug("this is an executive listing. value of field is {}", isExecutiveListing);
                     }else{
-                        log.debug("this is not an executive listing. value of field is {}", isExecutiveListing);
                         VendorListItem vi = VendorListItem.getVendorListItem(contentFragment, resource, cfListItem);
                         listOfVendorItems.add(vi);
                     }
@@ -79,7 +75,6 @@ public class VendorComponentList implements List {
             }
 
         }
-        log.debug(listOfVendorItems.toString());
         return listOfVendorItems;
     }
 }
