2e3257e79c4ecd0b50c83d4eae5641a80c4b598c
Kamil Karlowicz <Kamil.Karlowicz@techdata.com>
2021-08-31 12:09:30 +0000

    Merged PR 25242: Fix url for product lines in product lines grid
    Fix url for product lines in product lines grid
    
    Related work items: #198621

--- content/techdata/ui.react.frontend/src/global/techdata/components/QuotePreview/ProductLines/ProductLinesItemInformation.jsx
+++ content/techdata/ui.react.frontend/src/global/techdata/components/QuotePreview/ProductLines/ProductLinesItemInformation.jsx
@@ -4,15 +4,27 @@ function ProductLinesItemInformation({ line }) {
   return (
     <section>
       <div className="cmp-product-lines-grid__item-information">
-        <a href={line.urlProductImage} className="cmp-product-lines-grid__item-information__image-link">
+        <a
+          href={line.urlProductSpec}
+          target="_blank"
+          className="cmp-product-lines-grid__item-information__image-link"
+        >
           <img alt="No Image" src={line.urlProductImage} />
         </a>
         <div className="cmp-product-lines-grid__item-information__box-text">
           <div className="cmp-product-lines-grid__item-information__box-text__header">
-            {line.description}
+            <a
+              href={line.urlProductSpec}
+              target="_blank"
+              className="cmp-product-lines-grid__item-information__box-text__header__link"
+            >
+              {line.description}
+            </a>
           </div>
           <div className="cmp-product-lines-grid__item-information__box-text__content">
-            <b>MFR#:</b>{line.mfrNumber} |  <b>TD#:</b>{line.tdNumber}
+            <b>MFR#:</b>
+            {line.mfrNumber} | <b>TD#:</b>
+            {line.tdNumber}
           </div>
         </div>
       </div>
@@ -21,5 +33,3 @@ function ProductLinesItemInformation({ line }) {
 }
 
 export default ProductLinesItemInformation;
-
-
