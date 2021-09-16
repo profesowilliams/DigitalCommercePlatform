335aa1610d943248153239b4492f66c48386904a
Christian Torres <T779427U@techdata.com>
2021-09-16 13:27:37 +0000

    Merged PR 26325: [204269] Quotes-detail:Create authorable fields
    [204269] Quotes-detail:Create authorable fields
    
    Related work items: #204269

--- content/techdata/ui.apps/src/main/content/jcr_root/apps/techdata/components/quotedetail/quotedetail.js
+++ content/techdata/ui.apps/src/main/content/jcr_root/apps/techdata/components/quotedetail/quotedetail.js
@@ -20,18 +20,46 @@ use(function () {
         jsonObject["subheaderLabel"] = properties["subheaderLabel"];
     }
 
+    if (properties && properties["createdDateLabel"]) {
+        jsonObject["createdDateLabel"] = properties["createdDateLabel"];
+    }
+
+    if (properties && properties["expiresDateLabel"]) {
+        jsonObject["expiresDateLabel"] = properties["expiresDateLabel"];
+    }
+
+    if (properties && properties["subTotalLabel"]) {
+        jsonObject["subTotalLabel"] = properties["subTotalLabel"];
+    }
+
+    if (properties && properties["confirmButtonLabel"]) {
+        jsonObject["confirmButtonLabel"] = properties["confirmButtonLabel"];
+    }
+
+    if (properties && properties["makeChoiceLabel"]) {
+        jsonObject["makeChoiceLabel"] = properties["makeChoiceLabel"];
+    }
+
+    if (properties && properties["continueStandardPriceLabel"]) {
+        jsonObject["continueStandardPriceLabel"] = properties["continueStandardPriceLabel"];
+    }
+
     if (properties && properties["resellerContactLabel"]) {
         jsonObject["resellerContactLabel"] = properties["resellerContactLabel"];
     }
+
     if (properties && properties["endUserContactLabel"]) {
         jsonObject["endUserContactLabel"]= properties["endUserContactLabel"];
     }
+
     if (properties && properties["subtotalLabel"]) {
         jsonObject["subtotalLabel"] = properties["subtotalLabel"];
     }
-     if (this.uiServiceDomain != null) {
+
+    if (this.uiServiceDomain !== null) {
     jsonObject["uiServiceEndPoint"] = this.uiServiceDomain+this.quoteDetailEndpoint;
     }
+
     return {
         configJson: JSON.stringify(jsonObject)
     };
