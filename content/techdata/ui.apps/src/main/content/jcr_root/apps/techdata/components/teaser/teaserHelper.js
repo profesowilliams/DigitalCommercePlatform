95f3a349a8249d11f8ae258a3d03bd894e7b9c43
Bala Kondepudi <T552441J@techdata.com>
2021-10-08 21:36:27 +0000

    Merged PR 28211: 209782: analytics changes for image/teaser link
    209782: analytics changes for image/teaser link
    
    Related work items: #209782

---
+++ content/techdata/ui.apps/src/main/content/jcr_root/apps/techdata/components/teaser/teaserHelper.js
@@ -0,0 +1,40 @@
+use(function () {
+    const ANALYTICS_REGION_PN = "analyticsRegion";
+    const ANALYTICS_CATEGORY_PN = "analyticsCategory";
+    const ANALYTICS_NAME_PN = "jcr:title";
+    var dataJson = null;
+    if(this.data)
+     dataJson = JSON.parse(this.data);
+    var jsonObject = new Packages.org.json.JSONObject();
+
+
+    if(dataJson) {
+        var keys = Object.keys(dataJson);
+        if (keys && keys.length > 0)
+        {
+            var firstKey = keys[0];
+            jsonObject.put(firstKey,dataJson[firstKey]);
+            if (properties.get(ANALYTICS_REGION_PN) != null)
+            {
+                jsonObject.getJSONObject(firstKey).put(ANALYTICS_REGION_PN, properties.get(ANALYTICS_REGION_PN));
+            }
+
+            if (properties.get(ANALYTICS_CATEGORY_PN) != null)
+            {
+                jsonObject.getJSONObject(firstKey).put(ANALYTICS_CATEGORY_PN, properties.get(ANALYTICS_CATEGORY_PN));
+            }
+
+            if (properties.get(ANALYTICS_NAME_PN) != null)
+            {
+                jsonObject.getJSONObject(firstKey).put(ANALYTICS_NAME_PN, properties.get(ANALYTICS_NAME_PN));
+            }
+
+        }else{
+            jsonObject = dataJson;
+        }
+    }
+
+    return {
+        dataLayerJson: jsonObject.toString()
+    };
+});
