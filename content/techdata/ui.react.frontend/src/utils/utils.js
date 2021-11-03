0076f35ae392fcc29cf541f1b6b6eb9bc949b567
Bala Kondepudi <T552441J@techdata.com>
2021-11-03 19:21:16 +0000

    Merged PR 29942: 217565: fix to xss input field validations for the form
    217565: fix to xss input field validations for the form
    
    Related work items: #217565

--- content/techdata/ui.react.frontend/src/utils/utils.js
+++ content/techdata/ui.react.frontend/src/utils/utils.js
@@ -50,3 +50,42 @@ export const getImageBuffer =  async imgPath => {
 
     return buffer.constructor.name === 'Buffer' ? buffer : Buffer.from(buffer);
 }
+
+/**
+* Add onload event for form submit to handle input text XSS validations.
+*/
+window.onload = function() {
+    var form = document.getElementsByClassName('cmp-form');
+    if(form) {
+        document.getElementsByClassName("cmp-form-button")[0].insertAdjacentHTML("beforeBegin","<p id='cmp-form-error-block' style='color: red;padding-bottom: 10px;font-size: 11px;font-weight: 700;'></p>");
+        form[0].addEventListener("submit", processForm);
+    }
+}
+/**
+* As part of XSS, validating text inputs on submit of form.
+*/
+function processForm (e) {
+    var formTextElements = document.getElementsByClassName('cmp-form-text__text');
+    var buttonEle = document.getElementsByClassName('cmp-form-button');
+    for (var i=0, max=formTextElements.length; i < max; i++) {
+        var currElement = formTextElements[i];
+        if(!(/^([-@.,;A-Za-z0-9_:/ ]{2,})$/.test(currElement.value)) && buttonEle) {
+            var eleName = currElement.name;
+            document.getElementById("cmp-form-error-block").innerHTML = "Validation Failed for " + eleName;
+            e.preventDefault();
+            return false;
+        }
+    }
+
+    var formTextAreaElements = document.getElementsByClassName('cmp-form-text__textarea');
+    for (var i=0, max=formTextAreaElements.length; i < max; i++) {
+            var currElement = formTextAreaElements[i];
+            if(!(/^([-@.,;A-Za-z0-9_:/ ]{2,})$/.test(currElement.value)) && buttonEle) {
+                var eleName = currElement.name;
+                document.getElementById("cmp-form-error-block").innerHTML = "Validation Failed for " + eleName;
+                e.preventDefault();
+                return false;
+            }
+        }
+    return true;
+};
