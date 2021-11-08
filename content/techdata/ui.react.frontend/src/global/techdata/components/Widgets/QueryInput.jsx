b8a68d01bb4df51d2794ad3b45195b7458bcdb1a
Thomas Sebastian <T205242F@techdata.com>
2021-11-08 12:56:53 +0000

    Merged PR 29762: Ability to restrict the input by x chars
    Ability to restrict the input by x chars
    
    Related work items: #217651

--- content/techdata/ui.react.frontend/src/global/techdata/components/Widgets/QueryInput.jsx
+++ content/techdata/ui.react.frontend/src/global/techdata/components/Widgets/QueryInput.jsx
@@ -5,6 +5,7 @@ import SimpleDropDown from "./SimpleDropDown";
 function QueryInput({ items, placeholder, onQueryChanged, onKeyPress }) {
   const [filterKey, setFilterKey] = useState(null);
   const [filterValue, setFilterValue] = useState(null);
+  const [maxLength, setMaxLength] = useState(524288);
 
   function textInputChanged(value) {
     setFilterValue(value);
@@ -12,6 +13,7 @@ function QueryInput({ items, placeholder, onQueryChanged, onKeyPress }) {
 
   function dropDownValueChanged(item) {
     setFilterKey(item.key);
+    (item.key === 'id') ? setMaxLength(15) : setMaxLength(524288)
   }
 
   useEffect(() => {
@@ -29,6 +31,7 @@ function QueryInput({ items, placeholder, onQueryChanged, onKeyPress }) {
       <input
         className="cmp-query-input__input"
         type="text"
+        maxLength={maxLength}
         placeholder={placeholder}
         onChange={(event) => textInputChanged(event.target.value)}
         onKeyPress={(event) => onKeyPress(isEnterKey(event))}
