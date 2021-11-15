555a2a32a0c185bba717cc5c2e03c3c21c7fe8cc
Thomas Sebastian <T205242F@techdata.com>
2021-11-15 14:08:33 +0000

    Merged PR 30715: location popup on click of footer icon
    location popup on click of footer icon
    
    Related work items: #220119

--- content/techdata/ui.frontend/src/main/webpack/global/td-components/region-select/region-select.js
+++ content/techdata/ui.frontend/src/main/webpack/global/td-components/region-select/region-select.js
@@ -1,23 +1,3 @@
-// (function() {
-//     function regionSelect() {
-//         document.getElementById("regionSelectDropdown").classList.toggle("cmp-show");
-//     }    
-
-//     window.onclick = function(event) {
-//       if (!event.target.matches('.cmp-dropbtn')) {
-
-//         var dropdowns = document.getElementsByClassName("cmp-dropdown-content");
-//         var i;
-//         for (i = 0; i < dropdowns.length; i++) {
-//           var openDropdown = dropdowns[i];
-//           if (openDropdown.classList.contains('cmp-show')) {
-//             openDropdown.classList.remove('cmp-show');
-//           }
-//         }
-//       }
-//     }
-
-// / })();
 (function () {
   "use strict";
   var countriesListModal = document.getElementById("countriesListModal");
@@ -62,6 +42,20 @@
       hideCountriesModal();
       hideRegionDropdown();
     }
+
+    /**
+     * Logic to show popup on footer icon click when enabled via AEM.
+     * The popup will be shown when the click is inside footer icon and
+     * has data-displayLocation attribute set to true.
+     */
+     const isInsideLinkList = event.target.closest(".cmp-link-list__item a");
+     const hasDisplayLocationEnabled =
+       isInsideLinkList &&
+       isInsideLinkList.hasAttribute("data-displayLocation") === true;
+ 
+     if (isInsideLinkList && hasDisplayLocationEnabled) {
+       countriesListPopup();
+     }
 
   });
 
