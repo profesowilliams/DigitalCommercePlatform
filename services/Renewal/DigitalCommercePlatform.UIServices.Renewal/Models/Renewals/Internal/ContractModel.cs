c94d4710227928b01fabed7350da24d1ae56d27d
Arturo Ceron <T598646P@techdata.com>
2021-11-16 19:54:47 +0000

    Merged PR 30951: UIRenewal: Nullable dates in dto and model
    Nullable dates in dto and model
    
    Related work items: #223947

--- services/Renewal/DigitalCommercePlatform.UIServices.Renewal/Models/Renewals/Internal/ContractModel.cs
+++ services/Renewal/DigitalCommercePlatform.UIServices.Renewal/Models/Renewals/Internal/ContractModel.cs
@@ -10,14 +10,14 @@ namespace DigitalCommercePlatform.UIServices.Renewal.Models.Renewals.Internal
         public string Id { get; set; }
         public string Duration { get; set; }
         public string RenewedDuration { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public DateTime NewAgreementStartDate { get; set; }
-        public DateTime NewAgreementEndDate { get; set; }
-        public DateTime NewUsagePeriodStartDate { get; set; }
-        public DateTime NewUsagePeriodEndDate { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public DateTime? NewAgreementStartDate { get; set; }
+        public DateTime? NewAgreementEndDate { get; set; }
+        public DateTime? NewUsagePeriodStartDate { get; set; }
+        public DateTime? NewUsagePeriodEndDate { get; set; }
         public string SupportLevel { get; set; }
         public string ServiceLevel { get; set; }
-        public DateTime UsagePeriod { get; set; }
+        public DateTime? UsagePeriod { get; set; }
     }
 }
