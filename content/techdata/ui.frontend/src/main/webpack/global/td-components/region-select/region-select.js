d1d36a002f2a296cce802e895c09f1f3add81b1f
Thomas Sebastian <T205242F@techdata.com>
2021-11-19 16:28:47 +0000

    Merged PR 31317: location popup refactor
    location popup cleanup part 1 - fixing issue with some cleanup. More refactor required in the future.
    
    Related work items: #224851

--- content/techdata/ui.frontend/src/main/webpack/global/td-components/region-select/region-select.js
+++ content/techdata/ui.frontend/src/main/webpack/global/td-components/region-select/region-select.js
@@ -3,21 +3,29 @@
   var countriesListModal = document.getElementById("countriesListModal");
   if (!countriesListModal) return;
 
-  function regionSelectDropdown() {
-    document.getElementById("regionSelectDropdown").classList.toggle("cmp-show");
+  function showRegionSelectDropdown() {
+    document.getElementById("regionSelectDropdown").classList.add("cmp-show");
   }
 
-  function countriesListPopup() {
-    document.getElementById("countriesListModal").style.display = 'block';
+  function hideRegionSelectDropdown() {
+    document
+      .getElementById("regionSelectDropdown")
+      .classList.remove("cmp-show");
+  }
+
+  function showCountriesListPopup() {
+    showRegionSelectDropdown(); // this is necessary since countries pop-up is inside this div.
+    document.getElementById("countriesListModal").style.display = "block";
   }
 
   function hideCountriesModal() {
-    document.getElementById("countriesListModal").style.display = 'none';
+    hideRegionSelectDropdown();
+    document.getElementById("countriesListModal").style.display = "none";
   }
 
   function onDocumentReady() {
     hideCountriesModal();
-  };
+  }
   document.addEventListener("DOMContentLoaded", onDocumentReady);
 
   function hideRegionDropdown() {
@@ -25,21 +33,26 @@
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('cmp-show')) {
-        openDropdown.classList.remove('cmp-show');
+      if (openDropdown.classList.contains("cmp-show")) {
+        openDropdown.classList.remove("cmp-show");
       }
     }
   }
 
   window.addEventListener("click", function (event) {
-    if (event.target.matches('.cmp-button__region-select')) {
-      regionSelectDropdown(); // unhiding this coz popup is inside this parent
-      countriesListPopup();
-    } if (!event.target.matches('.cmp-button__region-select')) {
+    if (event.target.matches(".cmp-button__region-select")) {
+      showCountriesListPopup();
+    }
+    if (!event.target.matches(".cmp-button__region-select")) {
       hideCountriesModal();
     }
-    if (event.target.matches('.cmp-dropbtn')) { regionSelectDropdown() }
-    if (!event.target.matches('.cmp-dropbtn') && !event.target.matches('.cmp-button__region-select')) {
+    if (event.target.matches(".cmp-dropbtn")) {
+      showRegionSelectDropdown();
+    }
+    if (
+      !event.target.matches(".cmp-dropbtn") &&
+      !event.target.matches(".cmp-button__region-select")
+    ) {
       hideCountriesModal();
       hideRegionDropdown();
     }
@@ -49,21 +62,19 @@
      * The popup will be shown when the click is inside footer icon and
      * has data-displayLocation attribute set to true.
      */
-     const isInsideLinkList = event.target.closest(".cmp-link-list__item a");
+    const isInsideLinkList = event.target.closest(".cmp-link-list__item a");
 
-     /**
-      * Turns out AEM changes camel-cased data attributes to lowercase when 
-      * rendered as HTML. Hence, data-displaylocation is all lowercase.
-      */
-     const hasDisplayLocationEnabled =
-       isInsideLinkList &&
-       (isInsideLinkList.hasAttribute("data-displaylocation") === true ||
-         isInsideLinkList.hasAttribute("data-displayLocation") === true);
- 
-     if (isInsideLinkList && hasDisplayLocationEnabled) {
-       countriesListPopup();
-     }
+    /**
+     * Turns out AEM changes camel-cased data attributes to lowercase when
+     * rendered as HTML. Hence, data-displaylocation is all lowercase.
+     */
+    const hasDisplayLocationEnabled =
+      isInsideLinkList &&
+      (isInsideLinkList.hasAttribute("data-displaylocation") === true ||
+        isInsideLinkList.hasAttribute("data-displayLocation") === true);
 
+    if (isInsideLinkList && hasDisplayLocationEnabled) {
+      showCountriesListPopup();
+    }
   });
-
 })();
