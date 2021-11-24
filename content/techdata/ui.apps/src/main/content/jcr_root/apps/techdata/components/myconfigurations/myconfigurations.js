9d0f47e4fd5afa4816f44cb743394a975e99f13e
Olivera Pavic <T480216M@techdata.com>
2021-11-24 15:07:12 +0000

    Merged PR 31570: Added daysLabel for 'past 30 days'
    Related work items: #225425

--- content/techdata/ui.apps/src/main/content/jcr_root/apps/techdata/components/myconfigurations/myconfigurations.js
+++ content/techdata/ui.apps/src/main/content/jcr_root/apps/techdata/components/myconfigurations/myconfigurations.js
@@ -1,38 +1,45 @@
 "use strict";
 use(function () {
+  var jsonObject = new Packages.org.json.JSONObject();
 
-    var jsonObject = new Packages.org.json.JSONObject();
+  var listValues = [];
+  var resourceResolver = resource.getResourceResolver();
 
-    var listValues = [];
-    var resourceResolver = resource.getResourceResolver();
+  var node = resourceResolver.getResource(
+    currentNode.getPath() + "/labelLinks"
+  );
 
-    var node = resourceResolver.getResource(currentNode.getPath() + "/labelLinks");
+  if (node !== null) {
+    var childrenList = node.getChildren();
 
-    if (node !== null) {
-        var childrenList = node.getChildren();
-
-        for (var [key, res] in Iterator(childrenList)) {
-            var orderLabel = res.properties["orderLabel"];
-            var orderKey = res.properties["orderKey"];
-            var itemData = {};
-            itemData.label = orderLabel;
-            itemData.key = orderKey;
-            listValues.push(itemData);
-
-        }
+    for (var [key, res] in Iterator(childrenList)) {
+      var orderLabel = res.properties["orderLabel"];
+      var orderKey = res.properties["orderKey"];
+      var itemData = {};
+      itemData.label = orderLabel;
+      itemData.key = orderKey;
+      listValues.push(itemData);
     }
-    if (properties.get("label") != null) {
-        jsonObject.put("label", properties.get("label"));
-    }
+  }
+  if (properties.get("label") != null) {
+    jsonObject.put("label", properties.get("label"));
+  }
 
-    if (this.uiServiceDomain != null) {
-        jsonObject.put("endpoint", this.uiServiceDomain+this.myConfigurationsEndpoint);
-    }
+  if (properties.get("daysLabel") != null) {
+    jsonObject.put("daysLabel", properties.get("daysLabel"));
+  }
 
-    if (listValues != null) {
-        jsonObject.put("orderLabels", listValues);
-    }
-    return {
-        listValues: jsonObject.toString()
-    };
+  if (this.uiServiceDomain != null) {
+    jsonObject.put(
+      "endpoint",
+      this.uiServiceDomain + this.myConfigurationsEndpoint
+    );
+  }
+
+  if (listValues != null) {
+    jsonObject.put("orderLabels", listValues);
+  }
+  return {
+    listValues: jsonObject.toString(),
+  };
 });
