ac42f8ea54ec4b43452bb22b915bbaafea967c48
Andres Rizzo <T136989D@techdata.com>
2021-12-01 13:07:14 +0000

    Merged PR 32107: BUG[224026]: Quote creation logic
    Error handling for required fields on end user and logic for quote creation
    
    Related work items: #224384, #224388

--- content/techdata/ui.react.frontend/src/global/techdata/components/QuotePreview/ConfigGrid/EndUserInfo.jsx
+++ content/techdata/ui.react.frontend/src/global/techdata/components/QuotePreview/ConfigGrid/EndUserInfo.jsx
@@ -3,7 +3,7 @@ import Info from '../../common/quotes/DisplayItemInfo';
 import Button from '../../Widgets/Button';
 
 
-function EndUserInfo({endUser, info, onValueChange}) {
+function EndUserInfo({endUser, info, onValueChange, handleQuickQuote}) {
     endUser = endUser && endUser[0];
     const initialState = {
         companyName: endUser?.companyName || '',
@@ -34,26 +34,50 @@ function EndUserInfo({endUser, info, onValueChange}) {
     };
 
     const handleSaveChanges = () => {
-        onValueChange(infoState);
         closeEditMode();
+        onValueChange(infoState);
     };
 
     const handleCancelChanges = () => {
         setInfoState(initialState);
         closeEditMode();
     };
+
+    const requiredValues = infoState
+    // Removing line2 from this validation since is an optional Value.
+    if(infoState.line2 === ''){
+        delete infoState.line2
+    } else {
+        null
+    }
+   
+    const values = Object.values(requiredValues).some(value => value === null || value === '')
+
+    const handleRequiredElements = () => {
+        if (values){
+            return (
+                <>
+                    <span className='errorInput__label'>Please complete the missing fields</span> 
+                </>)
+        } else {
+            null
+        }
+    }
+
     return (
         <div className="cmp-qp__enduser-info">
             <p onClick={handleEditModeChange} className="cmp-qp__enduser-info--title">{info.endUserHeaderLabel}</p>
                 {!editMode && (
                     <>
-                        <p className="cmp-qp__enduser-info--sub-title">{infoState.companyName}</p>
+                        <p label={info.companyLabel} className="cmp-qp__enduser-info--sub-title">{infoState.companyName}</p>
                         <div className="cmp-qp__enduser-info--address-group">
-                            <Info>{infoState.name}</Info>
-                            <Info>{infoState.line1}</Info>
-                            <Info>{infoState.line2}</Info>
-                            <Info >{infoState.city}, {infoState.state} {infoState.zip}</Info>
-                            <Info>{infoState.country}</Info>
+                            <Info label={info.nameLabel}>{infoState.name}</Info>
+                            <Info label={`${info.addressLabel} 1`}>{infoState.line1}</Info>
+                            <Info label={`${info.addressLabel} 2`}>{infoState.line2}</Info>
+                            <Info label={info.cityLabel}>{infoState.city}</Info>
+                            <Info label={info.stateLabel}>{infoState.state}</Info>
+                            <Info label={info.zipLabel}>{infoState.zip}</Info>
+                            <Info label={info.countryLabel}>{infoState.country}</Info>
                             <p>
                                 <Info label={info.emailLabel}>{infoState.email}</Info>
                                 <Info label={info.phoneLabel}>{infoState.phone}</Info>
@@ -63,11 +87,12 @@ function EndUserInfo({endUser, info, onValueChange}) {
                 )}
                 {editMode && (
                     <div className="cmp-qp__edit-mode">
+                        {handleRequiredElements()}
                         <form>
                             <div className="form-check">
                                 <label htmlFor="companyName">{info.companyLabel}</label>
                                 <input
-                                    className="field element"
+                                    className={infoState.companyName === '' ? "field element errorInput" : "field element"}
                                     value={infoState.companyName}
                                     name="companyName"
                                     id="companyName"
@@ -77,7 +102,7 @@ function EndUserInfo({endUser, info, onValueChange}) {
                             <div className="form-check">
                                 <label htmlFor="name">{info.nameLabel}</label>
                                 <input
-                                    className="field element"
+                                    className={infoState.name === '' ? "field element errorInput" : "field element"}
                                     value={infoState.name}
                                     name="name"
                                     id="name"
@@ -87,7 +112,7 @@ function EndUserInfo({endUser, info, onValueChange}) {
                             <div className="form-check">
                                 <label htmlFor="line1">{info.addressLabel + ' 1'}</label>
                                 <input
-                                    className="field element"
+                                    className={infoState.line1 === '' ? "field element errorInput" : "field element"}
                                     value={infoState.line1}
                                     name="line1"
                                     id="line1"
@@ -107,7 +132,7 @@ function EndUserInfo({endUser, info, onValueChange}) {
                             <div className="form-check">
                                 <label htmlFor="city">{info.cityLabel}</label>
                                 <input
-                                    className="field element"
+                                    className={infoState.city === '' ? "field element errorInput" : "field element"}
                                     value={infoState.city}
                                     name="city"
                                     id="city"
@@ -117,7 +142,7 @@ function EndUserInfo({endUser, info, onValueChange}) {
                             <div className="form-check">
                                 <label htmlFor="state">{info.stateLabel}</label>
                                 <input
-                                    className="field element"
+                                    className={infoState.state === '' ? "field element errorInput" : "field element"}
                                     value={infoState.state}
                                     name="state"
                                     id="state"
@@ -127,7 +152,7 @@ function EndUserInfo({endUser, info, onValueChange}) {
                             <div className="form-check">
                                 <label htmlFor="zip">{info.zipLabel}</label>
                                 <input
-                                    className="field element"
+                                    className={infoState.zip === '' ? "field element errorInput" : "field element"}
                                     value={infoState.zip}
                                     name="zip"
                                     id="zip"
@@ -137,7 +162,7 @@ function EndUserInfo({endUser, info, onValueChange}) {
                             <div className="form-check">
                                 <label htmlFor="country">{info.countryLabel}</label>
                                 <input
-                                    className="field element"
+                                    className={infoState.country === '' ? "field element errorInput" : "field element"}
                                     value={infoState.country}
                                     name="country"
                                     id="country"
@@ -147,7 +172,7 @@ function EndUserInfo({endUser, info, onValueChange}) {
                             <div className="form-check">
                                 <label htmlFor="email">{info.emailLabel}</label>
                                 <input
-                                    className="field element"
+                                    className={infoState.email === '' ? "field element errorInput" : "field element"}
                                     value={infoState.email}
                                     name="email"
                                     id="email"
@@ -157,7 +182,7 @@ function EndUserInfo({endUser, info, onValueChange}) {
                             <div className="form-check">
                                 <label htmlFor="phone">{info.phoneLabel}</label>
                                 <input
-                                    className="field element"
+                                    className={infoState.phone === '' ? "field element errorInput" : "field element"}
                                     value={infoState.phone}
                                     name="phone"
                                     id="phone"
@@ -165,7 +190,7 @@ function EndUserInfo({endUser, info, onValueChange}) {
                                     type="text"/>
                             </div>
                             <div className="form-group">
-                                <Button btnClass="cmp-qp--save-information" disabled={false} onClick={handleSaveChanges}>
+                                <Button btnClass="cmp-qp--save-information" disabled={values ? true : false} onClick={handleSaveChanges}>
                                     {info.submitLabel}
                                 </Button>
                                 <Button btnClass="cmp-qp--cancel-information" disabled={false} onClick={handleCancelChanges}>
