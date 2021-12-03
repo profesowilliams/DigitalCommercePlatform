b421740f2bc1142841de524435e2fe90db29fef3
Gil Marcano <T679591R@techdata.com>
2021-12-03 21:07:57 +0000

    Merged PR 32388: [222927] subheader: closes menu when clicking outside the menu (fixed with header click)
    Related work items: #222927

--- content/techdata/ui.frontend/src/main/webpack/global/td-components/sub-header/subHeader.js
+++ content/techdata/ui.frontend/src/main/webpack/global/td-components/sub-header/subHeader.js
@@ -2,14 +2,37 @@ export const subHeader = () => {
     document.addEventListener("click", () => toolsToggle(event));
 
     function toolsToggle(event) {
-        const isntMenu = event.target.nodeName === "DIV";
-        const isTools = event.target.closest(`[data-tab-name]`)
+        let isntMenu;
+        const isSubHeader = event.target.parentNode.parentNode.classList[0] === "cmp-tabs";
+        const isMenu = event.target.parentNode.classList[0] === "cmp-tools";
+        const isMenuBlank = event.target.parentNode.classList[0] === "menucategorieslist";
+        if (isSubHeader || isMenu || isMenuBlank) {
+            isntMenu = false;
+        } else {
+            isntMenu = true;
+        }
+        const isTools = event.target.closest(`[data-tab-name]`);
         let uniqueId = isTools?.getAttribute('id');
-
         if (isntMenu) {
             // Search tag menu that does have class active and removed it to close the menu 
             // when clicking outside the menu and subheader
             
+            // For OLD Subheader
+            const oldMenu = document.getElementsByClassName('cmp-tabs__tabpanel--active cmp-tabs__tabpanel--custom-active')[0];
+            if (oldMenu) {
+                oldMenu?.classList.remove("cmp-tabs__tabpanel--active", "cmp-tabs__tabpanel--custom-active");
+            }
+
+            // For New Subheader
+            const newMenu = document.getElementsByClassName('menucategorieslist')[0];
+            if (newMenu) {
+                newMenu.style.display = "none";
+            }
+
+        if (isntMenu) {
+            // Search tag menu that does have class active and removed it to close the menu 
+            // when clicking outside the menu and subheader
+
             // For OLD Subheader
             const oldMenu = document.getElementsByClassName('cmp-tabs__tabpanel--active cmp-tabs__tabpanel--custom-active')[0];
             if (oldMenu) {
