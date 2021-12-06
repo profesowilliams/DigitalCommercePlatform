5019a08563d7b4a4dc4a9426b45067f191716916
Harris, Robert <Robert.Harris@techdata.com>
2021-12-06 20:04:02 +0000

    Merged PR 32475: Reimplement UI-Service headers from AEM/React
    - Merged PR 32375: #227725 Set service headers for traceId and Consumer
    - Add required import for timestamps
    
    Related work items: #227725

--- content/techdata/core/src/main/java/com/techdata/core/util/impl/UIServiceHelperImpl.java
+++ content/techdata/core/src/main/java/com/techdata/core/util/impl/UIServiceHelperImpl.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.Instant;
 
 @Component(service = UIServiceHelper.class)
 public class UIServiceHelperImpl implements  UIServiceHelper {
@@ -37,9 +38,9 @@ public class UIServiceHelperImpl implements  UIServiceHelper {
             conn.setReadTimeout(5 * 1000);
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
-            conn.setRequestProperty("TraceId", "35345345-Browse");
+            conn.setRequestProperty("TraceId", "AEM_" + Instant.now().toString());
             conn.setRequestProperty("Site", "NA");
-            conn.setRequestProperty("Consumer", "NA");
+            conn.setRequestProperty("Consumer", "AEM");
             conn.setRequestProperty("Accept-Language", "en-us");
             conn.setRequestProperty("sessionid", sessionID);
             if (conn.getResponseCode() != 200) {
