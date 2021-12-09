f5734035f03653ec5ebb926dad4e73a45e87029d
Brumoon Ahamat <T892269G@techdata.com>
2021-12-09 14:55:08 +0000

    Merged PR 32680: removing unsupported methods in the FormData object
    Related work items: #228169

--- content/techdata/ui.apps/src/main/content/jcr_root/apps/techdata/components/form/form-post-container/clientlib-form-site/js/site.js
+++ content/techdata/ui.apps/src/main/content/jcr_root/apps/techdata/components/form/form-post-container/clientlib-form-site/js/site.js
@@ -1,18 +1,33 @@
 var submitButton = document.getElementById("formSubmit");
 var apacForm = document.getElementById("apac-form");
 
+function createFormData(form)
+{
+    var newData    = new FormData();
+    var inputs     = form.getElementsByTagName('input');
+    var selects     = form.getElementsByTagName('input');
+    var inputsList = Array.prototype.slice.call(inputs);
+
+    inputsList.forEach(
+        function (i) {
+            if (!i.name.startsWith(":formstart") && !i.name.startsWith("_charset_")) {
+                newData.append(i.name, i.value);
+            }
+        }
+    );
+
+    return newData;
+}
+
 if (submitButton) {
 
     submitButton.addEventListener("click",(e) => {
-        let data = new FormData(apacForm);
-        data.delete(":formstart");
-        data.delete("_charset_");
+        let data = createFormData(apacForm);
         let endPoint = "/bin/form";
-        let newFormData = new FormData();
         var xhr = new XMLHttpRequest();
         xhr.open("POST", endPoint, true);
         xhr.send(data);
     });
 }else {
-    console.log("submit button null");
+    console.error("Submit Button Not Configured.");
 }
