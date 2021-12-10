549b0ca4c974d69ca658d29df86193d8ae0bb66f
Christian Torres <T779427U@techdata.com>
2021-12-10 22:34:08 +0000

    Merged PR 32786: Add plainResellerColumn on the select under columnList in aem
    Add plainResellerColumn on the select under columnList in aem
    
    Related work items: #226493

--- content/techdata/ui.react.frontend/src/global/techdata/components/RenewalsGrid/GenericColumnTypes.jsx
+++ content/techdata/ui.react.frontend/src/global/techdata/components/RenewalsGrid/GenericColumnTypes.jsx
@@ -1,3 +1,4 @@
+
 export const dateColumn = ({ columnLabel, columnKey, sortable = false }) => (
 {
     headerName: columnLabel,
@@ -22,18 +23,36 @@ export const plainTextColumn = ({ columnLabel, columnKey, sortable = false }) =>
 {
     headerName: columnLabel,
     field: columnKey,
-    sortable: sortable
+    sortable: sortable,
 })
+
+export const plainResellerColumnFn = ({ columnLabel, columnKey, sortable}) => {
+    // check if Its a reseller or techdata employee
+    //refactor needed
+    const testVariable = false;
+    if (testVariable) {
+        return { 
+            headerName: columnLabel,
+            field: columnKey,
+            sortable: sortable,
+        }
+    } else {
+        return null;
+    }
+}
 
 const columnTypes = {
     "date": dateColumn,
     "plainText": plainTextColumn,
-    "buttonList": plainTextColumn
+    "buttonList": plainTextColumn,
+    "plainResellerColumn": plainResellerColumnFn
 }
 
 export const getColumnDefinitions = (originalDefinitions) => {
-    return originalDefinitions.map((definition) => {
+    const colDefs = originalDefinitions.map((definition) => {
         console.log(definition.type, columnTypes[definition.type], columnTypes)
         return columnTypes[definition.type](definition);
     });
+    return colDefs.filter(columnDef => columnDef !== null)
 }
+
