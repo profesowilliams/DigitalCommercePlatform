63df75fdc91350705a0a6b68d7f20f556c6f83ed
Dudek, Damian <Damian.Dudek@techdata.com>
2021-12-16 10:21:54 +0000

    Merged PR 33238: XS⚠️ ◾ 228529-Search UI Service - Add Web Promo Flag
    228529-Search UI Service - Add Web Promo Flag
    
    Related work items: #228529

--- services/Search/DigitalCommercePlatform.UIServices.Search/Dto/FullSearch/Internal/PriceDto.cs
+++ services/Search/DigitalCommercePlatform.UIServices.Search/Dto/FullSearch/Internal/PriceDto.cs
@@ -5,8 +5,9 @@ namespace DigitalCommercePlatform.UIServices.Search.Dto.FullSearch.Internal
 {
     public class PriceDto : ElasticPriceDto
     {
-        public decimal? BestPrice { get; set; }
         public decimal? BasePrice { get; set; }
+        public decimal? BestPrice { get; set; }
         public DateTime? BestPriceExpiration { get; set; }
+        public bool? BestPriceIncludesWebDiscount { get; set; }
     }
 }
--- services/Search/DigitalCommercePlatform.UIServices.Search/Models/FullSearch/Internal/PriceModel.cs
+++ services/Search/DigitalCommercePlatform.UIServices.Search/Models/FullSearch/Internal/PriceModel.cs
@@ -8,10 +8,11 @@ namespace DigitalCommercePlatform.UIServices.Search.Models.FullSearch.Internal
     [ExcludeFromCodeCoverage]
     public class PriceModel : ElasticPriceModel
     {
-        public decimal? BestPrice { get; set; }
         public decimal? BasePrice { get; set; }
-        public decimal? PromoAmount { get; set; }
+        public decimal? BestPrice { get; set; }
         public DateTime? BestPriceExpiration { get; set; }
+        public bool? BestPriceIncludesWebDiscount { get; set; }
+        public decimal? PromoAmount { get; set; }
         public List<VolumePricing> VolumePricing { get; set; }
     }
 }
