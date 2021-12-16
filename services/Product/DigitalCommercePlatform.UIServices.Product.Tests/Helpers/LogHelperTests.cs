20e42a7d5466733d0374696f2851d692ec801a73
Adam Wojnowski <Adam.Wojnowski@techdata.com>
2021-12-16 15:21:38 +0000

    Merged PR 33261: XS✔ ◾ Added ProductProfileTests and ProductServiceTests
    Added ProductProfileTests and ProductServiceTests
    
    Related work items: #230761

---
+++ services/Product/DigitalCommercePlatform.UIServices.Product.Tests/Helpers/LogHelperTests.cs
@@ -0,0 +1,34 @@
+﻿//2021 (c) Tech Data Corporation -. All Rights Reserved.
+using DigitalCommercePlatform.UIServices.Order.Helpers;
+using DigitalFoundation.Common.TestUtilities;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using System;
+using Xunit;
+
+namespace DigitalCommercePlatform.UIServices.Product.Tests.Helpers
+{
+    public class LogHelperTests
+    {
+        [Theory]
+        [AutoDomainData]
+        public void ExcludeHealthChecksReturnsError(HttpContext httpContext)
+        {
+            //Arrange and Act
+            var response = LogHelper.ExcludeHealthChecks(httpContext, 0, new ArgumentException("test exception"));
+            //Assert
+            response.Should().Be(Serilog.Events.LogEventLevel.Error);
+
+        }
+
+        [Fact]
+        public void ExcludeHealthChecksReturnsInformation()
+        {
+            //Arrange and Act
+            var response = LogHelper.ExcludeHealthChecks(new DefaultHttpContext(), 0, null);
+            //Assert
+            response.Should().Be(Serilog.Events.LogEventLevel.Information);
+
+        }
+    }
+}
