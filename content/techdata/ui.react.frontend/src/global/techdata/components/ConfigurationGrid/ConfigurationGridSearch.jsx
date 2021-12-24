5d702fa89421e5db91be9c9067e84d9a087d3c03
Hector Romero <T175389E@techdata.com>
2021-12-24 14:45:48 +0000

    Merged PR 33786: Fix Configurations datepicker selecting previous day
    Fix Configurations datepicker selecting previous day
    
    Related work items: #235101

--- content/techdata/ui.react.frontend/src/global/techdata/components/ConfigurationGrid/ConfigurationGridSearch.jsx
+++ content/techdata/ui.react.frontend/src/global/techdata/components/ConfigurationGrid/ConfigurationGridSearch.jsx
@@ -58,13 +58,13 @@ function ConfigurationGridSearch({
     let from =
       query.from?.key && query.from?.value
         ? `&createdFrom=${new Date(
-            query.from.value.setUTCHours(0, 0, 0)
+            Date.UTC(query.from.value.getFullYear(),query.from.value.getMonth(), query.from.value.getDate())
           ).toISOString()}`
         : "";
     let to =
       query.to?.key && query.to?.value
         ? `&createdTo=${new Date(
-            query.to.value.setUTCHours(23, 59, 59)
+            new Date(Date.UTC(query.to.value.getFullYear(),query.to.value.getMonth(), query.to.value.getDate())).setUTCHours(23, 59, 59)
           ).toISOString()}`
         : "";
     let concatedQuery = `${keyword}${configurations}${from}${to}`;
@@ -114,14 +114,14 @@ function ConfigurationGridSearch({
         pickerKey={"from"}
         placeholder={config.datePlaceholder}
         label={config.fromLabel}
-        forceZeroUTC={true}
+        forceZeroUTC={false}
         onSelectedDateChanged={(change) => handleFilterChange(change, "from")}
       ></SimpleDatePicker>
       <SimpleDatePicker
         pickerKey={"to"}
         placeholder={config.datePlaceholder}
         label={config.toLabel}
-        forceZeroUTC={true}
+        forceZeroUTC={false}
         onSelectedDateChanged={(change) => handleFilterChange(change, "to")}
       ></SimpleDatePicker>
     </div>
