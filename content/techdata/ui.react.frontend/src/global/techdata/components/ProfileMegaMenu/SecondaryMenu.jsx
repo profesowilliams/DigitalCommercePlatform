8377fa0d5bc50a1b58fb236c15d8b44b3e28cc7c
Nikhila Janga <T881361W@techdata.com>
2021-12-31 13:01:52 +0000

    Merged PR 34064: Analytics tracking for ProfileDropdown For mobile and Desktop Version
    Task: 236148 Analytics tracking for ProfileDropdown For mobile and Desktop Version
    
    Related work items: #236148

--- content/techdata/ui.react.frontend/src/global/techdata/components/ProfileMegaMenu/SecondaryMenu.jsx
+++ content/techdata/ui.react.frontend/src/global/techdata/components/ProfileMegaMenu/SecondaryMenu.jsx
@@ -1,48 +1,82 @@
-import React, {useState} from 'react';
-import TertiaryMenu from '../ProfileMegaMenu/TertiaryMenu';
-import FontIcon from '../Widgets/FontIcon';
+import React, { useState } from "react";
+import TertiaryMenu from "../ProfileMegaMenu/TertiaryMenu";
+import FontIcon from "../Widgets/FontIcon";
 import { hasDCPAccess } from "../../../../utils/user-utils";
 
-function SecondaryMenu({secondaryData, userData, handleBackBtnClick}) {
-    const [isOpen, setIsOpen] = useState(null);
+function SecondaryMenu({ secondaryData, userData, handleBackBtnClick }) {
+  const [isOpen, setIsOpen] = useState(null);
 
-    const handleSecondaryClick = (event, index, hasChild) => {
-        if (hasChild)
-            event.preventDefault();
-        event.stopPropagation();
-        setIsOpen(prevIsOpen => {
-            if (prevIsOpen === index) return null;
-            return prevIsOpen = index
-        })
-    }
+  const handleSecondaryClick = (event, index, hasChild) => {
+    if (hasChild) event.preventDefault();
+    event.stopPropagation();
+    setIsOpen((prevIsOpen) => {
+      if (prevIsOpen === index) return null;
+      return (prevIsOpen = index);
+    });
+  };
 
-    return (
-        <div className="cmp-sign-in-secondary">
-            <div className="cmp-sign-in-secondary--title">{secondaryData.primaryLabel}</div>
-            <div className="cmp-sign-in-secondary--sub-title">{secondaryData.primaryLabel + " home"}</div>
-            <span className="cmp-sign-in-secondary__back" onClick={handleBackBtnClick} >
-                <FontIcon iconClassName="fa-chevron-left" />
-            </span>
-            <ul className="cmp-sign-in-group">
-                {secondaryData.secondaryMenus.items.map((item, index) => {
-                    return (
-                        <li onClick={() => handleSecondaryClick(event, index, item.tertiaryMenus)} key={Symbol(item.secondaryLabel).toString()} className={`cmp-sign-in__item cmp-sign-in__level-1 ${item.tertiaryMenus ? 'has-child' : ''} ${(isOpen === index) ? 'active' : ''}`}>
-                            <a href={hasDCPAccess(userData) ? item.secondaryDcpLink : item.secondaryLink}>
-                                {item.secondaryLabel}
-                            </a>
-                            {
-                                item.tertiaryMenus ? (
-                                    <TertiaryMenu
-                                        userData={userData}
-                                        tertiaryData={item.tertiaryMenus}/>
-                                ) : null
-                            }
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    )
+  const handleSecondaryLinkClick = (linkUrl, dcpLink, linkTitle) => {
+    let finalLinkUrl = hasDCPAccess(userDataCheck) ? dcpLink : linkUrl;
+    if(finalLinkUrl != undefined){
+        DataLayerUtils.pushEvent("click", {
+            name: linkTitle,
+            selectionDepth: "",
+            type: "link",
+            category: "profile dropdown",
+          });
+        window.location.href = finalLinkUrl
+      }
+  };
+
+  return (
+    <div className="cmp-sign-in-secondary">
+      <div className="cmp-sign-in-secondary--title">
+        {secondaryData.primaryLabel}
+      </div>
+      <div className="cmp-sign-in-secondary--sub-title">
+        {secondaryData.primaryLabel + " home"}
+      </div>
+      <span
+        className="cmp-sign-in-secondary__back"
+        onClick={handleBackBtnClick}
+      >
+        <FontIcon iconClassName="fa-chevron-left" />
+      </span>
+      <ul className="cmp-sign-in-group">
+        {secondaryData.secondaryMenus.items.map((item, index) => {
+          return (
+            <li
+              onClick={() =>
+                handleSecondaryClick(event, index, item.tertiaryMenus)
+              }
+              key={Symbol(item.secondaryLabel).toString()}
+              className={`cmp-sign-in__item cmp-sign-in__level-1 ${
+                item.tertiaryMenus ? "has-child" : ""
+              } ${isOpen === index ? "active" : ""}`}
+            >
+              <a
+                onClick={() =>
+                  handleSecondaryLinkClick(
+                    item.secondaryLink,
+                    item.secondaryDcpLink,
+                    item.secondaryLabel
+                  )
+                }
+              >
+                {item.secondaryLabel}
+              </a>
+              {item.tertiaryMenus ? (
+                <TertiaryMenu
+                  userData={userData}
+                  tertiaryData={item.tertiaryMenus}
+                />
+              ) : null}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default SecondaryMenu;
