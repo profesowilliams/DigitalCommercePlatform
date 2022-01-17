57389fd1f3e49ca394ae83a43fdccba5a5feb995
Jacek Pietrzyk <Jacek.Pietrzyk@techdata.com>
2022-01-17 15:34:34 +0000

    Merged PR 34967: unit testing
    unit testing
    
    Related work items: #234870

--- services/Account/DigitalCommercePlatform.UIServices.Account.Tests/Services/AccountServiceTests.cs
+++ services/Account/DigitalCommercePlatform.UIServices.Account.Tests/Services/AccountServiceTests.cs
@@ -3,7 +3,9 @@ using AutoMapper;
 using DigitalCommercePlatform.UIServices.Account.Actions.ActionItemsSummary;
 using DigitalCommercePlatform.UIServices.Account.Actions.ConfigurationsSummary;
 using DigitalCommercePlatform.UIServices.Account.Actions.CustomerAddress;
+using DigitalCommercePlatform.UIServices.Account.Actions.DealsSummary;
 using DigitalCommercePlatform.UIServices.Account.Actions.GetMyQuotes;
+using DigitalCommercePlatform.UIServices.Account.Actions.MyOrders;
 using DigitalCommercePlatform.UIServices.Account.Actions.TopConfigurations;
 using DigitalCommercePlatform.UIServices.Account.Actions.TopOrders;
 using DigitalCommercePlatform.UIServices.Account.Actions.TopQuotes;
@@ -16,7 +18,9 @@ using DigitalCommercePlatform.UIServices.Account.Services;
 using DigitalFoundation.Common.Features.Client;
 using DigitalFoundation.Common.Features.Client.Exceptions;
 using DigitalFoundation.Common.Features.Contexts;
+using DigitalFoundation.Common.Features.Contexts.Models;
 using DigitalFoundation.Common.Providers.Settings;
+using DigitalFoundation.Common.Services.Layer.UI.ExceptionHandling;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -54,6 +58,8 @@ namespace DigitalCommercePlatform.UIServices.Account.Tests.Services
             _uiContext = new Mock<IUIContext>();
         }
 
+        #region GetConfigurationsSummaryAsync
+
         [Fact]
         public async Task GetConfigurationsSummaryAsync_Test()
         {
@@ -66,6 +72,59 @@ namespace DigitalCommercePlatform.UIServices.Account.Tests.Services
             //Assert
             Assert.NotNull(result);
         }
+
+        [Fact]
+        public async Task GetConfigurationsSummaryAsync_ClientThrowsException_NotFoundProperlyCatched()
+        {
+            //Arrange
+            GetConfigurationsSummary.Request request = new();
+
+            _middleTierHttpClient.Setup(t => t.GetAsync<Response>(It.IsAny<string>(),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()))
+                .Throws(new RemoteServerHttpException("", System.Net.HttpStatusCode.NotFound, new object()));
+
+            //Act
+            var sut = GetService();
+            var result = await sut.GetConfigurationsSummaryAsync(request);
+
+            //Assert
+            result.Should().BeEquivalentTo(new ConfigurationsSummaryModel
+            {
+                Quoted = 0,
+                UnQuoted = 0,
+                OldConfigurations = 0,
+                CurrencyCode = "USD"
+            });
+        }
+
+        [Fact]
+        public async Task GetConfigurationsSummaryAsync_ClientThrowsException_MethodThrowsAnother()
+        {
+            //Arrange
+            GetConfigurationsSummary.Request request = new();
+
+            var innerException = new RemoteServerHttpException();
+
+            _middleTierHttpClient.Setup(t => t.GetAsync<Response>(It.IsAny<string>(),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()))
+                .Throws(new RemoteServerHttpException("", System.Net.HttpStatusCode.BadRequest, new object(), innerException));
+
+            var user = new User() { ID = "123" };
+            _uiContext.Setup(t => t.User).Returns(user);
+
+            //Act
+            var sut = GetService();
+            var act = () => sut.GetConfigurationsSummaryAsync(request);
+
+            //Assert
+            await Assert.ThrowsAsync<UIServiceException>(act);
+        }
+
+        #endregion GetConfigurationsSummaryAsync
 
         #region GetAddress Unit Tests
 
@@ -712,6 +771,385 @@ namespace DigitalCommercePlatform.UIServices.Account.Tests.Services
         }
 
         #endregion GetActionItemsSummaryAsync Unit Tests
+
+        #region GetMyOrdersStatusAsync Unit Tests
+
+        [Fact]
+        public async Task GetMyOrdersStatusAsync_RunsClientWithValidUrl()
+        {
+            //Arrange
+            var request = new GetMyOrdersStatus.Request()
+            {
+                FromDate = new DateTime(2020, 1, 1),
+                ToDate = new DateTime(2020, 1, 1)
+            };
+
+            _middleTierHttpClient.Setup(t => t.GetAsync<OrderStatsDto>(
+                It.IsAny<string>(),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()))
+                .ReturnsAsync(new OrderStatsDto());
+
+            var urlExpected = "tats?key=status&createdFrom=2020-01-01&createdTo=2020-01-01";
+
+            //Act
+            var sut = GetService();
+            var result = await sut.GetMyOrdersStatusAsync(request);
+
+            //Assert
+            _middleTierHttpClient.Verify(x => x.GetAsync<OrderStatsDto>(
+                It.Is<string>(s => s.EndsWith(urlExpected)),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task GetMyOrdersStatusAsync_NoOrdersMatching_ReturnsEmptyResponseObject()
+        {
+            //Arrange
+            var request = new GetMyOrdersStatus.Request()
+            {
+                FromDate = new DateTime(2020, 1, 1),
+                ToDate = new DateTime(2020, 1, 1)
+            };
+
+            _middleTierHttpClient.Setup(t => t.GetAsync<OrderStatsDto>(
+                It.IsAny<string>(),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()))
+                .ReturnsAsync(new OrderStatsDto() { Data = null });
+
+            //Act
+            var sut = GetService();
+            var result = await sut.GetMyOrdersStatusAsync(request);
+
+            //Assert
+            result.Should().BeEquivalentTo(new MyOrdersStatusDashboard());
+        }
+
+        [Fact]
+        public async Task GetMyOrdersStatusAsync_NoOrdersInUSD_ReturnsEmptyResponseObject()
+        {
+            //Arrange
+            var request = new GetMyOrdersStatus.Request()
+            {
+                FromDate = new DateTime(2020, 1, 1),
+                ToDate = new DateTime(2020, 1, 1)
+            };
+
+            OrderStatsDataDto[] data = new OrderStatsDataDto[1];
+            data[0] = new OrderStatsDataDto() { Currency = "PLN" };
+
+            _middleTierHttpClient.Setup(t => t.GetAsync<OrderStatsDto>(
+                It.IsAny<string>(),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()))
+                .ReturnsAsync(new OrderStatsDto()
+                {
+                    Data = data
+                });
+
+            //Act
+            var sut = GetService();
+            var result = await sut.GetMyOrdersStatusAsync(request);
+
+            //Assert
+            result.Should().BeEquivalentTo(new MyOrdersStatusDashboard());
+        }
+
+        [Fact]
+        public async Task GetMyOrdersStatusAsync_OrdersInUSD_ReturnsValidResponseObject()
+        {
+            //Arrange
+            var request = new GetMyOrdersStatus.Request()
+            {
+                FromDate = new DateTime(2020, 1, 1),
+                ToDate = new DateTime(2020, 1, 1)
+            };
+
+            OrderStatsDataDto[] data = new OrderStatsDataDto[2];
+            data[0] = new OrderStatsDataDto() { Currency = "USD", Value = "IN_PROCESS", Count = "2" };
+            data[1] = new OrderStatsDataDto() { Currency = "USD", Value = "ON_HOLD" };
+
+            _middleTierHttpClient.Setup(t => t.GetAsync<OrderStatsDto>(
+                It.IsAny<string>(),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()))
+                .ReturnsAsync(new OrderStatsDto()
+                {
+                    Data = data
+                });
+
+            //Act
+            var sut = GetService();
+            var result = await sut.GetMyOrdersStatusAsync(request);
+
+            //Assert
+            result.Should().BeEquivalentTo(new MyOrdersStatusDashboard
+            {
+                InProcess = "2",
+                OnHold = "0",
+                Shipped = "0"
+            });
+        }
+
+        #endregion GetMyOrdersStatusAsync Unit Tests
+
+        #region GetMyOrdersSummaryAsync Unit Tests
+
+        [Fact]
+        public async Task GetMyOrdersSummaryAsync_IsMonthlyFalse_RunsClientWithValidUrl()
+        {
+            //Arrange
+            var request = new GetMyOrders.Request()
+            {
+                IsMonthly = false
+            };
+
+            _middleTierHttpClient.Setup(t => t.GetAsync<OrderStatsDto>(
+                It.IsAny<string>(),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()))
+                .ReturnsAsync(new OrderStatsDto());
+
+            var createdFrom = DateTime.Now.Date.AddDays(-90).ToString("yyyy-MM-dd");
+            var createdTo = DateTime.Now.Date.AddHours(23).AddMinutes(59).AddSeconds(59).ToString("yyyy-MM-dd");
+            var urlExpected = $"stats?key=status&createdFrom={createdFrom}&createdTo={createdTo}";
+
+            //Act
+            var sut = GetService();
+            var result = await sut.GetMyOrdersSummaryAsync(request);
+
+            //Assert
+            _middleTierHttpClient.Verify(x => x.GetAsync<OrderStatsDto>(
+                It.Is<string>(s => s.EndsWith(urlExpected)),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task GetMyOrdersSummaryAsync_IsMonthlyTrue_RunsClientWithValidUrl()
+        {
+            //Arrange
+            var request = new GetMyOrders.Request()
+            {
+                IsMonthly = true
+            };
+
+            _middleTierHttpClient.Setup(t => t.GetAsync<OrderStatsDto>(
+                It.IsAny<string>(),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()))
+                .ReturnsAsync(new OrderStatsDto());
+
+            var createdFrom = DateTime.Now.Date.AddDays(-30).ToString("yyyy-MM-dd");
+            var createdTo = DateTime.Now.Date.AddHours(23).AddMinutes(59).AddSeconds(59).ToString("yyyy-MM-dd");
+            var urlExpected = $"stats?key=status&createdFrom={createdFrom}&createdTo={createdTo}";
+
+            //Act
+            var sut = GetService();
+            var result = await sut.GetMyOrdersSummaryAsync(request);
+
+            //Assert
+            _middleTierHttpClient.Verify(x => x.GetAsync<OrderStatsDto>(
+                It.Is<string>(s => s.EndsWith(urlExpected)),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task GetMyOrdersSummaryAsync_ClientThrowsException_ExceptionCatchedDefaultReturned()
+        {
+            //Arrange
+            var request = new GetMyOrders.Request()
+            {
+                IsMonthly = true
+            };
+
+            _middleTierHttpClient.Setup(t => t.GetAsync<OrderStatsDto>(
+                It.IsAny<string>(),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()))
+                .Throws(new RemoteServerHttpException("", System.Net.HttpStatusCode.NotFound, new object()));
+
+            var createdFrom = DateTime.Now.Date.AddDays(-30).ToString("yyyy-MM-dd");
+            var createdTo = DateTime.Now.Date.AddHours(23).AddMinutes(59).AddSeconds(59).ToString("yyyy-MM-dd");
+            var urlExpected = $"stats?key=status&createdFrom={createdFrom}&createdTo={createdTo}";
+
+            //Act
+            var sut = GetService();
+            var result = await sut.GetMyOrdersSummaryAsync(request);
+
+            //Assert
+            result.Should().BeEquivalentTo(new MyOrdersDashboard
+            {
+                CurrencyCode = "USD",
+                CurrencySymbol = "$",
+                IsMonthly = request.IsMonthly,
+                Total = new OrderData { Amount = 0, FormattedAmount = string.Format("{0:N2}", 0), Percentage = "100%" },
+                Processed = new OrderData { Amount = 0, FormattedAmount = string.Format("{0:N2}", 0), Percentage = string.Empty },
+                Shipped = new OrderData { Amount = 0, FormattedAmount = string.Format("{0:N2}", 0), Percentage = string.Empty }
+            });
+        }
+
+        [Fact]
+        public async Task GetMyOrdersSummaryAsync_NoUsdOrders_EmptyObjectReturned()
+        {
+            //Arrange
+            var request = new GetMyOrders.Request();
+
+            var ordersData = new OrderStatsDataDto[2]
+            {
+                new OrderStatsDataDto() { Currency = "PLN" },
+                new OrderStatsDataDto() { Currency = "EUR" }
+            };
+
+            _middleTierHttpClient.Setup(t => t.GetAsync<OrderStatsDto>(
+                It.IsAny<string>(),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()))
+                .ReturnsAsync(new OrderStatsDto()
+                {
+                    Data = ordersData
+                });
+
+            //Act
+            var sut = GetService();
+            var result = await sut.GetMyOrdersSummaryAsync(request);
+
+            //Assert
+            result.Should().BeEquivalentTo(new MyOrdersDashboard());
+        }
+
+        [Fact]
+        public async Task GetMyOrdersSummaryAsync_ThrowsExceptionNoMatchingCode_DefaultReturned()
+        {
+            //Arrange
+            var request = new GetMyOrders.Request();
+
+            _middleTierHttpClient.Setup(t => t.GetAsync<OrderStatsDto>(
+                It.IsAny<string>(),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()))
+                .Throws(new RemoteServerHttpException("", System.Net.HttpStatusCode.BadRequest, new object()));
+
+            //Act
+            var sut = GetService();
+            var result = await sut.GetMyOrdersSummaryAsync(request);
+
+            //Assert
+            result.Should().BeEquivalentTo(new MyOrdersDashboard());
+        }
+
+        [Fact]
+        public async Task GetMyOrdersSummaryAsync_UsdOrders_ValidResponseObjectReturned()
+        {
+            //Arrange
+            var request = new GetMyOrders.Request();
+
+            var inProcess = 2;
+            var onHold = 1;
+            var shipped = 0;
+            var open = 0;
+
+            var ordersData = new OrderStatsDataDto[2]
+            {
+                new OrderStatsDataDto() { Currency = "USD", Value="IN_PROCESS", TotalValue = inProcess.ToString() },
+                new OrderStatsDataDto() { Currency = "USD", Value = "ON_HOLD", TotalValue = onHold.ToString() }
+            };
+
+            _middleTierHttpClient.Setup(t => t.GetAsync<OrderStatsDto>(
+                It.IsAny<string>(),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()))
+                .ReturnsAsync(new OrderStatsDto()
+                {
+                    Data = ordersData
+                });
+
+            //Act
+            var sut = GetService();
+            var result = await sut.GetMyOrdersSummaryAsync(request);
+
+            //Assert
+            var total = inProcess + onHold + shipped + open;
+            var percentageProcessed = (((inProcess + onHold) / total) * 100).ToString("F") + "%";
+            var percentageShipped = ((shipped / total) * 100).ToString("F") + "%";
+
+            result.Should().BeEquivalentTo(new MyOrdersDashboard
+            {
+                CurrencyCode = "USD",
+                CurrencySymbol = "$",
+                IsMonthly = request.IsMonthly,
+                Total = new OrderData { Amount = 3, FormattedAmount = string.Format("{0:N2}", 3), Percentage = "100%" },
+                Processed = new OrderData { Amount = 3, FormattedAmount = string.Format("{0:N2}", 3), Percentage = percentageProcessed },
+                Shipped = new OrderData { Amount = 0, FormattedAmount = string.Format("{0:N2}", 0), Percentage = percentageShipped }
+            });
+        }
+
+        #endregion GetMyOrdersSummaryAsync Unit Tests
+
+        #region GetDealsSummaryAsync
+
+        [Fact]
+        public async Task GetDealsSummaryAsync_RunsClientWithValidUrl()
+        {
+            //Arrange
+            var request = new GetDealsSummary.Request();
+
+            _middleTierHttpClient.Setup(t => t.GetAsync<DigitalFoundation.Common.Features.Contexts.Models.FindResponse<DealsBase>>(
+                It.IsAny<string>(),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()))
+                .ReturnsAsync(new DigitalFoundation.Common.Features.Contexts.Models.FindResponse<DealsBase>());
+
+            var validTo = DateTime.Now.AddYears(1).ToString("yyyy-MM-dd");
+            var urlExpected = $"Find?ValidTo={validTo}&TotalCount=False&Details=False&WithPaginationInfo=False&SortBy=expirationDate";
+
+            //Act
+            var sut = GetService();
+            var result = await sut.GetDealsSummaryAsync(request);
+
+            //Assert
+            _middleTierHttpClient.Verify(x => x.GetAsync<DigitalFoundation.Common.Features.Contexts.Models.FindResponse<DealsBase>>(
+                It.Is<string>(s => s.EndsWith(urlExpected)),
+                It.IsAny<List<object>>(),
+                It.IsAny<Dictionary<string, object>>(),
+                It.IsAny<Dictionary<string, string>>()), Times.Once());
+        }
+
+        #endregion GetDealsSummaryAsync
+
+        [Fact]
+        public async Task GetRenewalsExpirationDatesAsync_OneRenewalReturnedByService_ReturnsValidResponseList()
+        {
+            //Arrange
+            var response = new RenewalDto[1] { new RenewalDto() { ExpirationDate = new DateTime(2020, 01, 02), StartDate = new DateTime(2020, 01, 01) } };
+
+            _renewalsService.Setup(t => t.RenewalSearchAsync(It.IsAny<RenewalSearchDto>()))
+                .ReturnsAsync(response);
+
+            //Act
+            var sut = GetService();
+            var result = await sut.GetRenewalsExpirationDatesAsync("", "", 1);
+
+            //Assert
+            result.Should().BeEquivalentTo(new List<string>() { response[0].ExpirationDate.ToShortDateString() });
+        }
 
         [Fact]
         public async Task GetTopConfigurationsAsync_RunsWithValidUrlOnce()
