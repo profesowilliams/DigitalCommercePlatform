f4d9aa04f13e6702686c01e19963b97b2f377fa4
Jenn Chavarria <T065892B@techdata.com>
2022-03-01 19:53:18 +0000

    Merged PR 38175: [250391] Order Details: Updated Order Details Status Mapping to Match Backend...
    [250391] Order Details: Updated Order Details Status Mapping to Match Backend Response.
    
    Related work items: #250870

--- content/techdata/ui.react.frontend/src/global/techdata/components/OrderDetails/orderStatus.js
+++ content/techdata/ui.react.frontend/src/global/techdata/components/OrderDetails/orderStatus.js
@@ -2,7 +2,7 @@ export const orderStatusValues = {
     CANCELLED : "cancelled",
     OPEN : "open",
     SHIPPED : "shipped",
-    OPEN_IN_PROCESS : "open and in process",
+    OPEN_IN_PROCESS : "in process",
     SALES_REVIEW : "sales review",
     CREDIT_REVIEW : "credit review"
 }
