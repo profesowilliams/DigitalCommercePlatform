80f9601bcd571ee9c1b9737cc9f9f833a5187733
Venugopal, Shyamala (c) <Shyamala.Venugopal@techdata.com>
2022-07-15 20:02:59 +0000

    Merged PR 46699: [332643]-"renewalsgrid" and "renewalsdetails" components to tds-core and proxy to tds-site
    Related work items: #311243, #320454, #320455, #320813, #321978, #328643, #329097

---
+++ content/tds-core/ui.apps/src/main/content/jcr_root/apps/tds-core/components/renewalsdetails/v1/renewalsdetails/renewalsdetails.js
@@ -0,0 +1,261 @@
+"use strict";
+use(['/apps/tds-core/components/common/utils.js'], function(utils) {
+    var jsonObject = {};
+
+    var productLines = {};
+    var quotePreview = {};
+    var reseller = {};
+    var agreementInfo = {};
+    var errorMessages = {};
+    var endCustomer = {};
+    if (properties && properties["line"]) {
+        productLines["line"] = properties["line"];
+    }
+
+    if (properties && properties["lineItemDetailsLabel"]) {
+        productLines["lineItemDetailsLabel"] = properties["lineItemDetailsLabel"];
+    }
+
+    if (properties && properties["downloadPDFLabel"]) {
+        productLines["downloadPDFLabel"] = properties["downloadPDFLabel"];
+    }
+
+    if (properties && properties["downloadXLSLabel"]) {
+        productLines["downloadXLSLabel"] = properties["downloadXLSLabel"];
+    }
+
+    if (properties && properties["shopURL"]) {
+        jsonObject["shopURL"] = properties["shopURL"];
+    }
+
+    if (properties && properties["productfamily"]) {
+        productLines["productfamily"] = properties["productfamily"];
+    }
+    if (properties && properties["productdescription"]) {
+        productLines["productdescription"] = properties["productdescription"];
+    }
+    if (properties && properties["vendorPartNo"]) {
+        productLines["vendorPartNo"] = properties["vendorPartNo"];
+    }
+    if (properties && properties["unitPrice"]) {
+        productLines["unitPrice"] = properties["unitPrice"];
+    }
+    if (properties && properties["quantity"]) {
+        productLines["quantity"] = properties["quantity"];
+    }
+    if (properties && properties["quoteSubtotal"]) {
+        productLines["quoteSubtotal"] = properties["quoteSubtotal"];
+    }
+    if (properties && properties["note"]) {
+        productLines["note"] = properties["note"];
+    }
+
+    if (properties && properties["menuCopy"]) {
+        productLines["menuCopy"] = properties["menuCopy"];
+    }
+
+    if (properties && properties["menuCopyWithHeaders"]) {
+        productLines["menuCopyWithHeaders"] = properties["menuCopyWithHeaders"];
+    }
+
+    if (properties && properties["menuExport"]) {
+        productLines["menuExport"] = properties["menuExport"];
+    }
+
+    if (properties && properties["menuCsvExport"]) {
+        productLines["menuCsvExport"] = properties["menuCsvExport"];
+    }
+
+    if (properties && properties["menuExcelExport"]) {
+        productLines["menuExcelExport"] = properties["menuExcelExport"];
+    }
+
+    if (properties && properties["menuOpenLink"]) {
+        productLines["menuOpenLink"] = properties["menuOpenLink"];
+    }
+
+    if (properties && properties["menuCopyLink"]) {
+        productLines["menuCopyLink"] = properties["menuCopyLink"];
+    }
+
+    if (this.agGridLicenseKey) {
+        jsonObject["agGridLicenseKey"] = this.agGridLicenseKey;
+    }
+
+    if (this.shopDomainPage !== null) {
+        jsonObject["shopDomainPage"] = this.shopDomainPage;
+    }
+
+    if (this.serviceData.renewalDetailLineItemEndpoint) {
+        jsonObject["uiServiceEndPoint"] = this.serviceData.uiServiceDomain + this.serviceData.renewalDetailLineItemEndpoint || '';
+    }
+
+    if (properties && properties["quotePreviewlabel"]) {
+        quotePreview["quotePreviewlabel"] = properties["quotePreviewlabel"];
+    }
+
+    if (properties && properties["renewalsUrlLabel"]) {
+        quotePreview["renewalsUrlLabel"] = properties["renewalsUrlLabel"];
+    }
+    if (properties && properties["renewalsUrl"]) {
+        quotePreview["renewalsUrl"] = properties["renewalsUrl"];
+    }
+
+    if (properties && properties["resellerLabel"]) {
+        reseller["resellerLabel"] = properties["resellerLabel"];
+    }
+
+    if (properties && properties["accountNoLabel"]) {
+        reseller["accountNoLabel"] = properties["accountNoLabel"];
+    }
+    if (properties && properties["resellerVendorAccountNoLabel"]) {
+        quotePreview["resellerVendorAccountNoLabel"] = properties["resellerVendorAccountNoLabel"];
+    }
+
+    if (properties && properties["previousPurchaseOrderNoLabel"]) {
+        reseller["previousPurchaseOrderNoLabel"] = properties["previousPurchaseOrderNoLabel"];
+    }
+
+    if (properties && properties["resellerSAPaccNoLabel"]) {
+        reseller["resellerSAPaccNoLabel"] = properties["resellerSAPaccNoLabel"];
+    }
+
+
+    if (properties && properties["previousResellerorderNoLabel"]) {
+        reseller["previousResellerorderNoLabel"] = properties["previousResellerorderNoLabel"];
+    }
+
+
+    if (properties && properties["typeEndUserLabel"]) {
+        reseller["typeEndUserLabel"] = properties["typeEndUserLabel"];
+    }
+    if (properties && properties["vendorAccountNoResLabel"]) {
+        reseller["vendorAccountNoResLabel"] = properties["vendorAccountNoResLabel"];
+    }
+    if (properties && properties["previousPurchaseOrderNoEndLabel"]) {
+        reseller["previousPurchaseOrderNoEndLabel"] = properties["previousPurchaseOrderNoEndLabel"];
+    }
+    if (properties && properties["previousOrderNoLabel"]) {
+        reseller["previousOrderNoLabel"] = properties["previousOrderNoLabel"];
+    }
+
+    if (reseller != null) {
+        quotePreview["reseller"] = reseller;
+    }
+
+
+    if (properties && properties["endCustomerLabel"]) {
+        endCustomer["endCustomerLabel"] = properties["endCustomerLabel"];
+    }
+
+    if (properties && properties["emailLabel"]) {
+        endCustomer["emailLabel"] = properties["emailLabel"];
+    }
+
+    if (properties && properties["phoneLabel"]) {
+        endCustomer["phoneLabel"] = properties["phoneLabel"];
+    }
+    if (properties && properties["endCustomerType"]) {
+        endCustomer["endCustomerType"] = properties["endCustomerType"];
+    }
+    if (properties && properties["vendorAccountNo"]) {
+        endCustomer["vendorAccountNo"] = properties["vendorAccountNo"];
+    }
+    if (properties && properties["endCustpreviousPurchaseOrderNo"]) {
+        endCustomer["endCustpreviousPurchaseOrderNo"] = properties["endCustpreviousPurchaseOrderNo"];
+    }
+    if (endCustomer != null) {
+        quotePreview["productLines"] = endCustomer;
+    }
+
+    if (properties && properties["agreementInfoLabel"]) {
+        agreementInfo["agreementInfoLabel"] = properties["agreementInfoLabel"];
+    }
+
+    if (properties && properties["programLabel"]) {
+        agreementInfo["programLabel"] = properties["programLabel"];
+    }
+
+    if (properties && properties["durationLabel"]) {
+        agreementInfo["durationLabel"] = properties["durationLabel"];
+    }
+    if (properties && properties["supportLevelLabel"]) {
+        agreementInfo["supportLevelLabel"] = properties["supportLevelLabel"];
+    }
+    if (properties && properties["distiQuoteNoLabel"]) {
+        agreementInfo["distiQuoteNoLabel"] = properties["distiQuoteNoLabel"];
+    }
+    if (properties && properties["distiQuoteIDLabel"]) {
+        agreementInfo["distiQuoteIDLabel"] = properties["distiQuoteIDLabel"];
+    }
+    if (properties && properties["agreementNoLabel"]) {
+        agreementInfo["agreementNoLabel"] = properties["agreementNoLabel"];
+    }
+    if (properties && properties["distiQuoteLabel"]) {
+        agreementInfo["distiQuoteLabel"] = properties["distiQuoteLabel"];
+    }
+    if (properties && properties["quotedueDateLabel"]) {
+        agreementInfo["quotedueDateLabel"] = properties["quotedueDateLabel"];
+    }
+    if (properties && properties["quoteExpiryDateLabel"]) {
+        agreementInfo["quoteExpiryDateLabel"] = properties["quoteExpiryDateLabel"];
+    }
+    if (properties && properties["agreeStartDateLabel"]) {
+        agreementInfo["agreeStartDateLabel"] = properties["agreeStartDateLabel"];
+    }
+    if (properties && properties["agreeEndDateLabel"]) {
+        agreementInfo["agreeEndDateLabel"] = properties["agreeEndDateLabel"];
+    }
+    if (properties && properties["usageStartDateLabel"]) {
+        agreementInfo["usageStartDateLabel"] = properties["usageStartDateLabel"];
+    }
+    if (properties && properties["usageEndDateLabel"]) {
+        agreementInfo["usageEndDateLabel"] = properties["usageEndDateLabel"];
+    }
+
+    if (properties && properties["notFoundErrorMessage"]) {
+        errorMessages["notFoundErrorMessage"] = properties["notFoundErrorMessage"];
+    }
+    if (properties && properties["unexpectedErrorMessage"]) {
+        errorMessages["unexpectedErrorMessage"] = properties["unexpectedErrorMessage"];
+    }
+    if (properties && properties["errorLoadingPageTitle"]) {
+        errorMessages["errorLoadingPageTitle"] = properties["errorLoadingPageTitle"];
+    }
+
+    if (properties && properties["errorLoadingPageRedirect"]) {
+        errorMessages["errorLoadingPageRedirect"] = properties["errorLoadingPageRedirect"];
+    }
+
+    if (agreementInfo != null) {
+        quotePreview["agreementInfo"] = agreementInfo;
+    }
+
+    if (productLines != null) {
+        jsonObject["productLines"] = productLines;
+    }
+
+    if (quotePreview != null) {
+        jsonObject["quotePreview"] = quotePreview;
+    }
+
+    if (errorMessages != null) {
+        jsonObject["errorMessages"] = errorMessages;
+    }
+
+    if (this.exportXLSRenewalsEndpoint != null && this.serviceData.uiServiceDomain) {
+        jsonObject["exportXLSRenewalsEndpoint"] = this.serviceData.uiServiceDomain + this.exportXLSRenewalsEndpoint;
+    }
+
+    if (this.exportPDFRenewalsEndpoint != null && this.serviceData.uiServiceDomain) {
+        jsonObject["exportPDFRenewalsEndpoint"] = this.serviceData.uiServiceDomain + this.exportPDFRenewalsEndpoint;
+    }
+
+    if (this.renewalDetailsEndpoint && this.serviceData.uiServiceDomain) {
+        jsonObject["renewalDetailsEndpoint"] = this.serviceData.uiServiceDomain + this.renewalDetailsEndpoint;
+    }
+
+    return {
+        configJson: JSON.stringify(jsonObject)
+    };
+});
---
+++ content/tds-core/ui.apps/src/main/content/jcr_root/apps/tds-core/components/renewalsgrid/v1/renewalsgrid/renewalsgrid.js
@@ -0,0 +1,220 @@
+"use strict";
+use(['/apps/tds-core/components/common/utils.js'], function(utils) {
+    let jsonObject = {};
+    let resourceResolver = resource.getResourceResolver();
+    let optionData = {};
+    var productGrid = {};
+    let icons = {};
+
+    if (properties && properties["detailUrl"]) {
+        jsonObject["detailUrl"] = properties["detailUrl"];
+    }
+
+    //Column definition
+
+    let columnListValues = utils.getDataFromMultifield(resourceResolver, "columnList", function(childResource) {
+        let itemData = {};
+
+        itemData.columnLabel = childResource.properties["columnLabel"];
+        itemData.columnKey = childResource.properties["columnKey"];
+        itemData.sortable = childResource.properties["sortable"];
+        itemData.type = childResource.properties["columnType"];
+
+        return itemData;
+    });
+
+    if (columnListValues != null) {
+        jsonObject["columnList"] = columnListValues;
+    }
+
+    //Search Options
+    let searchOptionsValues = utils.getDataFromMultifield(resourceResolver, "searchOptionsList", function(childResource) {
+        let itemData = {};
+        itemData.searchLabel = childResource.properties["searchLabel"];
+        itemData.searchKey = childResource.properties["searchKey"];
+        itemData.showIfIsHouseAccount = childResource.properties["showIfIsHouseAccount"];
+        return itemData;
+    });
+
+    if (searchOptionsValues != null) {
+        jsonObject["searchOptionsList"] = searchOptionsValues;
+    }
+    jsonObject["uiServiceEndPoint"] = this.serviceData.uiServiceDomain + this.serviceData.renewalsGridEndpoint || '';
+
+    if (this.agGridLicenseKey) {
+        jsonObject["agGridLicenseKey"] = this.agGridLicenseKey;
+    }
+    if (properties && properties["itemsPerPage"]) {
+        jsonObject["itemsPerPage"] = properties["itemsPerPage"];
+    }
+    if (properties && properties["shopPath"] && this.shopDomainPage ) {
+        jsonObject["shopURL"] = this.shopDomainPage + properties["shopPath"];
+    }
+    if (properties && properties["paginationStyle"]) {
+        jsonObject["paginationStyle"] = properties["paginationStyle"];
+    }
+    if (properties && properties["defaultSortingColumnKey"]) {
+        optionData.defaultSortingColumnKey = properties["defaultSortingColumnKey"];
+    }
+    if (properties && properties["defaultSortingDirection"]) {
+        optionData.defaultSortingDirection = properties["defaultSortingDirection"];
+    }
+
+    if (optionData != null) {
+        jsonObject["options"] = optionData;
+    }
+
+    if (properties && properties["filterTitle"]) {
+        jsonObject["filterTitle"] = properties["filterTitle"];
+    }
+
+    if (properties && properties["clearAllFilterTitle"]) {
+        jsonObject["clearAllFilterTitle"] = properties["clearAllFilterTitle"];
+    }
+
+    if (properties && properties["showResultLabel"]) {
+        jsonObject["showResultLabel"] = properties["showResultLabel"];
+    }
+
+    if (properties && properties["menuCopy"]) {
+        jsonObject["menuCopy"] = properties["menuCopy"];
+    }
+
+    if (properties && properties["menuCopyWithHeaders"]) {
+        jsonObject["menuCopyWithHeaders"] = properties["menuCopyWithHeaders"];
+    }
+
+    if (properties && properties["menuExport"]) {
+        jsonObject["menuExport"] = properties["menuExport"];
+    }
+
+    if (properties && properties["menuCsvExport"]) {
+        jsonObject["menuCsvExport"] = properties["menuCsvExport"];
+    }
+
+    if (properties && properties["menuExcelExport"]) {
+        jsonObject["menuExcelExport"] = properties["menuExcelExport"];
+    }
+
+    if (properties && properties["menuOpenLink"]) {
+        jsonObject["menuOpenLink"] = properties["menuOpenLink"];
+    }
+
+    if (properties && properties["menuCopyLink"]) {
+        jsonObject["menuCopyLink"] = properties["menuCopyLink"];
+    }
+
+    if (properties && properties["ofTextLabel"]) {
+        jsonObject["ofTextLabel"] = properties["ofTextLabel"];
+    }
+
+    if (properties && properties["filterType"]) {
+        jsonObject["filterType"] = properties["filterType"];
+    }
+
+    let node = resourceResolver.getResource(currentNode.getPath() + "/filterList");
+    let filterListValues = [];
+    if (node !== null) {
+        let childrenList = node.getChildren();
+        for (let [key, res] in Iterator(childrenList)) {
+            let accordionLabel = res.properties["accordionLabel"];
+            let filterField = res.properties["filterField"];
+            let itemData = {};
+            itemData.accordionLabel = accordionLabel;
+            itemData.filterField = filterField;
+            let childNode = resourceResolver.getResource(res.getPath() + '/filtersOptionsList');
+
+            if (childNode != null) {
+                itemData.filterOptionsValues = [];
+                let childNodeList = childNode.getChildren();
+                for (let [childkey, childRes] in Iterator(childNodeList)) {
+                    let childDataItem = {};
+                    let filterOptionLabel = childRes.properties["filterOptionLabel"];
+                    childDataItem.filterOptionLabel = filterOptionLabel;
+                    let subChildNode = resourceResolver.getResource(childRes.getPath() + '/subFilterOptionList');
+
+                    if (subChildNode != null) {
+                        let subFilterOptionsValues = [];
+                        let subChildNodeList = subChildNode.getChildren();
+
+                        for (let [subChildKey, subChildRes] in Iterator(subChildNodeList)) {
+                            let subChildDataItem = {};
+                            subChildDataItem.subFilterOptionsLabel = subChildRes.properties["subFilterOptionsLabel"];
+                            subFilterOptionsValues.push(subChildDataItem);
+                        }
+                        childDataItem.subFilterOptionsValues = subFilterOptionsValues;
+                    }
+                    itemData.filterOptionsValues.push(childDataItem);
+                }
+            }
+            filterListValues.push(itemData);
+        }
+    }
+
+    if (filterListValues != null) {
+        jsonObject['filterListValues'] = filterListValues;
+    }
+
+    if (properties && properties["quoteIdLabel"]) {
+        productGrid["quoteIdLabel"] = properties["quoteIdLabel"];
+    }
+
+    if (properties && properties["refNoLabel"]) {
+        productGrid["refNoLabel"] = properties["refNoLabel"];
+    }
+
+    if (properties && properties["expiryDateLabel"]) {
+        productGrid["expiryDateLabel"] = properties["expiryDateLabel"];
+    }
+
+    if (properties && properties["downloadPDFLabel"]) {
+        productGrid["downloadPDFLabel"] = properties["downloadPDFLabel"];
+    }
+
+    if (properties && properties["downloadXLSLabel"]) {
+        productGrid["downloadXLSLabel"] = properties["downloadXLSLabel"];
+    }
+
+    if (properties && properties["seeDetailsLabel"]) {
+        productGrid["seeDetailsLabel"] = properties["seeDetailsLabel"];
+    }
+
+    if (productGrid != null) {
+        jsonObject["productGrid"] = productGrid;
+    }
+
+    if (this.exportXLSRenewalsEndpoint != null && this.serviceData.uiServiceDomain) {
+        jsonObject["exportXLSRenewalsEndpoint"] = this.serviceData.uiServiceDomain + this.exportXLSRenewalsEndpoint;
+    }
+
+    if (this.exportPDFRenewalsEndpoint != null && this.serviceData.uiServiceDomain) {
+        jsonObject["exportPDFRenewalsEndpoint"] = this.serviceData.uiServiceDomain + this.exportPDFRenewalsEndpoint;
+    }
+    
+    if (this.renewalDetailsEndpoint && this.serviceData.uiServiceDomain) {
+        jsonObject["renewalDetailsEndpoint"] = this.serviceData.uiServiceDomain + this.renewalDetailsEndpoint;
+    }
+    const overdueProperties = {values: ['overdueIcon','overdueIconColor'], propertyName: 'overdueDaysRange'};   
+    const thirtyDaysProperties = {values:['afterZeroIcon', 'afterZeroIconColor'], propertyName: 'afterZeroDaysRange'};
+    const sixtyOneDaysProperties = {values : ['afterThirtyIcon', 'afterThirtyIconColor'], propertyName: 'afterThirtyDaysRange'};   
+    const sixtyOnePlusProperties = {values : ['sixtyPlusIcon', 'sixtyPlusIconColor'], propertyName:'sixtyPlusDaysRange'};
+
+    function populateOutterProperty (obj, prop) {
+        const populated = utils.fillFieldsDialogProperties(prop.values);       
+        const daysPropertyName = properties[prop.propertyName]
+        if (populated && daysPropertyName) {obj[daysPropertyName] = populated};
+    }
+    
+    if (properties){     
+        populateOutterProperty(icons,overdueProperties);
+        populateOutterProperty(icons,thirtyDaysProperties);
+        populateOutterProperty(icons,sixtyOneDaysProperties);
+        populateOutterProperty(icons,sixtyOnePlusProperties);  
+    }
+
+    if (!!icons) {jsonObject['icons'] = icons};
+
+    return {
+        configJson: JSON.stringify(jsonObject)
+    };
+});
