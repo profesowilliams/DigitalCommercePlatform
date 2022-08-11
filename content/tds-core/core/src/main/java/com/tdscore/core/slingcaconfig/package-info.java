cfbde91e5820c160723f8d2dbdceef9a9afe0269
Harris, Robert <Robert.Harris@techdata.com>
2022-08-11 20:57:20 +0000

    Merged PR 47980: [335982] - Bump version on packages
    - Mirror changes for 335982 into tds-core
    - Bump version for packages
    
    Related work items: #338551

--- content/tds-core/core/src/main/java/com/tdscore/core/slingcaconfig/package-info.java
+++ content/tds-core/core/src/main/java/com/tdscore/core/slingcaconfig/package-info.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-@Version("1.1")
+@Version("1.2")
 package com.tdscore.core.slingcaconfig;
 
 import org.osgi.annotation.versioning.Version;
