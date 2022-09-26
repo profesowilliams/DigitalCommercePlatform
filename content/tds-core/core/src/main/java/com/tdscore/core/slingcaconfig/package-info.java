2fa7c3c2097cdc8aa572cc86c7324469ee9cd94e
Jenn Chavarria <T065892B@techdata.com>
2022-09-26 19:29:32 +0000

    Merged PR 50293: Feature Flag: Label disableImpersonateAccountRequestHeader Appropriately.
    Feature Flag: Label disableImpersonateAccountRequestHeader Appropriately.
    
    Related work items: #351863

--- content/tds-core/core/src/main/java/com/tdscore/core/slingcaconfig/package-info.java
+++ content/tds-core/core/src/main/java/com/tdscore/core/slingcaconfig/package-info.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-@Version("1.3")
+@Version("1.4")
 package com.tdscore.core.slingcaconfig;
 
 import org.osgi.annotation.versioning.Version;
