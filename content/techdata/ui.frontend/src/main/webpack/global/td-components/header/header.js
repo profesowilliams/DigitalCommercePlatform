87625aaa97f2f119dacbe0b91a0de7fd733b9769
Leonardo Orejuela <T092191C@techdata.com>
2022-09-27 12:46:37 +0000

    Merged PR 50351: [343559] sticky behaviour removed for techdata
    [343559] sticky behaviour removed for techdata
    
    Related work items: #343559

--- content/techdata/ui.frontend/src/main/webpack/global/td-components/header/header.js
+++ content/techdata/ui.frontend/src/main/webpack/global/td-components/header/header.js
@@ -11,28 +11,7 @@ export default class Header {
         window.addEventListener('resize', () => this.headerResize());
         this.searchEl?.addEventListener('click', () => this.toggleSearch(this.searchEl));
         document.addEventListener('click', (event) => this.showSearchIconOnly(event, this.searchEl));
-        window.addEventListener('scroll', () => this.handleStickyHeader());
         // this.initSecondaryImage();
-
-        this.header = document.getElementById('cmp-techdata-header');
-        this.header.classList.add('cmp-experiencefragment__header--sticky');
-        
-        const aemGrid = document.querySelector(".aem-Grid");
-        this.subheaderList = aemGrid.querySelectorAll(".subheader");
-        this.subheader = this.subheaderList[0];
-        this.container = this.subheader ? this.subheader.previousElementSibling : null;
-
-        if(this.header && this.subheader && this.container){
-            this.subheaderNav = this.subheader.querySelector('.cmp-sub-header--sub-nav');
-            this.subheaderNav.style.marginTop = "-"+this.subheaderNav.clientHeight+"px";
-            this.isSubheaderSticky = false;
-
-            if(this.container.classList.contains("container") || this.container.classList.contains("teaser")){
-                this.isContainer = true;
-                this.checkHeaderImage();
-                this.checkSubheaderImage();  
-            }
-        }
     }
 
     headerResize() {
@@ -77,44 +56,6 @@ export default class Header {
                 document.querySelector('.cmp-header--logo-small')?.classList.remove('active');
                 document.querySelector('.dp-figure').style.display = 'block';
             }
-        }
-    }
-
-    handleStickyHeader() {
-        if(this.header && this.isContainer && this.subheader){
-            this.checkHeaderImage();
-            this.checkHeaderSubheader();
-        }
-    }
-
-    checkHeaderSubheader(){
-        if(this.header.getBoundingClientRect().bottom >= this.subheaderNav.getBoundingClientRect().top){
-            if(this.isSubheaderSticky && window.pageYOffset <= this.container.clientHeight - this.subheaderNav.clientHeight - this.header.clientHeight){
-                this.subheaderNav.classList.add('cmp-experiencefragment__subheader--sticky--opaque');
-                this.subheaderNav.classList.remove('cmp-experiencefragment__subheader--sticky');
-                this.isSubheaderSticky = false;
-            }
-            else{
-                this.subheaderNav.classList.add('cmp-experiencefragment__subheader--sticky');
-                this.subheaderNav.style.top = this.header.clientHeight + this.subheaderNav.clientHeight + "px";
-                this.subheaderNav.classList.remove('cmp-experiencefragment__subheader--sticky--opaque');
-                this.isSubheaderSticky = true;
-            }
-        }
-    }
-
-    checkHeaderImage(){
-        if(this.header.getBoundingClientRect().bottom >= this.container.getBoundingClientRect().top && this.container.getBoundingClientRect().bottom > 0 && window.pageYOffset == 0){
-            this.header.classList.add('cmp-experiencefragment__header--sticky--opaque');
-        }
-        else{
-            this.header.classList.remove('cmp-experiencefragment__header--sticky--opaque');
-        }
-    }
-
-    checkSubheaderImage(){
-        if(this.subheaderNav.getBoundingClientRect().top + parseInt(this.subheaderNav.style.marginTop,10) <= this.container.getBoundingClientRect().bottom){
-            this.subheaderNav.classList.add('cmp-experiencefragment__subheader--sticky--opaque');
         }
     }
 }
