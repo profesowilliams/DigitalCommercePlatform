e3ba937056724580cdeee7b5572304d837938a3c
Diego Campanero <T455476L@techdata.com>
2022-10-20 22:53:10 +0000

    Merged PR 51755: US[357878] TDS-Core Notification: Fix close event issue
    Related work items: #364455

--- content/tds-core/ui.frontend/src/components/notification/notification.js
+++ content/tds-core/ui.frontend/src/components/notification/notification.js
@@ -1,10 +1,11 @@
 (function () {
     const CLASS_NOTIFICATION_CLOSE_BUTTON = 'close-notification';
     document.addEventListener('DOMContentLoaded', () => {
-        // When the user clicks anywhere outside of the notification, close it
         window.onclick = function (event) {            
             if (event.target.className === CLASS_NOTIFICATION_CLOSE_BUTTON){
-              const container = event.target.parentNode;
+              const parent = event.target.parentNode;
+              const container = parent.parentNode;
+              container.parentNode;
               container.parentNode.remove();
             }
         };
