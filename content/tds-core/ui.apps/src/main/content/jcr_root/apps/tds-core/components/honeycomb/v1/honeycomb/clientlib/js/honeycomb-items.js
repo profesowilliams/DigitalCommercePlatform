8484a6ca9035160cc45582d95c713eafd4ffdff7
Manuel Gutierrez <T144302D@techdata.com>
2022-11-04 13:14:09 +0000

    Merged PR 52543: [339200]: Honeycomb: Added validations to the author dialog and some style fixes
    [339200]: Honeycomb: Added validations to the author dialog and some style fixes
    
    Related work items: #339200

--- content/tds-core/ui.apps/src/main/content/jcr_root/apps/tds-core/components/honeycomb/v1/honeycomb/clientlib/js/honeycomb-items.js
+++ content/tds-core/ui.apps/src/main/content/jcr_root/apps/tds-core/components/honeycomb/v1/honeycomb/clientlib/js/honeycomb-items.js
@@ -1,63 +1,100 @@
+(function ($, ns) {
+  const DISABLED_PROPERTY = "disabled";
+  const DIALOG_CONTENT_SELECTOR = ".cmp-honeycomb__dialog";
 
-(function($, ns){
-    
-    let DIALOG_CONTENT_SELECTOR = ".cmp-honeycomb__dialog";
+  const selectors = {
+    childrenEditor: "data-cmp-is='childrenEditor'",
+    addButton: "data-cmp-hook-childreneditor='add'",
+    honeycombMaxItems: "data-cmp-dialog-honeycomb-maxitems",
+    dataContainerPath: "data-container-path",
+    honeycombElementsTag: "coral-multifield-item",
+    itemTitle: ".cmp-childreneditor__item-title",
+    removeButton: ".coral3-Multifield-remove",
+  };
 
-    let selectors = {
-        childrenEditor: "data-cmp-is='childrenEditor'",
-        addButton: "data-cmp-hook-childreneditor='add'",
-        honeycombMaxItems: "data-cmp-dialog-honeycomb-maxitems",
-        dataContainerPath : "data-container-path",
-        honeycombElementsTag : "coral-multifield-item"
+  $(document).on("dialog-loaded", function (e) {
+    const $dialog = e.dialog;
+    const $componentDialog = $dialog.find(DIALOG_CONTENT_SELECTOR);
+    const dialogContent =
+      $componentDialog.length > 0 ? $componentDialog[0] : undefined;
+
+    if (dialogContent) {
+      disableAddButton(dialogContent);
+      addMaxLengthValidation($dialog);
     }
+  });
 
-    $(document).on("dialog-loaded", function(e) {
-        let $dialog = e.dialog;
-        let $componentDialog = $dialog.find(DIALOG_CONTENT_SELECTOR);
-        var dialogContent = $componentDialog.length > 0 ? $componentDialog[0] : undefined;
-        if(dialogContent) {
-            handleEditDialog(dialogContent);
+  function disableAddButton(componentDialog) {
+    // Get Max Items that can be added to Children Editor
+    let honeyCombMaxItemsElement = componentDialog.querySelector(
+      `[${selectors.honeycombMaxItems}]`
+    );
+    let maxItems = honeyCombMaxItemsElement.getAttribute(
+      selectors.honeycombMaxItems
+    );
+
+    // Get Children Editor Panel
+    let childrenEditorElement = componentDialog.querySelector(
+      `[${selectors.childrenEditor}]`
+    );
+
+    Coral.commons.ready(childrenEditorElement, function () {
+      // Get 'Add' Button Element
+      let addButtonElement = childrenEditorElement.querySelector(
+        `[${selectors.addButton}]`
+      );
+
+      // Get Children Editor Stored Editable Fields (on dialog load).
+      let childrenEditorEditableFields = ns.editables.find(
+        childrenEditorElement?.getAttribute(selectors.dataContainerPath)
+      )[0];
+
+      let honeycombElements = childrenEditorEditableFields.getChildren();
+
+      // Set initial state as disabled if number of items on load surpasses the limit set by
+      if (honeycombElements.length >= maxItems) {
+        addButtonElement.setAttribute(DISABLED_PROPERTY);
+      }
+
+      enableDisableRemoveButtons(honeycombElements, childrenEditorElement);
+
+      // Add listeners to childrenEditorElement
+      childrenEditorElement.on("coral-collection:add", function (e) {
+        let honeycombElements = childrenEditorElement.getElementsByTagName(
+          selectors.honeycombElementsTag
+        );
+        if (honeycombElements.length >= maxItems) {
+          addButtonElement.setAttribute(DISABLED_PROPERTY);
         }
+
+        enableDisableRemoveButtons(honeycombElements, childrenEditorElement);
+      });
+
+      childrenEditorElement.on("coral-collection:remove", function (e) {
+        let honeycombElements = childrenEditorElement.getElementsByTagName(
+          selectors.honeycombElementsTag
+        );
+        if (honeycombElements.length < maxItems) {
+          addButtonElement.removeAttribute(DISABLED_PROPERTY);
+        }
+
+        enableDisableRemoveButtons(honeycombElements, childrenEditorElement);
+      });
     });
+  }
 
-    function handleEditDialog(componentDialog) {
-        // Get Max Items that can be added to Children Editor
-        let honeyCombMaxItemsElement = componentDialog.querySelector(`[${selectors.honeycombMaxItems}]`);
-        let maxItems = honeyCombMaxItemsElement.getAttribute(selectors.honeycombMaxItems);
+  function enableDisableRemoveButtons(honeycombElements, childrenEditorElement) {
+    const $removeButtons = $(childrenEditorElement).find(
+      selectors.removeButton
+    );
 
-        // Get Children Editor Panel
-        let childrenEditorElement = componentDialog.querySelector(`[${selectors.childrenEditor}]`);
-
-        Coral.commons.ready(childrenEditorElement, function() {
-
-            // Get 'Add' Button Element
-            let addButtonElement = childrenEditorElement.querySelector(`[${selectors.addButton}]`);
-
-            // Get Children Editor Stored Editable Fields (on dialog load).
-            let childrenEditorEditableFields = ns.editables.find(childrenEditorElement?.getAttribute(selectors.dataContainerPath))[0];
-
-            let honeycombElements = childrenEditorEditableFields.getChildren();
-            
-            // Set initial state as disabled if number of items on load surpasses the limit set by
-            if (honeycombElements.length >= maxItems) {
-                addButtonElement.setAttribute('disabled');
-            }
-
-            // Add listeners to childrenEditorElement
-            childrenEditorElement.on("coral-collection:add", function(e) {
-                let honeycombElements = childrenEditorElement.getElementsByTagName(selectors.honeycombElementsTag);
-                if (honeycombElements.length >= maxItems) {
-                    addButtonElement.setAttribute('disabled');
-                }
-            });
-
-            childrenEditorElement.on("coral-collection:remove", function(e) {
-                let honeycombElements = childrenEditorElement.getElementsByTagName(selectors.honeycombElementsTag);
-                if (honeycombElements.length < maxItems) {
-                    addButtonElement.removeAttribute('disabled');
-                }
-            });
-
-        });
+    if (honeycombElements.length <= 3) {
+      $removeButtons.prop(DISABLED_PROPERTY, true);
+    } else {
+      $removeButtons.prop(DISABLED_PROPERTY, false);
     }
+  }
+  function addMaxLengthValidation(componentDialog) {
+    componentDialog.find(selectors.itemTitle).attr("maxlength", "10");
+  }
 })(jQuery, Granite.author);
