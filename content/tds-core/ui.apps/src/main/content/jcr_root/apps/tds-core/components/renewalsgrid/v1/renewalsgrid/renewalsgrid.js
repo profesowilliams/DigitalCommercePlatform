a166d51be5b3241cefd9de41514d8a1b9200143b
Zmich, David <David.Zmich@techdata.com>
2023-01-03 20:09:52 +0000

    Merged PR 55223: [356704] Renewals grid: Fix typo for display logic - no results
    Update config property name
    
    Related work items: #396837

--- content/tds-core/ui.apps/src/main/content/jcr_root/apps/tds-core/components/renewalsgrid/v1/renewalsgrid/renewalsgrid.js
+++ content/tds-core/ui.apps/src/main/content/jcr_root/apps/tds-core/components/renewalsgrid/v1/renewalsgrid/renewalsgrid.js
@@ -294,7 +294,7 @@ use(['../common/utils.js'], function(utils) {
     }
 
     if (noResultsValues != null) {
-        jsonObject["noResultsValues"] = noResultsValues;
+        jsonObject["searchResultsError"] = noResultsValues;
     }
 
     return {
