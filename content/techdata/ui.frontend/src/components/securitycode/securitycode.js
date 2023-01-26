becf0713d1dc54262c1b32f723d058409f60c532
Diego Campanero <T455476L@techdata.com>
2023-01-26 22:18:12 +0000

    Merged PR 56187: US[385945] Techdata - SecurityCode : Add code to techdata repo
    Related work items: #400336

--- content/techdata/ui.frontend/src/components/securitycode/securitycode.js
+++ content/techdata/ui.frontend/src/components/securitycode/securitycode.js
@@ -1,41 +1,42 @@
 (function () {
-  const md5 = require("blueimp-md5");
-  const CLASS_MODAL = "cmp-popup__modal";
-  const CLASS_MODAL_OPENED = "cmp-popup__modal--open";
-  const ID_SECURITY_FORM = "securityform";
+  const md5  = require("blueimp-md5");
+  const CLASS_MODAL = 'securitycode'; 
+  const CLASS_MODAL_OPENED = 'cmp-popup__modal--open';
+  const ID_SECURITY_FORM = 'securityform';
   const pageID = document.body.id;
-  const COOKIE_NAME = "s_code_passed";
-  const ID_SECURITY_BUTTON = "verification-button";
+  const COOKIE_NAME = 's_code_passed';
+  const ID_SECURITY_BUTTON = 'verification-button';
   const DIGIT_NUMBER_SECURITY_CODE = 5;
   const submitButton = document.getElementById(ID_SECURITY_BUTTON);
   const securityForm = document.getElementById(ID_SECURITY_FORM);
-  const inputs = securityForm?.querySelectorAll("input");
-  const dataset = document.querySelector("[data-sec-code]");
-  const dataSecCode = document.body.getAttribute("data-sec-code");
+  const inputs = securityForm?.querySelectorAll('input');
+  const dataset = document.querySelector('[data-sec-code]');
+  const dataSecCodeEnabled = document.querySelector('[data-sec-code-enabled]') ? true :  false;
+  const dataSecCode = document.body.getAttribute('data-sec-code');
   const encryptCode = encryptSecurityCode(dataSecCode);
-  const errorDataset = document.querySelector("[data-error-code]");
-  const errorIconDataset = document.querySelector("[data-error-icon]");
+  const errorDataset = document.querySelector('[data-error-code]');
+  const errorIconDataset = document.querySelector('[data-error-icon]');
   const { secCode, secCodeEnabled } = dataset?.dataset
     ? dataset.dataset
     : { secCode: null, secCodeEnabled: null };
   // encrypt security code
-  document.body.setAttribute("data-sec-code", encryptCode);
-  const validationCodeEnabledFlag = secCodeEnabled || secCodeEnabled === "";
+  document.body.setAttribute('data-sec-code', encryptCode);
+  const validationCodeEnabledFlag = secCodeEnabled || secCodeEnabled === '';
   const { errorCode } = errorDataset?.dataset
     ? errorDataset?.dataset
     : { errorCode: null };
   const { errorIcon } = errorIconDataset?.dataset
     ? errorIconDataset?.dataset
-    : { errorIcon: "fa-solid fa-triangle-exclamation" };
+    : { errorIcon: 'fa-solid fa-triangle-exclamation' };
   const cookieValue = getCookie(COOKIE_NAME);
-  const cookieString = pageID + "#" + encryptSecurityCode(secCode);
+  const cookieString = pageID + '#' + encryptSecurityCode(secCode);
   function handlerOpenSecurityCodeModal() {
     inputs.forEach((input) => {
       input.addEventListener(
-        "keydown",
+        'keydown',
         function (e) {
           input.value = null;
-          if (e.key == "Backspace") {
+          if (e.key == 'Backspace') {
             submitButton.disabled = true;
           }
 
@@ -49,9 +50,8 @@
         true
       );
     });
-
     securityForm.addEventListener(
-      "submit",
+      'submit',
       function (e) {
         submitButton.disabled = true;
         validateSecurityCode();
@@ -63,9 +63,9 @@
   }
 
   function getCookie(name) {
-    var cookieArr = document.cookie.split(";");
+    var cookieArr = document.cookie.split(';');
     for (var i = 0; i < cookieArr.length; i++) {
-      var cookiePair = cookieArr[i].split("=");
+      var cookiePair = cookieArr[i].split('=');
       if (name == cookiePair[0].trim()) {
         return decodeURIComponent(cookiePair[1]);
       }
@@ -77,7 +77,7 @@
     const previousPagesCookie = cookieValue;
     let returnValue = true;
     if (previousPagesCookie) {
-      const cookieArray = previousPagesCookie.split(",");
+      const cookieArray = previousPagesCookie.split(',');
       cookieArray.forEach((cookie) => {
         if (cookie.includes(cookieString)) {
           returnValue = false;
@@ -91,18 +91,18 @@
   function insertPageToCookies() {
     const previousPagesCookie = cookieValue;
     if (previousPagesCookie) {
-      const hashArray = previousPagesCookie.split(",");
+      const hashArray = previousPagesCookie.split(',');
       hashArray.push(cookieString);
-      document.cookie = COOKIE_NAME + "=" + hashArray.toString();
+      document.cookie = COOKIE_NAME + '=' + hashArray.toString();
     } else {
-      document.cookie = COOKIE_NAME + "=" + encodeURIComponent(cookieString);
+      document.cookie = COOKIE_NAME + '=' + encodeURIComponent(cookieString);
     }
   }
 
   function removeLabels() {
-    const form = document.querySelector("#securityform");
+    const form = document.querySelector('#securityform');
     const elements = [
-      ...form.getElementsByClassName("cmp-form__error-container"),
+      ...form.getElementsByClassName('cmp-form__error-container'),
     ];
     elements.forEach((element) => element.remove());
   }
@@ -114,7 +114,7 @@
       inputValues.push(i.value);
     });
     const arrayString = inputValues.toString();
-    const formatValue = arrayString.replace(/,/g, "");
+    const formatValue = arrayString.replace(/,/g, '');
     if (formatValue === secCode) {
       insertPageToCookies();
       closeModal();
@@ -125,19 +125,19 @@
 
   function createErrorContainer() {
     ResetInputs();
-    const errorContainer = document.createElement("div");
-    const errorLabel = document.createElement("span");
-    const errorIconElement = document.createElement("span");
+    const errorContainer = document.createElement('div');
+    const errorLabel = document.createElement('span');
+    const errorIconElement = document.createElement('span');
     errorLabel.innerText = errorCode;
-    errorLabel.style.color = "red";
-    errorIconElement.style.color = "red";
-    errorContainer.classList.add("cmp-form__error-container");
-    errorIconElement.classList.add("cmp-form_icon");
-    const classList = errorIcon.split(" ");
-    classList.forEach((cl) => errorIconElement.classList.add(cl));
+    errorLabel.style.color = 'red';
+    errorIconElement.style.color = 'red';
+    errorContainer.classList.add('cmp-form__error-container');
+    errorIconElement.classList.add('cmp-form_icon');
+    const classList = errorIcon.split(' ');
+    classList.forEach(cl => errorIconElement.classList.add(cl))
     errorContainer.appendChild(errorIconElement);
     errorContainer.appendChild(errorLabel);
-    const referenceNode = document.querySelector(".cmp-form__buttons");
+    const referenceNode = document.querySelector('.cmp-form__buttons');
     referenceNode.parentNode.insertBefore(errorContainer, referenceNode);
   }
 
@@ -176,7 +176,7 @@
   }
 
   function handlerOnKeyDown(e) {
-    if (e.target.nodeName === "INPUT" && e.target.type !== "textarea") {
+    if (e.target.nodeName === 'INPUT' && e.target.type !== 'textarea') {
       e.preventDefault();
       focusNextElement();
       return false;
@@ -194,24 +194,42 @@
 
   function closeModal() {
     const tempModal = document.getElementsByClassName(CLASS_MODAL)[0];
-    tempModal.style.display = "none";
+    tempModal.style.display = 'none';
   }
 
-  function loadValidation(modals) {
+  function hideModal(modals) {
+    for (let element of modals) {
+      element.style["display"] = "none";
+    }
+  }
+
+  function handlerModalEnable(modals) {
+    if (pageID && validationCodeEnabledFlag && dataSecCodeEnabled) {
       if (validatePageCookie()) {
-        if (modals && modals.length > 0) {
           for (let element of modals) {
-            modalElement = element;
             openModal(element);
           }
-        }
+      } else {
+        hideModal(modals);
       }
+    } else {
+      hideModal(modals);
+    }
   }
 
-  document.addEventListener("DOMContentLoaded", () => {
+  function setHeightModal() {
+    const subHeader = document?.getElementsByClassName('subheader')[0];
+    const header = document.getElementById('cmp-techdata-header');
+    const  subHeaderHeight = subHeader?.offsetHeight ? subHeader?.offsetHeight : 0;
+    const  headerHeight = header.offsetHeight;
+    const totalHeight = subHeaderHeight + headerHeight;
+    document.getElementsByClassName('cmp-s-code-popup__modal__content')[0].style['top'] = `${totalHeight}px`;
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
     const modals = document.getElementsByClassName(CLASS_MODAL);
-    if (pageID && validationCodeEnabledFlag) {
-        loadValidation(modals)
-    }
+    setHeightModal();
+    handlerModalEnable(modals);
   });
+    
 })();
