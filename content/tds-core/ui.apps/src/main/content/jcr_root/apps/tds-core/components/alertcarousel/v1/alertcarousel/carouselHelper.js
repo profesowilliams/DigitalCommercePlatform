a679fc3b0f84b9aad44f33a8794a95809fa02105
Bala Kondepudi <T552441J@techdata.com>
2023-02-06 20:48:55 +0000

    Merged PR 56632: Fix to Null check issue for analytics usecase JS
    Fix to Null check issue for analytics usecase JS
    
    Related work items: #404332

--- content/tds-core/ui.apps/src/main/content/jcr_root/apps/tds-core/components/alertcarousel/v1/alertcarousel/carouselHelper.js
+++ content/tds-core/ui.apps/src/main/content/jcr_root/apps/tds-core/components/alertcarousel/v1/alertcarousel/carouselHelper.js
@@ -6,31 +6,31 @@ use(function () {
     var jsonObject = new Packages.org.json.JSONObject();
 
 
-
-    var keys = Object.keys(dataJson);
-    if (keys && keys.length > 0)
-    {
-        var firstKey = keys[0];
-        jsonObject.put(firstKey,dataJson[firstKey]);
-        if (properties.get(ANALYTICS_REGION_PN) != null)
+    if(dataJson) {
+        var keys = Object.keys(dataJson);
+        if (keys && keys.length > 0)
         {
-            jsonObject.getJSONObject(firstKey).put(ANALYTICS_REGION_PN, properties.get(ANALYTICS_REGION_PN));
-        }
+            var firstKey = keys[0];
+            jsonObject.put(firstKey,dataJson[firstKey]);
+            if (properties.get(ANALYTICS_REGION_PN) != null)
+            {
+                jsonObject.getJSONObject(firstKey).put(ANALYTICS_REGION_PN, properties.get(ANALYTICS_REGION_PN));
+            }
 
-        if (properties.get(ANALYTICS_CATEGORY_PN) != null)
-        {
-            jsonObject.getJSONObject(firstKey).put(ANALYTICS_CATEGORY_PN, properties.get(ANALYTICS_CATEGORY_PN));
-        }
+            if (properties.get(ANALYTICS_CATEGORY_PN) != null)
+            {
+                jsonObject.getJSONObject(firstKey).put(ANALYTICS_CATEGORY_PN, properties.get(ANALYTICS_CATEGORY_PN));
+            }
 
-        if (properties.get(ANALYTICS_NAME_PN) != null)
-        {
-            jsonObject.getJSONObject(firstKey).put(ANALYTICS_NAME_PN, properties.get(ANALYTICS_NAME_PN));
-        }
+            if (properties.get(ANALYTICS_NAME_PN) != null)
+            {
+                jsonObject.getJSONObject(firstKey).put(ANALYTICS_NAME_PN, properties.get(ANALYTICS_NAME_PN));
+            }
 
-    }else{
-        jsonObject = dataJson;
+        }else{
+            jsonObject = dataJson;
+        }
     }
-
 
 
     return {
