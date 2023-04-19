0cb639c65044c2b37716181f49073f129c43e9e7
Harris, Robert <Robert.Harris@techdata.com>
2023-04-19 21:50:26 +0000

    Merged PR 59969: [420391] - Megamenu v2 for Techdata tenant
    - Adjust size of chevron on TD MMv2 (uses SVG.. TDS does not for some reason)
    - Set container height on XF for header to 68px to match Figma
    - Fix alignment of submenus
    - Enable mobile menu styling and functionality to match tds-site.  Note- there are still some bugs that also exist in tds-site these will need to be addressed in a future bug
    
    Related work items: #425024, #434072

--- content/tds-site/ui.frontend/src/main/webpack/global/tds-core-components/hamburger-menu/hamburger.js
+++ content/tds-site/ui.frontend/src/main/webpack/global/tds-core-components/hamburger-menu/hamburger.js
@@ -1,71 +1,71 @@
 export default class Hamburger {
-    constructor() {
-        const el = this.injectHamburger();
+  constructor() {
+    const el = this.injectHamburger();
 
-        if (!el) return;
+    if (!el) return;
 
-        const classObj = {
-            active: 'active',
-            open: 'megamenu--open',
-        }
-
-        this.el = el;
-        this.open = false;
-        this.containerEl = document.querySelector('.megamenu');
-
-        this.el.addEventListener('click', () => this.handleClick(classObj));
-        document.addEventListener('click', (e) => this.hideWhenOutside(e, classObj));
+    const classObj = {
+      active: 'active',
+      open: 'megamenu--open',
     }
 
-    injectHamburger() {
-        const figure = document.querySelector('.dp-figure');
-        const navbar = document.querySelector('#cmp-techdata-header');
+    this.el = el;
+    this.open = false;
+    this.containerEl = document.querySelector('.megamenu');
 
-        if (!figure || !navbar) return;
+    this.el.addEventListener('click', () => this.handleClick(classObj));
+    document.addEventListener('click', (e) => this.hideWhenOutside(e, classObj));
+  }
 
-        const hamburgerBtn = document.createElement('button');
-        hamburgerBtn.type = 'button';
-        hamburgerBtn.ariaLabel = 'hamburger menu';
-        hamburgerBtn.classList.add('cmp-td-hamburgerMenu');
+  injectHamburger() {
+    const figure = document.querySelector('.dp-figure');
+    const navbar = document.querySelector('#cmp-techdata-header');
 
-        const hamIcon = document.createElement('i');
-        hamIcon.classList.add('fas', 'fa-bars');
-        hamburgerBtn.append(hamIcon);
+    if (!figure || !navbar) return;
 
-        figure.parentNode.insertBefore(hamburgerBtn, figure);
-        return hamburgerBtn;
+    const hamburgerBtn = document.createElement('button');
+    hamburgerBtn.type = 'button';
+    hamburgerBtn.ariaLabel = 'hamburger menu';
+    hamburgerBtn.classList.add('cmp-td-hamburgerMenu');
+
+    const hamIcon = document.createElement('i');
+    hamIcon.classList.add('fas', 'fa-bars');
+    hamburgerBtn.append(hamIcon);
+
+    figure.parentNode.insertBefore(hamburgerBtn, figure);
+    return hamburgerBtn;
+  }
+
+  hideMegaMenu(active, open) {
+    this.el?.classList.remove(active);
+    this.containerEl?.classList.remove(open);
+    this.open = false;
+    if (this.el?.closest('.cmp-container')) {
+      this.el?.closest('.cmp-container').classList.remove('header-active');
     }
+  }
 
-    hideMegaMenu(active, open) {
-        this.el?.classList.remove(active);
-        this.containerEl?.classList.remove(open);
-        this.open = false;
-        if (this.el?.closest('.cmp-container')) {
-            this.el?.closest('.cmp-container').classList.remove('header-active');
-        }
+  handleClick({ active, open }) {
+    if (!this.open) {
+      this.el.classList.add(active);
+      this.containerEl.classList.add(open);
+      this.open = true;
+      if (this.el?.closest('.cmp-container')) {
+        this.el?.closest('.cmp-container').classList.add('header-active');
+      }
+
+    } else {
+      this.hideMegaMenu(active, open);
     }
+  }
 
-    handleClick({active, open}) {
-        if (!this.open) {
-            this.el.classList.add(active);
-            this.containerEl.classList.add(open);
-            this.open = true;
-            if (this.el?.closest('.cmp-container')) {
-                this.el?.closest('.cmp-container').classList.add('header-active');
-            }
+  hideWhenOutside(event, classObj) {
+    const isInsideMenu = event.target.closest('.cmp-megamenu__body');
+    const isHamburgerBtn = event.target.closest('.cmp-td-hamburgerMenu');
+    const headerActive = event.target.closest('.header-active');
+    const { active, open } = classObj;
 
-        } else {
-            this.hideMegaMenu(active, open);
-        }
-    }
-
-    hideWhenOutside(event, classObj) {
-        const isInsideMenu = event.target.closest('.cmp-megamenu__body');
-        const isHamburgerBtn = event.target.closest('.cmp-td-hamburgerMenu');
-        const headerActive =  event.target.closest('.header-active');
-        const {active, open} = classObj;
-
-        if (isInsideMenu || isHamburgerBtn || headerActive) return;
-        this.hideMegaMenu(active, open);
-    }
+    if (isInsideMenu || isHamburgerBtn || headerActive) return;
+    this.hideMegaMenu(active, open);
+  }
 }
---
+++ content/techdata/ui.frontend/src/components/hamburger-menu/hamburger.js
@@ -0,0 +1,70 @@
+export default class Hamburger {
+    constructor() {
+        const el = this.injectHamburger();
+
+        if (!el) return;
+
+        const classObj = {
+            active: 'active',
+            open: 'megamenu--open',
+        }
+
+        this.el = el;
+        this.open = false;
+        this.containerEl = document.querySelector('.megamenu');
+
+        this.el.addEventListener('click', () => this.handleClick(classObj));
+        document.addEventListener('click', (e) => this.hideWhenOutside(e, classObj));
+    }
+
+    injectHamburger() {
+        const figure = document.querySelector('.dp-figure');
+        const navbar = document.querySelector('#cmp-techdata-header');
+
+        if (!figure || !navbar) return;
+
+        const hamburgerBtn = document.createElement('button');
+        hamburgerBtn.type = 'button';
+        hamburgerBtn.ariaLabel = 'hamburger menu';
+        hamburgerBtn.classList.add('cmp-td-hamburgerMenu');
+
+        const hamIcon = document.createElement('i');
+        hamIcon.classList.add('fas', 'fa-bars');
+        hamburgerBtn.append(hamIcon);
+
+        figure.parentNode.insertBefore(hamburgerBtn, figure);
+        return hamburgerBtn;
+    }
+
+    hideMegaMenu(active, open) {
+        this.el?.classList.remove(active);
+        this.containerEl?.classList.remove(open);
+        this.open = false;
+        if (this.el?.closest('.cmp-container')) {
+            this.el?.closest('.cmp-container').classList.remove('header-active');
+        }
+    }
+
+    handleClick({active, open}) {
+        if (!this.open) {
+            this.el.classList.add(active);
+            this.containerEl.classList.add(open);
+            this.open = true;
+            if (this.el?.closest('.cmp-container')) {
+                this.el?.closest('.cmp-container').classList.add('header-active');
+            }
+        } else {
+            this.hideMegaMenu(active, open);
+        }
+    }
+
+    hideWhenOutside(event, classObj) {
+        const isInsideMenu = event.target.closest('.cmp-megamenu__body');
+        const isHamburgerBtn = event.target.closest('.cmp-td-hamburgerMenu');
+        const headerActive =  event.target.closest('.header-active');
+        const {active, open} = classObj;
+
+        if (isInsideMenu || isHamburgerBtn || headerActive) return;
+        this.hideMegaMenu(active, open);
+    }
+}
