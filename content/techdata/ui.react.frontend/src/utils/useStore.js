bc71b8b007fe5a5a89a1322d8d041559e2b68537
Pruszynski, Andrzej <T407735T@techdata.com>
2023-05-18 11:54:08 +0000

    Merged PR 61251: 441788_OTG-Currency_in_total_column_header_is_not_displaying
    Related work items: #441787

--- content/techdata/ui.react.frontend/src/utils/useStore.js
+++ content/techdata/ui.react.frontend/src/utils/useStore.js
@@ -10,9 +10,10 @@ export const useStore = create((set) => ({
     set((state) => ({
       refreshRenewalDetailApi: !state.refreshRenewalDetailApi,
     })),
-  setUserData: async () => {
+  setUserData: () => {
+    console.log("window.getSessionInfo",window.getSessionInfo())
     console.log('setting user data');
-    await getSessionInfo().then((data) => {
+    window.getSessionInfo && window.getSessionInfo().then((data) => {
       console.log('user data: ', data[1]);
       set((state) => ({ userData: data[1] }));
     });
