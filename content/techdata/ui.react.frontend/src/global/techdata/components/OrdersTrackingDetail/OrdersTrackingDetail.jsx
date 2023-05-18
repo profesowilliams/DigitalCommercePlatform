1249c5fcbc1886e7e251cecdc871e734e65ea927
Adam Dobrzanski <Adam.Dobrzanski@techdata.com>
2023-05-18 15:47:33 +0000

    Merged PR 61285: Rollback details page footer
    Rollback details page footer
    
    Related work items: #351622

--- content/techdata/ui.react.frontend/src/global/techdata/components/OrdersTrackingDetail/OrdersTrackingDetail.jsx
+++ content/techdata/ui.react.frontend/src/global/techdata/components/OrdersTrackingDetail/OrdersTrackingDetail.jsx
@@ -6,56 +6,49 @@ import OrderAcknowledgementCard from './OrderAcknowledgementCard';
 import ContactCard from './ContactCard';
 import { getUrlParams } from '../../../../utils';
 import OrderTrackingDetailHeader from './OrderTrackingDetailHeader';
-import OrderTrackingDetailFooter from './OrderTrackingDetailFooter';
 import OrderTrackingContainer from './OrderTrackingContainer';
 
 function OrdersTrackingDetail(props) {
-  const { id = '' } = getUrlParams();
-  const componentProps = JSON.parse(props.componentProp);
-  const config = {
-    ...componentProps,
-    productLines: { agGridLicenseKey: componentProps?.agGridLicenseKey },
-    serverSide: false,
-    paginationStyle: 'none',
-  };
-  const [apiResponse] = useGet(`${config.uiServiceEndPoint}?id=${id}`);
-  return (
-    <div className="cmp-quote-preview cmp-order-preview">
-      <section>
-        <div className="cmp-orders-qp__config-grid">
-          <OrderTrackingDetailHeader
-            config={config}
-            apiResponse={apiResponse}
-          />
-          <div className="info-container">
-            <SoldToCard
-              soldTo={apiResponse?.content?.shipTo || {}}
-              config={config}
-            />
-            <OrderAcknowledgementCard
-              content={apiResponse?.content || {}}
-              config={config}
-            />
-            <ContactCard content={apiResponse?.content || {}} config={config} />
-          </div>
+    const { id = '' } = getUrlParams();
+    const componentProps = JSON.parse(props.componentProp);
+    const config = {
+        ...componentProps,
+        productLines: { agGridLicenseKey: componentProps?.agGridLicenseKey },
+        serverSide: false,
+        paginationStyle: 'none',
+    };
+    const [apiResponse] = useGet(`${config.uiServiceEndPoint}?id=${id}`);
+    return (
+        <div className="cmp-quote-preview cmp-order-preview">
+            <section>
+                <div className="cmp-orders-qp__config-grid">
+                    <OrderTrackingDetailHeader
+                        config={config}
+                        apiResponse={apiResponse}
+                    />
+                    <div className="info-container">
+                        <SoldToCard
+                            soldTo={apiResponse?.content?.shipTo || {}}
+                            config={config}
+                        />
+                        <OrderAcknowledgementCard
+                            content={apiResponse?.content || {}}
+                            config={config}
+                        />
+                        <ContactCard content={apiResponse?.content || {}} config={config} />
+                    </div>
+                </div>
+                <div className="cmp-orders-qp__grid cmp-order-preview">
+                    <OrderTrackingContainer config={config} />
+                    {apiResponse && (
+                        <OrdersTrackingDetailGrid
+                            data={apiResponse.content}
+                            gridProps={config}
+                        />
+                    )}
+                </div>
+            </section>
         </div>
-        <div className="cmp-orders-qp__grid cmp-order-preview">
-          <OrderTrackingContainer config={config} />
-          {apiResponse && (
-            <OrdersTrackingDetailGrid
-              data={apiResponse.content}
-              gridProps={config}
-            />
-          )}
-        </div>
-        <div className="footer-container">
-          <OrderTrackingDetailFooter
-            config={config}
-            apiResponse={apiResponse}
-          />
-        </div>
-      </section>
-    </div>
-  );
+    );
 }
 export default OrdersTrackingDetail;
