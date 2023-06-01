52672b9e4366aaf9a2d56a5d2b98d5212e35e036
Pruszynski, Andrzej <T407735T@techdata.com>
2023-06-01 13:07:37 +0000

    Merged PR 61915: 450878_Hotfix_for_revert_Export_Flyout
    Related work items: #435468

--- content/tds-core/core/src/main/java/com/tdscore/core/models/package-info.java
+++ content/tds-core/core/src/main/java/com/tdscore/core/models/package-info.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-@Version("2.16")
+@Version("3.0.0")
 package com.tdscore.core.models;
 
 import org.osgi.annotation.versioning.Version;
