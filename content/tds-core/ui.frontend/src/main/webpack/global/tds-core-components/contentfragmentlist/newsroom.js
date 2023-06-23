64aaea5e11f1fb185a70fa116c8a6d6b92900c1a
Venugopal, Shyamala (c) <Shyamala.Venugopal@techdata.com>
2023-06-23 17:23:43 +0000

    Merged PR 62897: [435455]-Contentfragmentlist -newsarticle  clickable
    Related work items: #457693

--- content/tds-core/ui.frontend/src/main/webpack/global/tds-core-components/contentfragmentlist/newsroom.js
+++ content/tds-core/ui.frontend/src/main/webpack/global/tds-core-components/contentfragmentlist/newsroom.js
@@ -1,12 +1,20 @@
 (() => {
-    const contentFragTileELems = document.querySelectorAll('.newsroom-style .cmp-contentfragment');
-    contentFragTileELems && contentFragTileELems.forEach(contentFragTileELem => {
-        const imgEle = contentFragTileELem.querySelector('.cmp-contentfragment__element--articleImage .cmp-contentfragment__element-value');
+  const contentFragTileELems = document.querySelectorAll('.newsroom-style .cmp-contentfragment');
+  if (contentFragTileELems) {
+    contentFragTileELems.forEach((contentFragTileELem) => {
+      const contentFragTileELem1 = contentFragTileELem;
+        const imgEle = contentFragTileELem1.querySelector('.cmp-contentfragment__element--articleImage .cmp-contentfragment__element-value');
         const imgPath = imgEle.innerText.trim() !== '' ? imgEle.innerText.trim() : '/content/dam/techdata/tech-data-dot-com/global/site/about-us/leadership/profile-icon.jpeg';
+        const linkEle = contentFragTileELem1.querySelector('.cmp-contentfragment__element--articlePageReference .cmp-contentfragment__element-value');
+        const linkPath = linkEle.innerText.trim() !== '' ? linkEle.innerText.trim() : '#';
         const tag = document.createElement('img');
         tag.setAttribute('src', imgPath);
         tag.setAttribute('alt', 'Article Image');
         tag.classList.add('cmp-contentfragment__image');
-        contentFragTileELem.prepend(tag);
-    });
+        contentFragTileELem1.prepend(tag);
+        const orgHTML = contentFragTileELem1.innerHTML;
+        const newHTML = `<a href="${linkPath}">${orgHTML}</a>`;
+        contentFragTileELem1.innerHTML = newHTML;
+      });
+  }
 })();
