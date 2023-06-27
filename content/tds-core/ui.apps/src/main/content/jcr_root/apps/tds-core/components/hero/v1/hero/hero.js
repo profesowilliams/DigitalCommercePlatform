5b6127cd88844509d4213d35495763a71aa0fd3a
Ryan Williams <ryan.williams@tdsynnex.com>
2023-06-27 21:45:06 +0000

    Merged PR 63004: #456581 Removed rogue code
    #456581 Removed rogue code
    
    Fixes #459442:
    - Removed leftover code from hero.js
    
    Related work items: #456581, #459442

--- content/tds-core/ui.apps/src/main/content/jcr_root/apps/tds-core/components/hero/v1/hero/hero.js
+++ content/tds-core/ui.apps/src/main/content/jcr_root/apps/tds-core/components/hero/v1/hero/hero.js
@@ -21,21 +21,3 @@ use(function () {
         path: path
     };
 });
-
-// Get all cmp-teaser__bottom-section--wrapper div elements
-const wrapperDivs = document.querySelectorAll('.cmp-teaser__bottom-section--wrapper');
-
-// Iterate over each wrapper div
-wrapperDivs.forEach(wrapperDiv => {
-    // Get the first child link element
-    const link = wrapperDiv.querySelector('.cmp-teaser__bottom-section--link:first-child');
-
-    // Add click event listener to the wrapper div
-    wrapperDiv.addEventListener('click', () => {
-        // Check if the first child link exists and has a href attribute
-        if (link && link.href) {
-            // Redirect to the link's URL
-            window.location.href = link.href;
-        }
-    });
-});
