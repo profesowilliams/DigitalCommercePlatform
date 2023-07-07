76c1f1844fcca6eb25e15e44b48b618e9ebcf612
Venugopal, Shyamala (c) <Shyamala.Venugopal@techdata.com>
2023-07-07 17:27:23 +0000

    Merged PR 63563: [462838]-Contentfragmentlist dateformat fix
    Related work items: #462840

--- content/techdata/ui.frontend/src/main/webpack/global/td-components/contentfragmentlist/newsroom.js
+++ content/techdata/ui.frontend/src/main/webpack/global/td-components/contentfragmentlist/newsroom.js
@@ -4,6 +4,8 @@
     contentFragTileELems.forEach((contentFragTileELem) => {
       const contentFragTileELem1 = contentFragTileELem;
       const imgEle = contentFragTileELem1.querySelector('.cmp-contentfragment__element--articleImage .cmp-contentfragment__element-value');
+      let dateEle = contentFragTileELem1.querySelector('.cmp-contentfragment__element--press-release-date .cmp-contentfragment__element-value');
+      const dateValue = dateEle.innerText.trim();
       const imgPath = imgEle.innerText.trim() !== '' ? imgEle.innerText.trim() : '/content/dam/techdata/tech-data-dot-com/global/site/about-us/leadership/profile-icon.jpeg';
       const linkEle = contentFragTileELem1.querySelector('.cmp-contentfragment__element--articlePageReference .cmp-contentfragment__element-value');
       const linkPath = linkEle.innerText.trim() !== '' ? linkEle.innerText.trim() : '#';
@@ -15,6 +17,15 @@
       const orgHTML = contentFragTileELem1.innerHTML;
       const newHTML = `<a href="${linkPath}">${orgHTML}</a>`;
       contentFragTileELem1.innerHTML = newHTML;
+
+      if (dateValue) {
+        const dateValues = dateValue.split('-');
+        const dayValue = dateValues[2].split(' ');
+        const dateFormat = new Date(dateValues[0], dateValues[1], dayValue[0]);
+        const month = dateFormat.toLocaleString('default', { month: 'short' });
+        dateEle = contentFragTileELem1.querySelector('.cmp-contentfragment__element--press-release-date .cmp-contentfragment__element-value');
+        dateEle.innerText = `${month} ${dayValue[0]}`;
+      }
     });
   }
 })();
