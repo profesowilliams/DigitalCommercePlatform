8be0b6994ba93999d3f8576837f6908135d6e32d
Pruszynski, Andrzej <T407735T@techdata.com>
2023-10-30 14:18:31 +0000

    Merged PR 69740: [492212]-Order-Details-Order-Lines-Action-Menus-in-Order-Tracking-Request-to-...
    [492212]-Order-Details-Order-Lines-Action-Menus-in-Order-Tracking-Request-to-show-unavailable-items-in-light-grey-and-inactive: add disabled styling to action dropdown
    
    Related work items: #492212

--- content/tds-site/ui.react.frontend/src/global/techdata/components/OrdersTrackingDetail/Header/MenuActions.jsx
+++ content/tds-site/ui.react.frontend/src/global/techdata/components/OrdersTrackingDetail/Header/MenuActions.jsx
@@ -4,15 +4,16 @@ import { getDictionaryValueOrKey } from '../../../../../utils/utils';
 const MenuActions = ({ items = [] }) => {
   return (
     <ul>
-      {items.map(
-        (item) =>
-          item.condition && (
-            <li key={item.label} onClick={item.onClick}>
-              {getDictionaryValueOrKey(item.label)}
-            </li>
-          )
-      )}
+      {items.map((item) => (
+        <li
+          key={item.label}
+          className={!item.condition ? 'disabled' : null}
+          onClick={item.condition ? item.onClick : null}
+        >
+          {getDictionaryValueOrKey(item.label)}
+        </li>
+      ))}
     </ul>
   );
 };
-export default MenuActions;
+export default MenuActions; 
