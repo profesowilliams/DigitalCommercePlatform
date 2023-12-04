25316819413dff22496efca5e2dc7e621ac5da10
Venugopal, Shyamala (c) <Shyamala.Venugopal@techdata.com>
2023-12-04 16:01:24 +0000

    Merged PR 71824: [504222]:Renewals APJ -UAT-APJ Renewals : IBM : India-unable to open renewal detail page
    Related work items: #504527

--- content/techdata/ui.react.frontend/src/global/techdata/components/RenewalsDetails/ConfigGrid/AgreementInfo.jsx
+++ content/techdata/ui.react.frontend/src/global/techdata/components/RenewalsDetails/ConfigGrid/AgreementInfo.jsx
@@ -30,18 +30,18 @@ function AgreementInfo({
     return (
       <div className="cmp-renewals-qp__agreement-info--address-group">
         <p>
-          <Info label={agreementInfo.quoteNo} multipleOrderFlag={source?.id.indexOf('see line') > -1 ? true : false}>{source?.id}</Info>
+          <Info label={agreementInfo.quoteNo} multipleOrderFlag={source?.id?.indexOf('see line') > -1 ? true : false}>{source?.id}</Info>
           <Info label={agreementInfo.vendorQuoteId} multipleOrderFlag={vendorReference?.value.indexOf('see line') > -1 ? true : false}>{vendorReference?.value}</Info>
-          <Info label={agreementInfo.agreementNoLabel} multipleOrderFlag={contract?.id.indexOf('see line') > -1 ? true : false}>{contract?.id}</Info>
+          <Info label={agreementInfo.agreementNoLabel} multipleOrderFlag={contract?.id?.indexOf('see line') > -1 ? true : false}>{contract?.id}</Info>
         </p>
         <p>
-          <Info label={agreementInfo.programLabel} multipleOrderFlag={programName.indexOf('see line') > -1 ? true : false}>{programName}</Info>
-          <Info label={agreementInfo.termLabel} multipleOrderFlag={contract?.renewedDuration.indexOf('see line') > -1 ? true : false}>{contract?.renewedDuration}</Info>
-          <Info label={agreementInfo.supportLevelLabel} multipleOrderFlag={contract?.serviceLevel.indexOf('see line') > -1 ? true : false}>{contract?.serviceLevel}</Info>
+          <Info label={agreementInfo.programLabel} multipleOrderFlag={programName?.indexOf('see line') > -1 ? true : false}>{programName}</Info>
+          <Info label={agreementInfo.termLabel} multipleOrderFlag={contract?.renewedDuration?.indexOf('see line') > -1 ? true : false}>{contract?.renewedDuration}</Info>
+          <Info label={agreementInfo.supportLevelLabel} multipleOrderFlag={contract?.serviceLevel?.indexOf('see line') > -1 ? true : false}>{contract?.serviceLevel}</Info>
         </p>
         <p>
-          <Info label={agreementInfo.quoteExpiryDateLabel} multipleOrderFlag={formattedExpiry.indexOf('see line') > -1 ? true : false}>{formattedExpiry}</Info>
-          <Info label={agreementInfo.dueDateLabel} multipleOrderFlag={formattedDueDate.indexOf('see line') > -1 ? true : false}>{formattedDueDate}</Info>
+          <Info label={agreementInfo.quoteExpiryDateLabel} multipleOrderFlag={formattedExpiry?.indexOf('see line') > -1 ? true : false}>{formattedExpiry}</Info>
+          <Info label={agreementInfo.dueDateLabel} multipleOrderFlag={formattedDueDate?.indexOf('see line') > -1 ? true : false}>{formattedDueDate}</Info>
           <DurationDates label={agreementInfo.duration} startDate={contract?.formattedNewAgreementStartDate}
           endDate={contract?.formattedNewAgreementEndDate}
           multipleOrderFlag={contract?.formattedNewAgreementStartDate?.indexOf('see line') > -1 ? true : false}/>
