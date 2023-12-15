624300b2f09b1cfdf3104699584a8356294fc347
Pruszynski, Andrzej <T407735T@techdata.com>
2023-12-15 13:15:53 +0000

    Merged PR 72570: 506419-Authoring-ALM-PL-UX-issue-restriction-page
    Related work items: #506419

--- content/tds-site/ui.react.frontend/src/global/techdata/components/AccessPermissionsNeeded/AccessPermissionsNeeded.jsx
+++ content/tds-site/ui.react.frontend/src/global/techdata/components/AccessPermissionsNeeded/AccessPermissionsNeeded.jsx
@@ -5,21 +5,20 @@ import { getDictionaryValueOrKey } from '../../../../utils/utils';
 const AccessPermissionsNeeded = ({ noAccessProps }) => {
   const { noAccessTitle, noAccessMessage, noAccessBack } = noAccessProps;
   return (
-    <div className="access-permissions_wrapper">
-      <div className="access-permissions_div">
-        <AccessPermisionsNeededIcon />
-        <h2 className="access-permissions_header">
-          {getDictionaryValueOrKey(noAccessTitle)}
-        </h2>
-        <h5 className="access-permissions_content">
+    <div className="access-permissions__wrapper">
+      <AccessPermisionsNeededIcon />
+      <h2 className="access-permissions__header">
+        {getDictionaryValueOrKey(noAccessTitle)}
+      </h2>
+      <div className="access-permissions__content">
+        <p className="access-permissions__message">
           {getDictionaryValueOrKey(noAccessMessage)}
-          <br />
-          <p>
-            <a href="https://intouch.staging.tdsynnex.eu/InTouch/MVC/MicroSite/Private?corpregionid=14&categorypageid=10612&msmenuid=11579">
-              {getDictionaryValueOrKey(noAccessBack)}
-            </a>
-          </p>
-        </h5>
+        </p>
+        <p className="access-permissions__link">
+          <a href="https://intouch.staging.tdsynnex.eu/InTouch/MVC/MicroSite/Private?corpregionid=14&categorypageid=10612&msmenuid=11579">
+            {getDictionaryValueOrKey(noAccessBack)}
+          </a>
+        </p>
       </div>
     </div>
   );
