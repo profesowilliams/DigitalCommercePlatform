59a5f2abe78a1c281992cf535ff42d081c1b992e
Adam Dobrzanski <Adam.Dobrzanski@techdata.com>
2023-12-21 13:47:26 +0000

    Merged PR 72904: #507026 - actual shipdate NA
    #507026 - actual shipdate NA
    
    Related work items: #507026

--- content/tds-site/ui.react.frontend/src/global/techdata/components/OrdersTrackingGrid/Columns/ShippedTabGrid/Columns/ShipDateColumn.jsx
+++ content/tds-site/ui.react.frontend/src/global/techdata/components/OrdersTrackingGrid/Columns/ShippedTabGrid/Columns/ShipDateColumn.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 function ShipDateColumn({ line }) {
-  return <div>{line?.actualShipDateFormatted || '-'}</div>;
+  return <div>{line?.actualShipDateFormatted || line?.actualShipDateNotAvailableTranslated || '-'}</div>;
 }
 export default ShipDateColumn;
