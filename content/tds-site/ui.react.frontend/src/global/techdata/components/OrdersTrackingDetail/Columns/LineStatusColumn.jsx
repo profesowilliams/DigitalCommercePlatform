52a5ad4b5c747d46358bf98cf57c2c37da37df8c
Pruszynski, Andrzej <T407735T@techdata.com>
2024-02-26 11:16:41 +0000

    Merged PR 76390: 522931-UX-Design-Order-Details-Page-Change-Color-of-links-to-Teal
    Related work items: #522931

--- content/tds-site/ui.react.frontend/src/global/techdata/components/OrdersTrackingDetail/Columns/LineStatusColumn.jsx
+++ content/tds-site/ui.react.frontend/src/global/techdata/components/OrdersTrackingDetail/Columns/LineStatusColumn.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useOrderTrackingStore } from '../../OrdersTrackingGrid/store/OrderTrackingStore';
 import { WarningTriangle } from '../../../../../fluentIcons/FluentIcons';
 import { getDictionaryValueOrKey } from '../../../../../utils/utils';
