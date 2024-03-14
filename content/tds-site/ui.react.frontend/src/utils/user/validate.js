9d2f36c61e07b81d2469349a09c2c84b3e61df59
Adam Dobrzanski <Adam.Dobrzanski@techdata.com>
2024-03-14 10:50:18 +0000

    Merged PR 77689: Added query params to user redirect url
    Added query params to user redirect url
    
    Related work items: #487008

--- content/tds-site/ui.react.frontend/src/utils/user/validate.js
+++ content/tds-site/ui.react.frontend/src/utils/user/validate.js
@@ -21,15 +21,12 @@ const redirectBasedOnCountry = (userData) => {
     if (window.location.href.toLowerCase().indexOf(countryCodeFromAPI.toLowerCase()) < 0) {
       return validateCountryFromUrlAndAPI(countryCodeFromAPI);
     }
-  } else {
-    console.log("RedirectBasedOnCountry missing required data");
   }
   return false;
 }
 
 const validateCountryFromUrlAndAPI = (countryCodeFromAPI) => {
   let infoFromUrl = getHeaderInfo();
-  console.log("validateCountryFromUrlAndAPI country = " + infoFromUrl.site + ", language = " + infoFromUrl.language);
   let countryCodeFromUrl = '/' + infoFromUrl.site + '/' + infoFromUrl.language;
   return performRedirect(countryCodeFromUrl, countryCodeFromAPI);
 }
@@ -37,11 +34,9 @@ const validateCountryFromUrlAndAPI = (countryCodeFromAPI) => {
 const performRedirect = (countryCodeFromUrl, countryCodeFromAPI) => {
   if (countryCodeFromAPI.toLowerCase().indexOf(countryCodeFromUrl.toLowerCase()) < 0) {
     let resultantRedirectUrl = window.location.href.toLowerCase().replace(countryCodeFromUrl.toLowerCase(), countryCodeFromAPI);
-    console.log("performRedirect = " + resultantRedirectUrl);
-    window.location.href = resultantRedirectUrl;
+    window.location.href = resultantRedirectUrl + window.location.search;
     return true;
   } else {
-    console.log("performRedirect = skip");
     return false;
   }
 }
