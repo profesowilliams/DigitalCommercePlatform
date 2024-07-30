4723ea4adb8049e0f29d0f751321024090ddcf9e
Besinovic, Ivan <T114491C@techdata.com>
2024-07-30 15:35:54 +0000

    Merged PR 85894: [591305] Add Workflow for detecting structural and text changes on pages
    [591305] Add Workflow for detecting structural and text changes on pages
    
    Related work items: #591305

---
+++ content/techdata/core/src/main/java/com/techdata/core/workflow/process/impl/ApjPageChangesProcess.java
@@ -0,0 +1,136 @@
+package com.techdata.core.workflow.process.impl;
+
+import com.adobe.granite.workflow.WorkflowException;
+import com.adobe.granite.workflow.WorkflowSession;
+import com.adobe.granite.workflow.exec.WorkItem;
+import com.adobe.granite.workflow.exec.WorkflowProcess;
+import com.adobe.granite.workflow.metadata.MetaDataMap;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.version.Version;
+import javax.jcr.version.VersionHistory;
+import javax.jcr.version.VersionIterator;
+import javax.jcr.version.VersionManager;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component(
+        service = WorkflowProcess.class,
+        property = {"process.label=Techdata Determine if there are structural or text changes on APJ"}
+)
+public class ApjPageChangesProcess implements WorkflowProcess {
+    private static final Logger log = LoggerFactory.getLogger(CatalogServiceSynchProcess.class);
+    public static final String REQUIRES_VALIDATION = "requiresValidation";
+    public static final String STRUCTURAL_CHANGES = "structuralChanges";
+    public static final String TEXT_CHANGES = "textChanges";
+    public static final String CQ_LAST_REPLICATED = "cq:lastReplicated";
+    private static final String SLASH_JCR_CONTENT = "/jcr:content";
+
+    @Override
+    public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap args) throws WorkflowException {
+        try {
+            ResourceResolver resourceResolver = workflowSession.adaptTo(ResourceResolver.class);
+            String payloadPath = workItem.getWorkflowData().getPayload().toString();
+            Resource pageResource = resourceResolver.getResource(payloadPath + SLASH_JCR_CONTENT);
+
+            if (pageResource != null && !pageResource.isResourceType(Resource.RESOURCE_TYPE_NON_EXISTING)) {
+                PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+                Page page = pageManager.getContainingPage(pageResource);
+                Node currentNode = page.adaptTo(Node.class);
+
+                Version currentVersion = getCurrentVersion(currentNode);
+                Version lastActivatedVersion = getLastActivatedVersion(currentNode);
+                boolean hasStructuralChanges = hasStructuralChanges(currentVersion, lastActivatedVersion);
+
+                if (hasStructuralChanges) {
+                    workItem.getWorkflow().getMetaDataMap().put(REQUIRES_VALIDATION, STRUCTURAL_CHANGES);
+                } else {
+                    workItem.getWorkflow().getMetaDataMap().put(REQUIRES_VALIDATION, TEXT_CHANGES);
+                }
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new WorkflowException(e.getMessage(), e);
+        }
+    }
+
+    private Version getLastActivatedVersion(Node node) throws RepositoryException {
+        Session session = node.getSession();
+        VersionManager versionManager = session.getWorkspace().getVersionManager();
+        VersionHistory versionHistory = versionManager.getVersionHistory(node.getPath() + SLASH_JCR_CONTENT);
+
+        Version lastActivatedVersion = null;
+        Calendar lastReplicationDate = null;
+
+        for (VersionIterator it = versionHistory.getAllVersions(); it.hasNext(); ) {
+            Version version = it.nextVersion();
+            Node frozenNode = version.getFrozenNode();
+
+            if (frozenNode.hasProperty(CQ_LAST_REPLICATED)) {
+                Calendar replicationDate = frozenNode.getProperty(CQ_LAST_REPLICATED).getDate();
+                if (lastReplicationDate == null || replicationDate.after(lastReplicationDate)) {
+                    lastActivatedVersion = version;
+                    lastReplicationDate = replicationDate;
+                }
+            }
+        }
+
+        return lastActivatedVersion;
+    }
+
+    private Version getCurrentVersion(Node node) throws RepositoryException {
+        VersionManager versionManager = node.getSession().getWorkspace().getVersionManager();
+
+        return versionManager.getBaseVersion(node.getPath() + SLASH_JCR_CONTENT);
+    }
+
+    private boolean hasStructuralChanges(Version currentVersion, Version lastActivatedVersion) throws RepositoryException {
+        if (lastActivatedVersion == null || currentVersion == null) {
+            return false;
+        }
+
+        return hasStructuralChangesInsideChildNodes(currentVersion.getFrozenNode(), lastActivatedVersion.getFrozenNode());
+    }
+
+    private boolean hasStructuralChangesInsideChildNodes(Node currentNode, Node lastActivatedNode) throws RepositoryException {
+        Set<String> currentChildNames = getChildNodeNames(currentNode);
+        Set<String> lastActivatedChildNames = getChildNodeNames(lastActivatedNode);
+
+        if (!currentChildNames.equals(lastActivatedChildNames)) {
+            return true;
+        }
+
+        for (String childName : currentChildNames) {
+            Node currentChildNode = currentNode.getNode(childName);
+            Node lastActivatedChildNode = lastActivatedNode.getNode(childName);
+
+            if (hasStructuralChangesInsideChildNodes(currentChildNode, lastActivatedChildNode)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private Set<String> getChildNodeNames(Node node) throws RepositoryException {
+        Set<String> childNames = new HashSet<>();
+        NodeIterator nodeIterator = node.getNodes();
+        while (nodeIterator.hasNext()) {
+            Node childNode = nodeIterator.nextNode();
+            childNames.add(childNode.getName());
+        }
+
+        return childNames;
+    }
+}
---
+++ content/techdata/core/src/test/java/com/techdata/core/workflows/ApjPageChangesProcessTest.java
@@ -0,0 +1,194 @@
+package com.techdata.core.workflows;
+
+import com.adobe.granite.workflow.WorkflowSession;
+import com.adobe.granite.workflow.exec.WorkItem;
+import com.adobe.granite.workflow.exec.Workflow;
+import com.adobe.granite.workflow.exec.WorkflowData;
+import com.adobe.granite.workflow.metadata.MetaDataMap;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import com.techdata.core.workflow.process.impl.ApjPageChangesProcess;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.Property;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.Workspace;
+import javax.jcr.version.Version;
+import javax.jcr.version.VersionHistory;
+import javax.jcr.version.VersionIterator;
+import javax.jcr.version.VersionManager;
+import java.util.Calendar;
+import java.util.UUID;
+
+import static com.techdata.core.workflow.process.impl.ApjPageChangesProcess.CQ_LAST_REPLICATED;
+import static com.techdata.core.workflow.process.impl.ApjPageChangesProcess.REQUIRES_VALIDATION;
+import static com.techdata.core.workflow.process.impl.ApjPageChangesProcess.STRUCTURAL_CHANGES;
+import static com.techdata.core.workflow.process.impl.ApjPageChangesProcess.TEXT_CHANGES;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ApjPageChangesProcessTest {
+
+    @InjectMocks
+    private ApjPageChangesProcess apjPageChangesProcess;
+
+    @Mock
+    private WorkItem workItem;
+
+    @Mock
+    private WorkflowSession workflowSession;
+
+    @Mock
+    private MetaDataMap args;
+
+    @Mock
+    private ResourceResolver resourceResolver;
+
+    @Mock
+    private PageManager pageManager;
+
+    @Mock
+    private Page page;
+
+    @Mock
+    private Resource pageResource;
+
+    @Mock
+    private Node currentNode;
+
+    @Mock
+    private Session session;
+
+    @Mock
+    private VersionManager versionManager;
+
+    @Mock
+    private VersionHistory versionHistory;
+
+    @Mock
+    private Version currentVersion;
+
+    @Mock
+    private Version lastActivatedVersion;
+
+    @Mock
+    private Node currentFrozenNode;
+
+    @Mock
+    private Node lastActivatedFrozenNode;
+
+    @Mock
+    private WorkflowData workflowData;
+
+    @Mock
+    private Workflow workflow;
+
+    @Mock
+    private MetaDataMap metaDataMap;
+
+    private static final String PAGE_PATH = UUID.randomUUID().toString();
+    private static final String CHILD_NODE_NAME_1 = UUID.randomUUID().toString();
+    private static final String CHILD_NODE_NAME_2 = UUID.randomUUID().toString();
+
+    @BeforeEach
+    public void setUp() throws RepositoryException {
+
+
+        when(workflowSession.adaptTo(ResourceResolver.class)).thenReturn(resourceResolver);
+        when(resourceResolver.getResource(anyString())).thenReturn(pageResource);
+        when(pageResource.isResourceType(Resource.RESOURCE_TYPE_NON_EXISTING)).thenReturn(false);
+        when(resourceResolver.adaptTo(PageManager.class)).thenReturn(pageManager);
+        when(pageManager.getContainingPage(pageResource)).thenReturn(page);
+        when(page.adaptTo(Node.class)).thenReturn(currentNode);
+        when(currentNode.getSession()).thenReturn(session);
+        when(session.getWorkspace()).thenReturn(mock(Workspace.class));
+        when(session.getWorkspace().getVersionManager()).thenReturn(versionManager);
+        when(workItem.getWorkflowData()).thenReturn(workflowData);
+        when(workItem.getWorkflow()).thenReturn(workflow);
+        when(workflow.getMetaDataMap()).thenReturn(metaDataMap);
+    }
+
+    @Test
+    public void testExecuteWithNoStructuralChanges() throws Exception {
+        Calendar replicationDate = Calendar.getInstance();
+
+        when(workItem.getWorkflowData().getPayload()).thenReturn(PAGE_PATH);
+        when(versionManager.getBaseVersion(anyString())).thenReturn(currentVersion);
+        when(versionManager.getVersionHistory(anyString())).thenReturn(versionHistory);
+        when(currentVersion.getFrozenNode()).thenReturn(currentFrozenNode);
+        when(lastActivatedVersion.getFrozenNode()).thenReturn(lastActivatedFrozenNode);
+
+        when(lastActivatedFrozenNode.hasProperty(CQ_LAST_REPLICATED)).thenReturn(true);
+        when(lastActivatedFrozenNode.getProperty(CQ_LAST_REPLICATED)).thenReturn(mock(Property.class));
+        when(lastActivatedFrozenNode.getProperty(CQ_LAST_REPLICATED).getDate()).thenReturn(replicationDate);
+
+        VersionIterator versionIterator = mock(VersionIterator.class);
+        when(versionIterator.hasNext()).thenReturn(true, false);
+        when(versionIterator.nextVersion()).thenReturn(lastActivatedVersion);
+        when(versionHistory.getAllVersions()).thenReturn(versionIterator);
+
+        NodeIterator nodeIterator = mock(NodeIterator.class);
+        when(nodeIterator.hasNext()).thenReturn(false);
+
+        when(currentFrozenNode.getNodes()).thenReturn(nodeIterator);
+        when(lastActivatedFrozenNode.getNodes()).thenReturn(nodeIterator);
+
+        apjPageChangesProcess.execute(workItem, workflowSession, args);
+
+        verify(workItem.getWorkflow().getMetaDataMap()).put(REQUIRES_VALIDATION, TEXT_CHANGES);
+    }
+
+    @Test
+    public void testExecuteWithStructuralChanges() throws Exception {
+        Calendar replicationDate = Calendar.getInstance();
+
+        when(workItem.getWorkflowData().getPayload()).thenReturn(PAGE_PATH);
+        when(versionManager.getBaseVersion(anyString())).thenReturn(currentVersion);
+        when(versionManager.getVersionHistory(anyString())).thenReturn(versionHistory);
+        when(currentVersion.getFrozenNode()).thenReturn(currentFrozenNode);
+
+        when(lastActivatedVersion.getFrozenNode()).thenReturn(lastActivatedFrozenNode);
+        when(lastActivatedFrozenNode.hasProperty(CQ_LAST_REPLICATED)).thenReturn(true);
+        when(lastActivatedFrozenNode.getProperty(CQ_LAST_REPLICATED)).thenReturn(mock(Property.class));
+        when(lastActivatedFrozenNode.getProperty(CQ_LAST_REPLICATED).getDate()).thenReturn(replicationDate);
+
+        VersionIterator versionIterator = mock(VersionIterator.class);
+        when(versionIterator.hasNext()).thenReturn(true, false);
+        when(versionIterator.nextVersion()).thenReturn(lastActivatedVersion);
+        when(versionHistory.getAllVersions()).thenReturn(versionIterator);
+
+        Node currentChildNode1 = mock(Node.class);
+        Node currentChildNode2 = mock(Node.class);
+        Node lastActivatedChildNode = mock(Node.class);
+        NodeIterator currentChildNodes = mock(NodeIterator.class);
+        when(currentChildNodes.hasNext()).thenReturn(true, true, false);
+        when(currentChildNodes.nextNode()).thenReturn(currentChildNode1, currentChildNode2);
+        when(currentChildNode1.getName()).thenReturn(CHILD_NODE_NAME_1);
+        when(currentChildNode2.getName()).thenReturn(CHILD_NODE_NAME_2);
+
+        NodeIterator lastActivatedChildNodes = mock(NodeIterator.class);
+        when(lastActivatedChildNodes.hasNext()).thenReturn(true, false);
+        when(lastActivatedChildNodes.nextNode()).thenReturn(lastActivatedChildNode);
+        when(lastActivatedChildNode.getName()).thenReturn(CHILD_NODE_NAME_1);
+
+        when(currentFrozenNode.getNodes()).thenReturn(currentChildNodes);
+        when(lastActivatedFrozenNode.getNodes()).thenReturn(lastActivatedChildNodes);
+
+        apjPageChangesProcess.execute(workItem, workflowSession, args);
+
+        verify(workItem.getWorkflow().getMetaDataMap()).put(REQUIRES_VALIDATION, STRUCTURAL_CHANGES);
+    }
+}
