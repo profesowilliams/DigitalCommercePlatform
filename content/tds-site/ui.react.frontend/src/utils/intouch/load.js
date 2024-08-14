d54a8ddd9524dafbd8c9f995a94d1cadbe895010
Adam Dobrzanski <Adam.Dobrzanski@techdata.com>
2024-08-14 14:55:39 +0000

    Merged PR 86944: XS✔ ◾ Intouch header/footer rework (copied from AEM separation)
    Intouch header/footer rework (copied from AEM separation)
    
    Related work items: #487008

--- content/tds-site/ui.react.frontend/src/utils/intouch/load.js
+++ content/tds-site/ui.react.frontend/src/utils/intouch/load.js
@@ -1,11 +1,26 @@
 import { intouchHeaderAPIUrl, intouchFooterAPIUrl } from "../intouchUtils";
-import { headerInfo, checkIntouchUser } from "../user/get";
+import { checkIntouchUser, loginPageUrl } from "../user/get";
+
+let headerLoadFailed = 0;
+let footerLoadFailed = 0;
+const MAX_LOAD_FAILED = 3;
+const REPEAT_PROMISES = 3;
+const REPEAT_INTERVAL = 2000; //2sec
+
+const redirectToLoginPage = () => {
+  window.location = loginPageUrl;
+  console.error('redirect to loginpage!');
+}
 
 const renderIntouchHeaderHTML = () => {
   const url = intouchHeaderAPIUrl();
   const element = document.getElementById('intouch-headerhtml');
 
   if (!element) return;
+  if (headerLoadFailed >= MAX_LOAD_FAILED) {
+    redirectToLoginPage();
+    return;
+  }
 
   return fetch(url, {
     method: 'GET',
@@ -15,9 +30,13 @@ const renderIntouchHeaderHTML = () => {
     .then(checkStatus)
     .then(parseResponse)
     .then(function (data) {
-      element.innerHTML = data;
+      if (element.innerHTML.length === 0) {
+        element.innerHTML = data;
+      }
     }).catch(function (error) {
+      headerLoadFailed++;
       console.log('Request failed', error);
+      renderIntouchHeaderHTML();
     });
 };
 
@@ -27,6 +46,11 @@ const renderIntouchFooterHTML = () => {
 
   if (!element) return;
 
+  if (footerLoadFailed >= MAX_LOAD_FAILED) {
+    redirectToLoginPage();
+    return;
+  }
+
   return fetch(url, {
     method: 'GET',
     credentials: "include",
@@ -35,9 +59,13 @@ const renderIntouchFooterHTML = () => {
     .then(checkStatus)
     .then(parseResponse)
     .then(function (data) {
-      element.innerHTML = data;
+      if (element.innerHTML.length === 0) {
+        element.innerHTML = data;
+      }
     }).catch(function (error) {
+      footerLoadFailed++;
       console.log('Request failed', error);
+      renderIntouchFooterHTML();
     });
 };
 
@@ -53,21 +81,49 @@ const parseResponse = (response) => {
   return response.text()
 }
 
+const loadIntouchHeaderAndFooterPromises = () => {
+  checkIntouchUser().then(
+    Promise.all([
+      renderIntouchHeaderHTML(),
+      renderIntouchFooterHTML()
+    ]).then(function () {
+      window.td?.deferred?.$htmlLoaded?.resolve();
+    })
+  )
+}
+
+const loadIntouchHeaderAndFooterLoop = () => {
+  let authorMode = !(typeof Granite === 'undefined' || typeof Granite.author === 'undefined');
+  if (authorMode) return;
+
+  let tries = 0;
+  loadIntouchHeaderAndFooterPromises();
+
+  const intervalId = setInterval(() => {
+    // Check if the innerHTML length of the element is 0
+    if (document.getElementById('intouch-headerhtml').innerHTML.length === 0) {
+      // Call the function
+      loadIntouchHeaderAndFooterPromises();
+      tries += 1;
+    }
+
+    // Stop the interval if max tries reached or if the condition is met
+    if (tries >= REPEAT_PROMISES || document.getElementById('intouch-headerhtml').innerHTML.length > 0) {
+      clearInterval(intervalId);
+    }
+  }, REPEAT_INTERVAL);
+}
+
 export const loadIntouchHeaderAndFooter = () => {
-  document.addEventListener(
-    'DOMContentLoaded',
-    function () {
-      let authorMode = !(typeof Granite === 'undefined' || typeof Granite.author === 'undefined');
-      if (!authorMode) {
-        Promise.all([
-          checkIntouchUser(),
-          renderIntouchHeaderHTML(),
-          renderIntouchFooterHTML()
-        ]).then(function () {
-          window.td?.deferred?.$htmlLoaded?.resolve();
-        })
-      } 
-    },
-    false
-  );
+  if (document.readyState !== 'loading') {
+    loadIntouchHeaderAndFooterLoop();
+  } else {
+    document.addEventListener(
+      'DOMContentLoaded',
+      function () {
+        loadIntouchHeaderAndFooterLoop();
+      },
+      false
+    );
+  }
 }
