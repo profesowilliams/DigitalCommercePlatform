e5e7465e0a451b843baf4e6bb26240c4f528988a
Besinovic, Ivan <T114491C@techdata.com>
2024-09-23 18:16:07 +0000

    Merged PR 89403: 601968 - Remove changing of feature flags httpOnlyEnabled and extraLoadDisabled
    601968 - Remove changing of feature flags httpOnlyEnabled and extraLoadDisabled
    
    Related work items: #601968

--- content/tds-core/core/src/main/java/com/tdscore/core/slingcaconfig/FeatureFlagsConfiguration.java
+++ content/tds-core/core/src/main/java/com/tdscore/core/slingcaconfig/FeatureFlagsConfiguration.java
@@ -7,15 +7,9 @@ import org.osgi.service.metatype.annotations.AttributeDefinition;
 
 @Configuration(label = "Feature Flags Configuration", description = "Enable and disable feature flags")
 public @interface FeatureFlagsConfiguration {
-    
-    @Property(label = "Http Only Enabled Flag", description = "Allow/Disallow cookies from being sent as part of requests.")
-    boolean httpOnlyEnabled() default false;
 
     @Property(label = "Environment Header Enabled Flag", description = "Enable Evnironment header to be sent as part of requests.")
     boolean environmentEnabled() default false;
-
-    @Property(label = "Extra Reload Disabled Flag", description = "Disable the extra reload that happens upon signing in to DCP.")
-    boolean extraReloadDisabled() default false;
 
     @Property(label = "Disable checks for DCP Access", description = "Helps to disable DCP checks for access for components, enabled by default.")
     boolean disableChecksForDCPAccess() default false;
