8fb39652a201861fcd17c5059d3589ab00c8bda6
Ryan Williams <ryan.williams@tdsynnex.com>
2024-11-04 13:24:33 +0000

    Merged PR 92046: #608718 Update components to latest version
    #608718 Update components to lastest version
    
    Fixes #609045:
    - Updated compoents to the latest release
    - Added certs
    - Fixed app.connector.js React issue.
    
    Related work items: #608718, #609045

--- content/techdata/ui.react.frontend/src/utils/app.connector.js
+++ content/techdata/ui.react.frontend/src/utils/app.connector.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactContentRenderer from "react-dom";
+import { createRoot } from "react-dom/client";
 import { nanoid } from "nanoid";
 import store from "../store/store";
 import { Provider } from "react-redux";
@@ -25,29 +25,25 @@ class AppConnector {
 		var _self = this;
 		this.components = [];
 		this.componentProps = componentProps;
-		writeLog(
-			this.logs,
-			"attempting to import",
-			{ path: `../global/techdata/components/${this.componentProps.component}/${this.componentProps.component}` }
-		);
-		import(
-			`../global/techdata/components/${this.componentProps.component}/${this.componentProps.component}`
-		)
+		writeLog(this.logs, "attempting to import", {
+			path: `../global/techdata/components/${this.componentProps.component}/${this.componentProps.component}`,
+		});
+		import(`../global/techdata/components/${this.componentProps.component}/${this.componentProps.component}`)
 			.then((component) => {
 				writeLog(_self.logs, "component imported", { component: component });
 				_self.loadComponent(component.default, element);
 			})
 			.catch((error) => {
-				writeLog(_self.logs, "error occured", { error: error });
+				writeLog(_self.logs, "error occurred", { error: error });
 				console.error(
-					`${_self.componentProps.component} not initialized succesfully. Check logs below:`
+					`${_self.componentProps.component} not initialized successfully. Check logs below:`
 				);
 				console.error(_self.logs);
 			});
 	}
 
 	loadComponent(componentRef, element) {
-		let Component = componentRef; // Captilize is mandatory for the variable 'Component'
+		let Component = componentRef; // Capitalize is mandatory for the variable 'Component'
 		let randomKey = nanoid();
 		writeLog(this.logs, "attempting to render", {
 			element: element,
@@ -57,15 +53,15 @@ class AppConnector {
 			componentProp: this.componentProps?.config,
 			componentPropParsed: parseJSON(this.componentProps?.config),
 		});
-		ReactContentRenderer.render(
+		const root = createRoot(element);
+		root.render(
 			<Provider store={store}>
 				<Component
 					key={randomKey}
 					componentProp={this.componentProps.config}
 					aemDataSet={this.componentProps}
 				/>
-			</Provider>,
-			element
+			</Provider>
 		);
 	}
 }
@@ -80,6 +76,6 @@ window.initConfiguration = async () => {
 
 	// /* Get the Anonymous token on initial load */
 	// return await initAuth();
-}
+};
 
 window.AppConnector = AppConnector;
