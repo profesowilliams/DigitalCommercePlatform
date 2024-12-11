8fc7cfd8d2e9c5a1427410ac5f8157e8b1df0024
Venugopal, Shyamala (c) <Shyamala.Venugopal@techdata.com>
2024-12-11 19:49:50 +0000

    Merged PR 94816: [619408]-UAT - Opportunities - "Undefined" is shown for grouped quotes in the...
    [619408]-UAT - Opportunities - "Undefined" is shown for grouped quotes in the expanded section
    
    Related work items: #619408

--- content/techdata/ui.react.frontend/src/global/techdata/components/RenewalsGrid/Columns/PriceColumn.jsx
+++ content/techdata/ui.react.frontend/src/global/techdata/components/RenewalsGrid/Columns/PriceColumn.jsx
@@ -5,9 +5,11 @@ import { useRenewalGridState } from "../store/RenewalsStore";
 function PriceColumn({ columnValue, currency }) {
   const valueInThousands = currency === 'VND' ? thousandSeparator(columnValue, 0) :
     thousandSeparator(columnValue);
+    const currencyValue = !columnValue || parseInt(columnValue) === 0
+    ? '-' : `${valueInThousands} ${currency}` ;
   const { displayCurrencyName = false } = useRenewalGridState(state => state.aemConfig);
   const selectedFormat = displayCurrencyName
-    ? `${valueInThousands} ${currency}` 
+    ? currencyValue
     : valueInThousands;
   return (
     <div className="cmp-price-column">       
